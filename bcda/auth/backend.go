6e7e57cb405a31a0c648a5bcee8dd0fa0c5beec8
Ryan Nagle <rmnagle@gmail.com>
2018-08-02 08:03:29 -0500

    jwt auth backend and middleware

---
+++ bcda/auth/backend.go
@@ -0,0 +1,109 @@
+package auth
+
+import (
+	"bufio"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JWTAuthenticationBackend struct {
+	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
+}
+
+const (
+	tokenDuration      = 72
+	expireOffset       = 3600
+	jwtExpirationDelta = 72
+)
+
+var authBackendInstance *JWTAuthenticationBackend = nil
+
+func InitAuthBackend() *JWTAuthenticationBackend {
+	if authBackendInstance == nil {
+		authBackendInstance = &JWTAuthenticationBackend{
+			privateKey: getPrivateKey(),
+			publicKey:  getPublicKey(),
+		}
+	}
+
+	return authBackendInstance
+}
+
+func (backend *JWTAuthenticationBackend) GenerateToken(userUUID string) (string, error) {
+	token := jwt.New(jwt.SigningMethodRS512)
+	token.Claims = jwt.MapClaims{
+		"exp": time.Now().Add(time.Hour * time.Duration(jwtExpirationDelta)).Unix(),
+		"iat": time.Now().Unix(),
+		"sub": userUUID,
+	}
+	tokenString, err := token.SignedString(backend.privateKey)
+	if err != nil {
+		panic(err)
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func getPrivateKey() *rsa.PrivateKey {
+	privateKeyFile, err := os.Open(os.Getenv("JWT_PRIVATE_KEY_FILE"))
+	if err != nil {
+		panic(err)
+	}
+
+	pemfileinfo, _ := privateKeyFile.Stat()
+	var size int64 = pemfileinfo.Size()
+	pembytes := make([]byte, size)
+
+	buffer := bufio.NewReader(privateKeyFile)
+	_, err = buffer.Read(pembytes)
+
+	data, _ := pem.Decode([]byte(pembytes))
+
+	privateKeyFile.Close()
+
+	privateKeyImported, err := x509.ParsePKCS1PrivateKey(data.Bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return privateKeyImported
+}
+
+func getPublicKey() *rsa.PublicKey {
+	publicKeyFile, err := os.Open(os.Getenv("JWT_PUBLIC_KEY_FILE"))
+	if err != nil {
+		panic(err)
+	}
+
+	pemfileinfo, _ := publicKeyFile.Stat()
+	var size int64 = pemfileinfo.Size()
+	pembytes := make([]byte, size)
+
+	buffer := bufio.NewReader(publicKeyFile)
+	_, err = buffer.Read(pembytes)
+
+	data, _ := pem.Decode([]byte(pembytes))
+
+	publicKeyFile.Close()
+
+	publicKeyImported, err := x509.ParsePKIXPublicKey(data.Bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	rsaPub, ok := publicKeyImported.(*rsa.PublicKey)
+
+	if !ok {
+		panic(err)
+	}
+
+	return rsaPub
+}
---
+++ bcda/auth/middleware.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+)
+
+func RequireTokenAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authBackend := InitAuthBackend()
+
+		var keyFunc jwt.Keyfunc = func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			} else {
+				return authBackend.publicKey, nil
+			}
+		}
+
+		token, err := request.ParseFromRequest(r, request.OAuth2Extractor, keyFunc)
+
+		// TODO(rnagle): check to see if token has been invalidated/blacklisted
+		if err == nil && token.Valid {
+			ctx := context.WithValue(r.Context(), "token", token)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			http.Error(w, http.StatusText(401), 401)
+			return
+		}
+	})
+}
