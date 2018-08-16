7ad422972ca6e6e4ac22b9f9705c3bee5a339eaf
Emily Hart <1923441+embh@users.noreply.github.com>
2018-08-16 10:20:41 -0400

    Add test for IsBlacklisted()

--- bcda/auth/backend_test.go
+++ bcda/auth/backend_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/CMSgov/bcda-app/bcda/auth"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -49,12 +50,27 @@ func (s *BackendTestSuite) TestGenerateToken() {
 
 func (s *BackendTestSuite) TestRevokeToken() {
 	token, _ := s.authBackend.GenerateToken(
-		"82503A18-BF3B-436D-BA7B-BAE09B7FFD2F", "DBBD1CE1-AE24-435C-807D-ED45953077D3")
+		"EFE6E69A-CD6B-4335-A2F2-4DBEDCCD3E73", "DBBD1CE1-AE24-435C-807D-ED45953077D3")
 	err := s.authBackend.RevokeToken(token)
 	assert.Nil(s.T(), err)
 }
 
-func (s *BackendTestSuite) TestIsBlacklisted() {}
+func (s *BackendTestSuite) TestIsBlacklisted() {
+	tokenString, _ := s.authBackend.GenerateToken(
+		"EFE6E69A-CD6B-4335-A2F2-4DBEDCCD3E73", "DBBD1CE1-AE24-435C-807D-ED45953077D3")
+
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return s.authBackend.PublicKey, nil
+	})
+
+	blacklisted := s.authBackend.IsBlacklisted(token)
+	assert.False(s.T(), blacklisted)
+
+	_ = s.authBackend.RevokeToken(tokenString)
+
+	blacklisted = s.authBackend.IsBlacklisted(token)
+	assert.True(s.T(), blacklisted)
+}
 
 func TestBackendTestSuite(t *testing.T) {
 	suite.Run(t, new(BackendTestSuite))
