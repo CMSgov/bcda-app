a3c18ddc301dc431dbfb1751d26f2ea1d949ebeb
Matt Snook <matt.snook@adhocteam.us>
2018-09-10 22:48:01 -0400

    BCDA-235: Initial cut at introducing code coverage metrics into the test process.

--- unit_test.sh
+++ unit_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 #
 # This script is intended to be run from within the Docker "unit_test" container
-# The docker-compose file brings forward the env vars: DB_HOST_URL and TEST_DB_URL
+# The docker-compose file brings forward the env vars: DB
 #
 
 echo "Running linter..."
 golangci-lint run
 
 echo "Setting up test DB (bcda_test)..."
+DB_HOST_URL=${DB}?sslmode=disable
+TEST_DB_URL=${DB}/bcda_test?sslmode=disable
 echo "DB_HOST_URL is $DB_HOST_URL"
 echo "TEST_DB_URL is $TEST_DB_URL"
 usql $DB_HOST_URL -c 'drop database if exists bcda_test;'
@@ -16,8 +17,14 @@ usql $DB_HOST_URL -c 'create database bcda_test;'
 usql $TEST_DB_URL -f db/api.sql
 usql $TEST_DB_URL -f db/fixtures.sql
 
-echo "Running unit tests..."
-DATABASE_URL=$TEST_DB_URL go test -v -race ./...
+timestamp=`date +%Y-%m-%d_%H-%M-%S`
+echo "Running unit tests and placing results/coverage in test_results/${timestamp} on host..."
+mkdir -p test_results/${timestamp}
+DATABASE_URL=$TEST_DB_URL go test -v -race ./... -coverprofile test_results/${timestamp}/testcoverage.out 2>&1 | tee test_results/${timestamp}/testresults.out
+go tool cover -func test_results/${timestamp}/testcoverage.out > test_results/${timestamp}/testcov_byfunc.out
+echo TOTAL COVERAGE:  $(tail -1 test_results/${timestamp}/testcov_byfunc.out | head -1)
+go tool cover -html=test_results/${timestamp}/testcoverage.out -o test_results/${timestamp}/testcoverage.html
+
 
 echo "Cleaning up test DB (bcda_test)..."
 usql $DB_HOST_URL -c 'drop database bcda_test;'
