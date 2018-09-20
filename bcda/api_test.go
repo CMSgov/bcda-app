a54c2b2e50a0e53942363088ee186a851e25ce48
Emily Hart <1923441+embh@users.noreply.github.com>
2018-09-20 12:29:16 -0400

    Update api_test.go to create and delete jobs with GORM

--- bcda/api_test.go
+++ bcda/api_test.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/CMSgov/bcda-app/bcda/bcdaModels"
+	"github.com/CMSgov/bcda-app/bcda/database"
 	"github.com/go-chi/chi"
+	"github.com/jinzhu/gorm"
+	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -14,9 +20,13 @@ import (
 type APITestSuite struct {
 	suite.Suite
 	rr *httptest.ResponseRecorder
+	db *gorm.DB
 }
 
 func (s *APITestSuite) SetupTest() {
+	bcdaModels.InitializeGormModels()
+	s.db = database.GetGORMDbConnection()
+
 	s.rr = httptest.NewRecorder()
 }
 
@@ -33,31 +43,49 @@ func (s *APITestSuite) TestBulkRequestMissingToken() {
 }
 
 func (s *APITestSuite) TestJobStatusPending() {
-	req, err := http.NewRequest("GET", "/api/v1/jobs/1", nil)
+	j := bcdaModels.Job{
+		AcoID:      uuid.Parse("DBBD1CE1-AE24-435C-807D-ED45953077D3"),
+		UserID:     uuid.Parse("82503A18-BF3B-436D-BA7B-BAE09B7FFD2F"),
+		RequestURL: "/api/v1/Patient/$export",
+		Status:     "Pending",
+	}
+	s.db.Save(&j)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/v1/jobs/%d", j.ID), nil)
 	assert.Nil(s.T(), err)
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(jobStatus)
 
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("jobId", "1")
+	rctx.URLParams.Add("jobId", fmt.Sprint(j.ID))
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(s.T(), http.StatusAccepted, rr.Code)
 	assert.Equal(s.T(), "Pending", rr.Header().Get("X-Progress"))
+
+	s.db.Unscoped().Delete(&j)
 }
 
 func (s *APITestSuite) TestJobStatusInProgress() {
-	req, err := http.NewRequest("GET", "/api/v1/jobs/2", nil)
+	j := bcdaModels.Job{
+		AcoID:      uuid.Parse("DBBD1CE1-AE24-435C-807D-ED45953077D3"),
+		UserID:     uuid.Parse("82503A18-BF3B-436D-BA7B-BAE09B7FFD2F"),
+		RequestURL: "/api/v1/Patient/$export",
+		Status:     "In Progress",
+	}
+	s.db.Save(&j)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/v1/jobs/%d", j.ID), nil)
 	assert.Nil(s.T(), err)
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(jobStatus)
 
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("jobId", "2")
+	rctx.URLParams.Add("jobId", fmt.Sprint(j.ID))
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
 	handler.ServeHTTP(rr, req)
@@ -67,37 +95,69 @@ func (s *APITestSuite) TestJobStatusInProgress() {
 }
 
 func (s *APITestSuite) TestJobStatusFailed() {
-	req, err := http.NewRequest("GET", "/api/v1/jobs/3", nil)
+	j := bcdaModels.Job{
+		AcoID:      uuid.Parse("DBBD1CE1-AE24-435C-807D-ED45953077D3"),
+		UserID:     uuid.Parse("82503A18-BF3B-436D-BA7B-BAE09B7FFD2F"),
+		RequestURL: "/api/v1/Patient/$export",
+		Status:     "Failed",
+	}
+
+	s.db.Save(&j)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/v1/jobs/%d", j.ID), nil)
 	assert.Nil(s.T(), err)
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(jobStatus)
 
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("jobId", "3")
+	rctx.URLParams.Add("jobId", fmt.Sprint(j.ID))
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(s.T(), http.StatusInternalServerError, rr.Code)
+
+	s.db.Delete(&j)
 }
 
 func (s *APITestSuite) TestJobStatusCompleted() {
-	req, err := http.NewRequest("GET", "/api/v1/jobs/4", nil)
+	j := bcdaModels.Job{
+		AcoID:      uuid.Parse("DBBD1CE1-AE24-435C-807D-ED45953077D3"),
+		UserID:     uuid.Parse("82503A18-BF3B-436D-BA7B-BAE09B7FFD2F"),
+		RequestURL: "/api/v1/Patient/$export",
+		Status:     "Completed",
+	}
+	s.db.Save(&j)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/v1/jobs/%d", j.ID), nil)
 	assert.Nil(s.T(), err)
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(jobStatus)
 
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("jobId", "4")
+	rctx.URLParams.Add("jobId", fmt.Sprint(j.ID))
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(s.T(), http.StatusOK, rr.Code)
 	assert.Equal(s.T(), "application/json", rr.Header().Get("Content-Type"))
-	assert.Equal(s.T(), `{"transactionTime":"2018-09-18T20:38:22.428453Z","request":"/api/v1/Patient/$export","requiresAccessToken":true,"output":[{"type":"ExplanationOfBenefit","url":"http:///data/DBBD1CE1-AE24-435C-807D-ED45953077D3.ndjson"}],"error":[]}`, rr.Body.String())
+
+	var rb bulkResponseBody
+	err = json.Unmarshal(rr.Body.Bytes(), &rb)
+	if err != nil {
+		s.Error(err)
+	}
+
+	assert.Equal(s.T(), j.RequestURL, rb.RequestURL)
+	assert.Equal(s.T(), true, rb.RequiresAccessToken)
+	assert.Equal(s.T(), "ExplanationOfBenefit", rb.Files[0].Type)
+	assert.Equal(s.T(), "http:///data/DBBD1CE1-AE24-435C-807D-ED45953077D3.ndjson", rb.Files[0].URL)
+	assert.Empty(s.T(), rb.Errors)
+
+	s.db.Delete(&j)
 }
 
 func (s *APITestSuite) TestServeData() {
