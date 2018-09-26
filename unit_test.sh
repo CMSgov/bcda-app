c8fd7f837996bd9e606f3349f473e8da1c3156a4
Michael Knoll <mknoll@fearless.tech>
2018-09-26 14:17:52 -0400

    refactoring install of dep to go get instead of scary curl command CHanging unit test execution to copy results to latest at end of run

--- unit_test.sh
+++ unit_test.sh
@@ -22,11 +22,12 @@ usql $TEST_DB_URL -f db/fixtures.sql
 timestamp=`date +%Y-%m-%d_%H-%M-%S`
 echo "Running unit tests and placing results/coverage in test_results/${timestamp} on host..."
 mkdir -p test_results/${timestamp}
+mkdir -p test_results/latest
 DATABASE_URL=$TEST_DB_URL go test -v -race ./... -coverprofile test_results/${timestamp}/testcoverage.out 2>&1 | tee test_results/${timestamp}/testresults.out
 go tool cover -func test_results/${timestamp}/testcoverage.out > test_results/${timestamp}/testcov_byfunc.out
 echo TOTAL COVERAGE:  $(tail -1 test_results/${timestamp}/testcov_byfunc.out | head -1)
 go tool cover -html=test_results/${timestamp}/testcoverage.out -o test_results/${timestamp}/testcoverage.html
-
+cp test_results/${timestamp}/* test_results/latest
 
 echo "Cleaning up test DB (bcda_test)..."
 usql $DB_HOST_URL -c 'drop database bcda_test;'
