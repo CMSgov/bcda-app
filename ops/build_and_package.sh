38c8327b451a1e08ecee9551227f04d8eb0a39da
Matt Snook <matt.snook@adhocteam.us>
2018-11-01 16:15:35 -0400

    BCDA-392: Include swaggerui directory in packaged rpm.

--- ops/build_and_package.sh
+++ ops/build_and_package.sh
@@ -25,11 +25,11 @@ go clean
 echo "Building bcda binary..." 
 go build -ldflags "-X main.version=$VERSION"
 echo "Packaging bcda binary into RPM..."
-fpm -v $VERSION -s dir -t rpm -n bcda bcda=/usr/local/bin/bcda
+fpm -v $VERSION -s dir -t rpm -n bcda bcda=/usr/local/bcda/bcda swaggerui=/usr/local/bcda/swaggerui
 cd ../bcdaworker
 go clean 
 echo "Building bcdaworker..."
 go build
 echo "Packaging bcdaworker binary into RPM..."
-fpm -v $VERSION -s dir -t rpm -n bcdaworker bcdaworker=/usr/local/bin/bcdaworker
+fpm -v $VERSION -s dir -t rpm -n bcdaworker bcdaworker=/usr/local/bcda/bcdaworker
 
