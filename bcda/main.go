ceb3a5e3f56bcdc0978f9cd9ca99555e7a67b931
Ryan Nagle <rmnagle@gmail.com>
2018-11-15 16:50:03 -0600

    Add a (temporary) way for clients to request file encryption (#74)
    * add a way for clients to request their data files be encrypted using the sandbox key pair (shared_files/ATO_*.pem)
    
    * remove unused env vars from docker-compose.yml

--- bcda/main.go
+++ bcda/main.go
@@ -38,6 +38,8 @@ type jobEnqueueArgs struct {
 	AcoID          string
 	UserID         string
 	BeneficiaryIDs []string
+	// TODO(rnagle): remove `Encrypt` when file encryption functionality is ready for release
+	Encrypt bool
 }
 
 // swagger:model fileItem
