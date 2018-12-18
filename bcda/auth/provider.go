9c820aca0d41c10475b13b292b637954dfa34edf
Emily Hart <1923441+embh@users.noreply.github.com>
2018-12-18 11:53:51 -0500

    Registering and updating client may need response content

--- bcda/auth/provider.go
+++ bcda/auth/provider.go
@@ -2,8 +2,8 @@ package auth
 
 // Provider is an interface for operations performed through an authentication provider.
 type Provider interface {
-	RegisterClient(params []byte) error
-	UpdateClient(params []byte) error
+	RegisterClient(params []byte) ([]byte, error)
+	UpdateClient(params []byte) ([]byte, error)
 	DeleteClient(params []byte) error
 
 	GenerateClientCredentials(params []byte) ([]byte, error)
