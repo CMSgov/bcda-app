7cbca30df929e0440f0b7271dd95c13432a09448
Terrance Bell <tbell@fearless.tech>
2019-01-16 17:47:51 -0500

    added a larger allocation of synthetic benes

--- db/fixtures.sql
+++ db/fixtures.sql
@@ -4,7 +4,8 @@ insert into acos values ('c14822fa-19ee-402c-9248-32af98419fe3', 'ACO Revoked', 
 insert into acos values ('82f55b6a-728e-4c8b-807e-535caad7b139', 'ACO Not Revoked', default, default);
 insert into acos values ('3461C774-B48F-11E8-96F8-529269fb1459', 'ACO Small', default,default),
                         ('C74C008D-42F8-4ED9-BF88-CEE659C7F692', 'ACO Medium', default, default),
-                        ('8D80925A-027E-43DD-8AED-9A501CC4CD91', 'ACO Large', default, default);
+                        ('8D80925A-027E-43DD-8AED-9A501CC4CD91', 'ACO Large', default, default),
+                        ('55954dba-d4d9-438d-bd03-453da4993fe9', 'ACO Extra Large', default, default);
 insert into acos values ('0c527d2e-2e8a-4808-b11d-0fa06baf8254', 'ACO Dev', default, default);
 
 insert into users values ('82503A18-BF3B-436D-BA7B-BAE09B7FFD2F', 'User One', 'userone@email.com', 'DBBD1CE1-AE24-435C-807D-ED45953077D3', default, default);
