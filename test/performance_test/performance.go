f148dcc609fd7016c67a202cd89571185f135b6a
Terrance Bell <tbell@fearless.tech>
2019-02-04 19:23:57 -0500

    testing

--- test/performance_test/performance.go
+++ test/performance_test/performance.go
@@ -130,7 +130,7 @@ func writeResults(filename string, buf bytes.Buffer) {
 		}
 		fn := fmt.Sprintf("%s/%s_%s.html", reportFilePath, filename, s)
 		fmt.Printf("Writing results: %s\n", fn)
-		err := ioutil.WriteFile(fn, data, 0644)
+		err := ioutil.WriteFile(fn, data, os.ModePerm)
 		if err != nil {
 			panic(err)
 		}
