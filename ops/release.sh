a0555d20d1eadf86b0025eb3520d4d3eb154d587
Matt Snook <matt.snook@adhocteam.us>
2019-02-15 09:49:50 -0500

    BCDA-672: Fix syntax issue.

--- ops/release.sh
+++ ops/release.sh
@@ -107,7 +107,7 @@ echo "" >> $TMPFILE
 
 git tag -a -m"$PROJECT_NAME release $NEWTAG" -s "$NEWTAG"
 
-RELEASE_PATH = "/repos$GITHUB_REPO_PATH/releases"
+RELEASE_PATH="/repos$GITHUB_REPO_PATH/releases"
 python github_release.py --release $NEWTAG --release-file $TMPFILE --repo $RELEASE_PATH
 
 rm $TMPFILE
