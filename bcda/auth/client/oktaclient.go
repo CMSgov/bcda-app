22a4ea21d860808701ff101d7cf8718552034eeb
Deirdre Holub <deirdre@adhocteam.us>
2019-02-20 11:43:20 -0800

    removed okta sdk use and imports

--- bcda/auth/client/oktaclient.go
+++ bcda/auth/client/oktaclient.go
@@ -1,14 +1,10 @@
 package client
 
 import (
-	"errors"
-	"net/http"
 	"os"
 
 	"github.com/pborman/uuid"
 
-	"github.com/okta/okta-sdk-golang/okta"
-	"github.com/okta/okta-sdk-golang/okta/query"
 	"github.com/sirupsen/logrus"
 )
 
@@ -41,84 +37,6 @@ func init() {
 	if oktaURL == "" {
 		logger.Fatal("No Okta URL found; please set OKTA_CLIENT_ORGURL")
 	}
-}
-
-func HealthCheck() error {
-	reqId := uuid.NewRandom()
-	client := NewOktaClient()
-
-	// The email in OKTA_EMAIL should represent a test user present in the Okta sandbox environment
-	oktaTestUser, success := os.LookupEnv("OKTA_EMAIL")
-	if !success {
-		logRequest(reqId).Warn("Unable to perform Okta HealthCheck.  Please set OKTA_EMAIL to match a test user account.")
-		return errors.New("cannot perform Okta health check without OKTA_EMAIL configured ")
-	}
-
-	logRequest(reqId).Info("Okta ping request")
-	users, resp, err := client.User.ListUsers(query.NewQueryParams(query.WithQ(oktaTestUser)))
-	if err != nil {
-		logError(err, reqId).Error("Okta ping request error")
-		return err
-	}
-
-	if len(users) >= 1 {
-		logResponse(resp.StatusCode, reqId).Info("Okta ping request successful")
-		return nil
-	}
-
-	logResponse(resp.StatusCode, reqId).Info("Okta ping request unsuccessful")
-	return errors.New("ping unsuccessful ")
-}
-
-func DeleteUser(userId string) (bool, error) {
-	reqId := uuid.NewRandom()
-	client := NewOktaClient()
-	userField := logrus.Fields{"user_id": userId}
-
-	logRequest(reqId).WithFields(userField).Info("Okta delete user request")
-	resp, err := client.User.DeactivateUser(userId, nil)
-	if err != nil {
-		logError(err, reqId).WithFields(userField).Error("Okta delete user error")
-		return false, err
-	}
-
-	logResponse(resp.StatusCode, reqId).WithFields(userField).Info("Okta delete user success")
-	return true, err
-}
-
-func FindUser(email string) (string, error) {
-	reqId := uuid.NewRandom()
-	client := NewOktaClient()
-
-	logRequest(reqId).Info("Okta find request")
-	filter := query.NewQueryParams(query.WithQ(email))
-	users, resp, err := client.User.ListUsers(filter)
-	if err != nil {
-		logError(err, reqId).Error("Okta find error")
-		return "", err
-	}
-
-	l := logResponse(resp.StatusCode, reqId)
-	switch len(users) {
-	case 0:
-		l.Info("Okta user find request unsuccessful")
-		return "", err
-	case 1:
-		userId := users[0].Id
-		userField := logrus.Fields{"user_id": userId}
-		l.WithFields(userField).Info("Okta user find request successful")
-		return userId, err
-	default:
-		l.Info("Okta user find request returned more than one user")
-		return "", err
-	}
-}
-
-func NewOktaClient() *okta.Client {
-	// Reads OKTA_CLIENT_TOKEN and OKTA_CLIENT_ORGURL for configuration
-	config := okta.NewConfig()
-	httpClient := &http.Client{}
-	return okta.NewClient(config, httpClient, nil)
 }
 
 func logRequest(requestId uuid.UUID) *logrus.Entry {
--- bcda/auth/client/oktaclient_test.go
+++ bcda/auth/client/oktaclient_test.go
@@ -3,58 +3,20 @@
 // To enable this test suite:
 // 1. Put an appropriate token into env var OKTA_CLIENT_TOKEN
 // 2. Put an existing Okta user email address into OKTA_EMAIL
-// 3. Run "go test -tags=okta" from the bcda/auth/client directory
+// 3. Run "go test -tags=okta -v" from the bcda/auth/client directory
 
 package client_test
 
 import (
-	"os"
 	"testing"
 
-	"github.com/CMSgov/bcda-app/bcda/auth/client"
 	"github.com/okta/okta-sdk-golang/okta"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
 type OTestSuite struct {
 	suite.Suite
 	oClient *okta.Client
-}
-
-func (s *OTestSuite) SetupTest() {
-	s.oClient = client.NewOktaClient()
-}
-
-func (s *OTestSuite) TestCreateClient() {
-
-}
-
-func (s *OTestSuite) TestFindUser() {
-	// The email in OKTA_EMAIL should represent a test user present in the Okta sandbox environment
-	userEmail, success := os.LookupEnv("OKTA_EMAIL")
-	assert.True(s.T(), success, "Please set OKTA_EMAIL to match a test user account")
-
-	u, err := client.FindUser(userEmail)
-	assert.Nil(s.T(), err)
-	assert.NotEqual(s.T(), "", u)
-
-	// Should return more than one user
-	u, err = client.FindUser("user")
-	assert.Nil(s.T(), err)
-	assert.Equal(s.T(), "", u)
-}
-
-func (s *OTestSuite) TestHealthCheck() {
-	err := client.HealthCheck()
-	assert.Nil(s.T(), err)
-}
-
-func (s *OTestSuite) TestDeleteUser() {
-	// Real user ID's are random lowercase alpha/numeric
-	b, err := client.DeleteUser("INVALID_USER_ID")
-	assert.NotNil(s.T(), err)
-	assert.False(s.T(), b)
 }
 
 func (s *OTestSuite) TearDownTest() {
