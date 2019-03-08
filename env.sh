951ee3be2076d30a435ac6068d7bef02d510f179
alexhova <alexhovancik@gmail.com>
2019-03-08 15:00:40 -0600

    small fix to env.sh

--- env.sh
+++ env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-# Source this script to add any environment variables for local development
+# Edit and source this script to add any environment 
+# variables for local development
 #
-# This file is included in .gitignore and should not be committed as it
-# may contain secrets
+# This file is included in .gitignore and should not
+# be committed as it may contain secrets
 #
 export BCDA_AUTH_PROVIDER=okta
-export OKTA_OAUTH_SERVER_ID=<serverID>
-export OKTA_API_KEY=<apiKey>
-export OKTA_CLIENT_SECRET=<clientSecret>
+export OKTA_OAUTH_SERVER_ID="<serverID>"
+export OKTA_API_KEY="<apiKey>"
+export OKTA_CLIENT_SECRET="<clientSecret>"
