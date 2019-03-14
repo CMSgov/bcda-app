c10a28409aed0bdd369533b8fdb6ebaa05c93d6c
Emily Hart <1923441+embh@users.noreply.github.com>
2019-03-14 10:13:47 -0400

    Fixes to beneficiaries table

---
+++ db/migrations/20190314-fix-01.sql
@@ -0,0 +1,7 @@
+ALTER TABLE beneficiaries
+    DROP CONSTRAINT beneficiaries_pkey,
+    DROP CONSTRAINT beneficiaries_aco_id_fkey,
+    DROP COLUMN beneficiary_id,
+    DROP COLUMN patient_id,
+    DROP COLUMN aco_id,
+    ADD PRIMARY KEY (id);
