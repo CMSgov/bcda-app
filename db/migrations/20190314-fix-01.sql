dfbf3dad09dc862fd30cbc5fffbd9fa9ede35dfb
Emily Hart <1923441+embh@users.noreply.github.com>
2019-03-14 16:03:20 -0400

    Update 20190314-fix-01.sql

--- db/migrations/20190314-fix-01.sql
+++ db/migrations/20190314-fix-01.sql
@@ -1,9 +1,17 @@
-ALTER TABLE beneficiaries
-    DROP CONSTRAINT beneficiaries_pkey,
-    DROP CONSTRAINT beneficiaries_aco_id_fkey,
-    DROP COLUMN beneficiary_id,
-    DROP COLUMN patient_id,
-    DROP COLUMN aco_id,
-    ADD PRIMARY KEY (id);
+CREATE TABLE acos_beneficiaries (
+  aco_id uuid NOT NULL,
+  beneficiary_id int NOT NULL
+);
 
-DELETE FROM beneficiaries WHERE blue_button_id IS null;
+INSERT INTO acos_beneficiaries (aco_id, beneficiary_id) 
+(SELECT aco_id, beneficiary_id FROM beneficiaries);
+
+ALTER TABLE beneficiaries
+    RENAME COLUMN beneficiary_id TO id;
+  
+ALTER TABLE beneficiaries
+    RENAME COLUMN patient_id TO blue_button_id;
+
+ALTER TABLE beneficiaries
+    DROP CONSTRAINT beneficiaries_aco_id_fkey,
+    DROP COLUMN aco_id;
