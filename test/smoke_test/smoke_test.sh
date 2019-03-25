5b2091555254407e7c16ef6b907f5d5c3f2636f1
Michael Knoll <mknoll@fearless.tech>
2019-03-25 14:18:49 -0400

    adding echos and removing double run of the coverage test.

--- test/smoke_test/smoke_test.sh
+++ test/smoke_test/smoke_test.sh
@@ -3,15 +3,15 @@
 # This script is intended to be run from within the Docker "smoke_test" container
 #
 set -e
-echo $PATH
-echo "Running Coverage Encrypted"
-go run bcda_client.go -host=api:3000 -endpoint=Coverage
 echo "Running EOB Encrypted"
 go run bcda_client.go -host=api:3000 -endpoint=ExplanationOfBenefit
 echo "Running Patient Encrypted"
 go run bcda_client.go -host=api:3000 -endpoint=Patient
 echo "Running Coverage Encrypted"
 go run bcda_client.go -host=api:3000 -endpoint=Coverage
+echo "Running EOB Unencrypted"
 go run bcda_client.go -host=api:3000 -endpoint=ExplanationOfBenefit -encrypt=false
+echo "Running Patient Unencrypted"
 go run bcda_client.go -host=api:3000 -endpoint=Patient -encrypt=false
+echo "Running Coverage Unencrypted"
 go run bcda_client.go -host=api:3000 -endpoint=Coverage -encrypt=false
