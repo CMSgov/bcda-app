807c3dcd880afa5cb10da9f81d0bf3310ed2edec
Emily Hart <1923441+embh@users.noreply.github.com>
2019-04-10 08:26:27 -0400

    BCDA-1182: Log worker health check errors (#229)
    * Log worker health check errors

--- bcda/health/health.go
+++ bcda/health/health.go
@@ -11,24 +11,36 @@ import (
 
 func IsDatabaseOK() bool {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Error("Health check: database connection error: ", err.Error())
+		return false
+	}
 	defer func() {
 		if err := db.Close(); err != nil {
 			log.Infof("failed to close db connection at bcda/health/health.go#IsDatabaseOK() because %s", err)
 		}
 	}()
-	if err != nil {
+
+	if err := db.Ping(); err != nil {
+		log.Error("Health check: database ping error: ", err.Error())
 		return false
 	}
 
-	return db.Ping() == nil
+	return true
 }
 
 func IsBlueButtonOK() bool {
 	bbc, err := client.NewBlueButtonClient()
 	if err != nil {
+		log.Error("Health check: Blue Button client error: ", err.Error())
 		return false
 	}
 
 	_, err = bbc.GetMetadata()
-	return err == nil
+	if err != nil {
+		log.Error("Health check: Blue Button connection error: ", err.Error())
+		return false
+	}
+
+	return true
 }
