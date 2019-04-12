5ea2dba1d438f13e5cc5dbe9f4cc24df414b08bb
Deirdre Holub <deirdre@adhocteam.us>
2019-04-12 09:58:45 -0700

    new env functions

--- bcda/testUtils/utils.go
+++ bcda/testUtils/utils.go
@@ -139,3 +139,36 @@ func RandomBase64(n int) string {
 	}
 	return base64.StdEncoding.EncodeToString(b)
 }
+
+func setEnv(why, key, value string) {
+	if err := os.Setenv(key, value); err != nil {
+		log.Printf("Error %s env value %s to %s\n", why, key, value)
+	}
+}
+
+// SetAndRestoreEnvKey replaces the current value of the env var key,
+// returning a function which can be used to restore the original value
+func SetAndRestoreEnvKey(key, value string) func() {
+	originalValue := os.Getenv(key)
+	setEnv("setting", key, value)
+	return func() {
+		setEnv("restoring", key, originalValue)
+	}
+}
+
+// SetUnitTestKeysForAuth sets the env vars auth uses to locate its signing key pair. Intended for use only
+// by unit tests of the API. Should be called by any API test that uses the auth backend. Returns a function
+// that will restore the original values, suitable for use with defer.
+func SetUnitTestKeysForAuth() func() {
+	private := SetAndRestoreEnvKey("JWT_PRIVATE_KEY_FILE", "../shared_files/api_unit_test_auth_private.pem")
+	public := SetAndRestoreEnvKey("JWT_PUBLIC_KEY_FILE", "../shared_files/api_unit_test_auth_public.pem")
+
+	return func() {
+		private()
+		public()
+	}
+	// if these paths are incorrect, unit tests will fail with an unhelpful panic referencing a logrus entry
+	// these paths only work because we assume that the binary location doesn't change from test to test or env to env
+	// the issue, and a path to a more robust solution, is well described in
+	// https://stackoverflow.com/questions/45579312/loading-a-needed-file-relative-vs-absolute-paths
+}
