6520188829266ae601ee42e8a27c63fea6d39dfb
Matt Snook <matt.snook@adhocteam.us>
2019-04-29 13:43:29 -0400

    BCDA-1186: Only perform a subset of initialization if the executable is in ETL Mode.

--- bcda/main.go
+++ bcda/main.go
@@ -33,7 +33,11 @@ var (
 )
 
 func init() {
-	createAPIDirs()
+	isEtlMode := os.Getenv("BCDA_ETL_MODE")
+	if isEtlMode != "true" {
+		createAPIDirs()
+	}
+
 	log.SetFormatter(&log.JSONFormatter{})
 	filePath := os.Getenv("BCDA_ERROR_LOG")
 
@@ -45,8 +49,11 @@ func init() {
 	} else {
 		log.Info("Failed to open error log file; using default stderr")
 	}
-	monitoring.GetMonitor()
-	log.Info(fmt.Sprintf(`Auth is made possible by %T`, auth.GetProvider()))
+
+	if isEtlMode != "true" {	
+		monitoring.GetMonitor()
+		log.Info(fmt.Sprintf(`Auth is made possible by %T`, auth.GetProvider()))
+	}
 
 }
 
