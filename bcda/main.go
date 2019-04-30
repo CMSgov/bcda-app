c7dd17b3046a5529dbe720337c3a5b9bc5f64212
Matt Snook <matt.snook@adhocteam.us>
2019-04-30 10:05:23 -0400

    Add logging to specify API vs ETL mode.

--- bcda/main.go
+++ bcda/main.go
@@ -49,11 +49,15 @@ func init() {
 	} else {
 		log.Info("Failed to open error log file; using default stderr")
 	}
-
+   
 	if isEtlMode != "true" {	
+                log.Info("BCDA application is running in API mode.")
 		monitoring.GetMonitor()
 		log.Info(fmt.Sprintf(`Auth is made possible by %T`, auth.GetProvider()))
 	}
+        else {
+		log.Info("BCDA application is running in ETL mode.")
+  	}
 
 }
 
