f0676342e53b312bc8cbe67f2e2cbae665b228f7
Michael Knoll <42681520+knollfear@users.noreply.github.com>
2019-05-22 14:23:17 -0400

    Update bcda/models/models.go
    Co-Authored-By: Emily Hart <1923441+embh@users.noreply.github.com>

--- bcda/models/models.go
+++ bcda/models/models.go
@@ -187,7 +187,7 @@ func (aco *ACO) GetBeneficiaryIDs() (cclfBeneficiaryIDs []string, err error) {
 		log.Errorf("Error retrieving beneficiaries from latest CCLF8 file for ACO ID %s: %s", aco.UUID.String(), err.Error())
 		return nil, err
 	} else if len(cclfBeneficiaryIDs) == 0 {
-		log.Errorf("Retrieved 0 ACO-beneficiaries for ACO ID %s", aco.UUID.String())
+		log.Errorf("Found 0 beneficiaries from latest CCLF8 file for ACO ID %s", aco.UUID.String())
 		return nil, fmt.Errorf("retrieved 0 ACO-beneficiaries for ACO ID %s", aco.UUID.String())
 	}
 
