b3071be740729e7ec9ca34d2e708e669de231ebd
Michael Knoll <mknoll@fearless.tech>
2019-05-31 10:18:45 -0400

    moving BB_HASH_PEPPER and BB_HASH_ITER to environment variables so they can be flexible on a per environment basis.

--- bcda/client/bluebutton.go
+++ bcda/client/bluebutton.go
@@ -6,6 +6,7 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"fmt"
+	"github.com/CMSgov/bcda-app/bcda/utils"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -26,7 +27,8 @@ import (
 var logger *logrus.Logger
 
 const blueButtonBasePath = "/v1/fhir"
-const blueButtonPepper = "b8ebdcc47fdd852b8b0201835c6273a9177806e84f2d9dc4f7ecaff08681e86d74195c6aef2db06d3d44c9d0b8f93c3e6c43d90724b605ac12585b9ab5ee9c3f00d5c0d284e6b8e49d502415c601c28930637b58fdca72476e31c22ad0f24ecd761020d6a4bcd471f0db421d21983c0def1b66a49a230f85f93097e9a9a8e0a4f4f0add775213cbf9ecfc1a6024cb021bd1ed5f4981a4498f294cca51d3939dfd9e6a1045350ddde7b6d791b4d3b884ee890d4c401ef97b46d1e57d40efe5737248dd0c4cec29c23c787231c4346cab9bb973f140a32abaa0a2bd5c0b91162f8d2a7c9d3347aafc76adbbd90ec5bfe617a3584e94bc31047e3bb6850477219a9"
+
+//const blueButtonPepper = "b8ebdcc47fdd852b8b0201835c6273a9177806e84f2d9dc4f7ecaff08681e86d74195c6aef2db06d3d44c9d0b8f93c3e6c43d90724b605ac12585b9ab5ee9c3f00d5c0d284e6b8e49d502415c601c28930637b58fdca72476e31c22ad0f24ecd761020d6a4bcd471f0db421d21983c0def1b66a49a230f85f93097e9a9a8e0a4f4f0add775213cbf9ecfc1a6024cb021bd1ed5f4981a4498f294cca51d3939dfd9e6a1045350ddde7b6d791b4d3b884ee890d4c401ef97b46d1e57d40efe5737248dd0c4cec29c23c787231c4346cab9bb973f140a32abaa0a2bd5c0b91162f8d2a7c9d3347aafc76adbbd90ec5bfe617a3584e94bc31047e3bb6850477219a9"
 
 type APIClient interface {
 	GetExplanationOfBenefitData(patientID, jobID string) (string, error)
@@ -208,10 +210,13 @@ func GetDefaultParams() (params url.Values) {
 }
 
 func HashHICN(toHash string) (hashedValue string) {
+	blueButtonPepper := os.Getenv("BB_HASH_PEPPER")
+	blueButtonIter := utils.GetEnvInt("BB_HASH_ITER", 1000)
+
 	pepper, err := hex.DecodeString(blueButtonPepper)
 	// not sure how this can happen
 	if err != nil {
 		return ""
 	}
-	return hex.EncodeToString(pbkdf2.Key([]byte(toHash), pepper, 1000, 32, sha256.New))
+	return hex.EncodeToString(pbkdf2.Key([]byte(toHash), pepper, blueButtonIter, 32, sha256.New))
 }
