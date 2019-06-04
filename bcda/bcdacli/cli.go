00c77aaa0a4f9f782330828cc47cb6d941287ad4
David Greene <david@adhocteam.us>
2019-06-04 11:32:34 -0400

    Remove TODO comment

--- bcda/bcdacli/cli.go
+++ bcda/bcdacli/cli.go
@@ -321,8 +321,6 @@ func setUpApp() *cli.App {
 				}
 
 				// Generate new credentials
-				// TODO: GenerateClientCredentials interface to be updated as part of BCDA-1304
-				//       Passing in a ttl of 0 for now, since it is not used.
 				creds, err := auth.GetProvider().GenerateClientCredentials(aco.ClientID)
 				if err != nil {
 					return err
