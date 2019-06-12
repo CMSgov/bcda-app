540afe5e6f3e33fb505564f4e93553b0a0813546
Emily Hart <1923441+embh@users.noreply.github.com>
2019-06-12 09:20:24 -0400

    BCDA-1421: Decryption Utilities fail if path/to/filename.ndjson is passed in via --file (#291)
    * Use os.path.split for files to be decrypted

--- decryption_utils/Python/decrypt.py
+++ decryption_utils/Python/decrypt.py
@@ -82,7 +82,8 @@ def get_private_key(loc):
         return RSA.importKey(fh.read())
 
 def valid_uuid(filename):
-    uuid = filename.split("/")[-1].split(".")[0]
+    path, name = os.path.split(filename)
+    uuid = name.split(".")[0]
     regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
     match = regex.match(uuid)
     return bool(match)
