a14fc959d79a1c4503cd6a71966150379ec61c86
Emily Hart <1923441+embh@users.noreply.github.com>
2019-07-03 16:29:52 -0400

    Create system endpoint

---
+++ ssas/service/admin/api.go
@@ -0,0 +1,41 @@
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/CMSgov/bcda-app/ssas"
+	"net/http"
+)
+
+func createSystem(w http.ResponseWriter, r *http.Request) {
+	type system struct {
+		GroupID    string `json:"group_id"`
+		ClientID   string `json:"client_id"`
+		ClientName string `json:"client_name"`
+		ClientURI  string `json:"client_uri"`
+		Scope      string `json:"scope"`
+		PublicKey  string `json:"public_key"`
+	}
+
+	sys := system{}
+	if err := json.NewDecoder(r.Body).Decode(&sys); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	creds, err := ssas.RegisterSystem(sys.ClientID, sys.ClientName, sys.ClientURI, sys.GroupID, sys.Scope, sys.PublicKey)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not create system. Error: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	credsJSON, err := json.Marshal(creds)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not create system. Error: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, string(credsJSON))
+}
---
+++ ssas/service/admin/api_test.go
@@ -0,0 +1,46 @@
+package admin
+
+import (
+	"encoding/json"
+	"github.com/CMSgov/bcda-app/bcda/database"
+	"github.com/CMSgov/bcda-app/ssas"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type APITestSuite struct {
+	suite.Suite
+	db *gorm.DB
+}
+
+func (s *APITestSuite) SetupSuite() {
+	s.db = database.GetGORMDbConnection()
+}
+
+func (s *APITestSuite) TearDownSuite() {
+	database.Close(s.db)
+}
+
+func (s *APITestSuite) TestCreateSystem() {
+	group := ssas.Group{GroupID: "T00000"}
+	s.db.Save(&group)
+
+	req := httptest.NewRequest("POST", "/auth/system", strings.NewReader(`{"group_id": "T00000", "client_id": "a1b2c3", "client_name": "Test Client", "public_key": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArhxobShmNifzW3xznB+L\nI8+hgaePpSGIFCtFz2IXGU6EMLdeufhADaGPLft9xjwdN1ts276iXQiaChKPA2CK\n/CBpuKcnU3LhU8JEi7u/db7J4lJlh6evjdKVKlMuhPcljnIKAiGcWln3zwYrFCeL\ncN0aTOt4xnQpm8OqHawJ18y0WhsWT+hf1DeBDWvdfRuAPlfuVtl3KkrNYn1yqCgQ\nlT6v/WyzptJhSR1jxdR7XLOhDGTZUzlHXh2bM7sav2n1+sLsuCkzTJqWZ8K7k7cI\nXK354CNpCdyRYUAUvr4rORIAUmcIFjaR3J4y/Dh2JIyDToOHg7vjpCtNnNoS+ON2\nHwIDAQAB\n-----END PUBLIC KEY-----"}`))
+	handler := http.HandlerFunc(createSystem)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	assert.Equal(s.T(), http.StatusCreated, rr.Result().StatusCode)
+	assert.Equal(s.T(), "application/json", rr.Result().Header.Get("Content-Type"))
+	var result map[string]interface{}
+	_ = json.Unmarshal(rr.Body.Bytes(), &result)
+	assert.Equal(s.T(), "a1b2c3", result["client_id"])
+}
+
+func TestAPITestSuite(t *testing.T) {
+	suite.Run(t, new(APITestSuite))
+}
