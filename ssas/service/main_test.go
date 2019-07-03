337c50bf6ed84e13747eae4a76ac828bab7964e2
DeirdreHolub <deirdre@adhocteam.us>
2019-07-03 12:01:40 -0600

    Establish a base code tree for SSAS (#295)
    * tree base with docker and test
    
    * define code tree (see BCDA-1451)

---
+++ ssas/service/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	if s := hello(); s != "hello SSAS" {
+		t.Errorf("expected 'hello SSAS'; got %v", s)
+	}
+}
+
+func TestSSASMain(t *testing.T) {
+	var str bytes.Buffer
+	logger.SetOutput(&str)
+	main()
+	s := str.String()
+	if !strings.Contains(s, "Future home of") {
+		t.Errorf("expected log containing 'Future home of'; got %s", s)
+	}
+}
