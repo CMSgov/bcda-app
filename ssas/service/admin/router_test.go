d186148730ee0578f445aa94767b19bf0586f355
Alex Hovancik <alexhovancik@gmail.com>
2019-07-12 10:56:47 -0500

    BCDA-1144 add post group endpoint to admin routes (#316)
    * add post group endpoint to admin routes
    
    * system to group is now one-to-one
    
    * change sample group to have singular system
    
    * add logging and address review comments
    
    * initialize system models in test

--- ssas/service/admin/router_test.go
+++ ssas/service/admin/router_test.go
@@ -1,20 +1,29 @@
 package admin
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type RouterTestSuite struct {
 	suite.Suite
-	router  http.Handler
+	router http.Handler
 }
 
 func (s *RouterTestSuite) SetupTest() {
 	s.router = NewRouter()
+}
+
+func (s *RouterTestSuite) TestPostGroupRoute() {
+	req := httptest.NewRequest("POST", "/group", nil)
+	rr := httptest.NewRecorder()
+	s.router.ServeHTTP(rr, req)
+	res := rr.Result()
+	assert.Equal(s.T(), http.StatusBadRequest, res.StatusCode)
 }
 
 func (s *RouterTestSuite) TestPostSystemRoute() {
