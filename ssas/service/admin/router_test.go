81c27bdc0fffa71374bf91cd25086062d08763c7
Emily Hart <1923441+embh@users.noreply.github.com>
2019-07-15 16:19:20 -0400

    Create system for non-404 router test

--- ssas/service/admin/router_test.go
+++ ssas/service/admin/router_test.go
@@ -3,8 +3,10 @@ package admin
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
+	"github.com/CMSgov/bcda-app/ssas"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -35,11 +37,20 @@ func (s *RouterTestSuite) TestPostSystem() {
 }
 
 func (s *RouterTestSuite) TestPutSystemCredentials() {
-	req := httptest.NewRequest("PUT", "/system/1/credentials", nil)
+	db := ssas.GetGORMDbConnection()
+	group := ssas.Group{GroupID: "put-system-credentials-test-group"}
+	db.Create(&group)
+	system := ssas.System{GroupID: group.GroupID, ClientID: "put-system-credentials-test-system"}
+	db.Create(&system)
+	systemID := strconv.FormatUint(uint64(system.ID), 10)
+
+	req := httptest.NewRequest("PUT", "/system/"+systemID+"/credentials", nil)
 	rr := httptest.NewRecorder()
 	s.router.ServeHTTP(rr, req)
 	res := rr.Result()
-	assert.Equal(s.T(), http.StatusNotFound, res.StatusCode)
+	assert.Equal(s.T(), http.StatusCreated, res.StatusCode)
+
+	_ = ssas.CleanDatabase(group)
 }
 
 func TestRouterTestSuite(t *testing.T) {
