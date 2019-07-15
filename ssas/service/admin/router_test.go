b4bfc76bd585559a62c49b6e1a64eae513716cc6
Emily Hart <1923441+embh@users.noreply.github.com>
2019-07-15 16:34:01 -0400

    Create system for non-404 router test

--- ssas/service/admin/router_test.go
+++ ssas/service/admin/router_test.go
@@ -3,7 +3,10 @@ package admin
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+
+	"github.com/CMSgov/bcda-app/ssas"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -34,13 +37,21 @@ func (s *RouterTestSuite) TestPostSystemRoute() {
 	assert.Equal(s.T(), http.StatusBadRequest, res.StatusCode)
 }
 
-func (s *RouterTestSuite) TestDeactivateSystemCredentials() {
-	req := httptest.NewRequest("DELETE", "/system/1/credentials", nil)
+func (s *RouterTestSuite) TestDeleteSystemCredentials() {
+	db := ssas.GetGORMDbConnection()
+	group := ssas.Group{GroupID: "delete-system-credentials-test-group"}
+	db.Create(&group)
+	system := ssas.System{GroupID: group.GroupID, ClientID: "delete-system-credentials-test-system"}
+	db.Create(&system)
+	systemID := strconv.FormatUint(uint64(system.ID), 10)
+
+	req := httptest.NewRequest("DELETE", "/system/"+systemID+"/credentials", nil)
 	rr := httptest.NewRecorder()
 	s.router.ServeHTTP(rr, req)
 	res := rr.Result()
-	// TODO Something else here? Not a useful test atm.
-	assert.Equal(s.T(), http.StatusNotFound, res.StatusCode)
+	assert.Equal(s.T(), http.StatusOK, res.StatusCode)
+
+	_ = ssas.CleanDatabase(group)
 }
 
 func TestRouterTestSuite(t *testing.T) {
