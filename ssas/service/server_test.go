751149c95eb0917720f5d20eb58ab61b6314e0d2
Deirdre Holub <deirdre@adhocteam.us>
2019-08-05 03:18:45 -0600

    cleanup server startup

--- ssas/service/server_test.go
+++ ssas/service/server_test.go
@@ -1,10 +1,11 @@
 package service
 
 import (
-	"os"
+	"net/http"
 	"testing"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -14,29 +15,60 @@ const unitSigningKeyPath string = "../../shared_files/ssas/unit_test_private_key
 type ServerTestSuite struct {
 	suite.Suite
 	server *Server
+	info map[string][]string
 }
 
 func (s *ServerTestSuite) SetupSuite() {
-	info := make(map[string][]string)
-	info["public"] = []string{"token", "register"}
-	s.server = NewServer("test-server", ":9999", "9.99.999", info, s.server.newBaseRouter(), true)
+	s.info = make(map[string][]string)
+	s.info["public"] = []string{"token", "register"}
 }
 
-func (s *ServerTestSuite) TestSetSigningKeys() {
-	err := s.server.SetSigningKeys("")
-	assert.NotNil(s.T(), err)
-	assert.Contains(s.T(), err.Error(), "bad signing key", "testing bad key")
+func (s *ServerTestSuite) SetupTest() {
+	s.server = NewServer("test-server", ":9999", "9.99.999", s.info, nil, true, unitSigningKeyPath, 37 * time.Minute)
+}
 
-	err = s.server.SetSigningKeys(unitSigningKeyPath)
+func (s *ServerTestSuite) TestNewServer() {
+	assert.NotNil(s.T(), s.server)
+	assert.NotNil(s.T(), s.server.tokenSigningKey)
+	assert.NotEmpty(s.T(), s.server.name)
+	assert.NotEmpty(s.T(), s.server.port)
+	assert.NotEmpty(s.T(), s.server.version)
+	assert.NotEmpty(s.T(), s.server.info)
+	assert.NotEmpty(s.T(), s.server.router)
+	assert.True(s.T(), s.server.notSecure)
+	assert.NotNil(s.T(), s.server.tokenSigningKey)
+	assert.NotZero(s.T(), s.server.tokenTTL)
+
+	r := chi.NewRouter()
+	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test"))
+	})
+	ts := NewServer("test-server", ":9999", "9.99.999", s.info, r, true, unitSigningKeyPath, 37 * time.Minute)
+	assert.NotEmpty(s.T(), ts.router)
+	routes, err := ts.ListRoutes()
 	assert.Nil(s.T(), err)
-	assert.NotNil(s.T(), s.server.privateSigningKey)
+	assert.NotNil(s.T(), routes)
+	expected := []string{"GET /_health", "GET /_info", "GET /_version", "GET /*/test"}
+	assert.Equal(s.T(), expected, routes)
 }
 
-func (s *ServerTestSuite) TestTokenDurationDefault() {
-	assert.NotEmpty(s.T(), s.server.tokenTTL)
-	assert.Equal(s.T(), s.server.tokenTTL, time.Hour)
+// test Server() ? how????
+
+// test getInfo(), getVersion(), getHealthCheck()
+
+// test NYI()
+
+// test ConnectionClose()
+
+// MintToken(), MintTokenWithDuration(), mintToken()
+
+func (s *ServerTestSuite) TestNewServerWithBadSigningKey() {
+	ts := NewServer("test-server", ":9999", "9.99.999", s.info, nil, true, "", 37 * time.Minute)
+	assert.Nil(s.T(), ts)
 }
 
+// TODO belongs in public and admin server testing
+/*
 func (s *ServerTestSuite) TestTokenDurationOverride() {
 	originalValue := os.Getenv("SSAS_TOKEN_TTL_IN_MINUTES")
 	assert.NotEmpty(s.T(), s.server.tokenTTL)
@@ -54,23 +86,7 @@ func (s *ServerTestSuite) TestTokenDurationEmptyOverride() {
 	s.server.initTokenDuration()
 	assert.Equal(s.T(), time.Hour, s.server.tokenTTL)
 }
-
-func (s *ServerTestSuite) TestUnavailableSigner() {
-	acoUUID := "DBBD1CE1-AE24-435C-807D-ED45953077D3"
-	token, ts, err := s.server.MintToken(CommonClaims{ACOID: acoUUID})
-
-	assert.Nil(s.T(), err)
-	assert.NotNil(s.T(), token)
-	assert.NotNil(s.T(), ts)
-
-	s.server.privateSigningKey = nil
-	defer func() {
-		_ = s.server.SetSigningKeys(unitSigningKeyPath)
-	}()
-	assert.Panics(s.T(), func() {
-		_, _, _ = s.server.MintToken(CommonClaims{ACOID: acoUUID})
-	})
-}
+*/
 
 func TestServerTestSuite(t *testing.T) {
 	suite.Run(t, new(ServerTestSuite))
