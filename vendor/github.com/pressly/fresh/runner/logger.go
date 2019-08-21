eef3a0c468cae8d19bc5ad23b0a03bc08b6524b1
Emily Hart <1923441+embh@users.noreply.github.com>
2019-08-21 10:14:13 -0400

    Add fresh as non-submodule

---
+++ vendor/github.com/pressly/fresh/runner/logger.go
@@ -0,0 +1,33 @@
+package runner
+
+import (
+	"fmt"
+	logPkg "log"
+	"time"
+
+	"github.com/mattn/go-colorable"
+)
+
+type logFunc func(string, ...interface{})
+
+var logger = logPkg.New(colorable.NewColorableStderr(), "", 0)
+
+func newLogFunc(prefix string) func(string, ...interface{}) {
+	color, clear := "", ""
+	if settings.Colors {
+		color = fmt.Sprintf("\033[%sm", logColor(prefix))
+		clear = fmt.Sprintf("\033[%sm", colors["reset"])
+	}
+	prefix = fmt.Sprintf("%-11s", prefix)
+
+	return func(format string, v ...interface{}) {
+		now := time.Now()
+		timeString := fmt.Sprintf("%d:%d:%02d", now.Hour(), now.Minute(), now.Second())
+		format = fmt.Sprintf("%s%s %s |%s %s", color, timeString, prefix, clear, format)
+		logger.Printf(format, v...)
+	}
+}
+
+func fatal(err error) {
+	logger.Fatal(err)
+}
