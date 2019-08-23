46754460c502f5a80a547ca468c710f82c707233
Emily Hart <1923441+embh@users.noreply.github.com>
2019-08-23 17:04:25 -0400

    Script

--- test/smoke_test/ssas_test.sh
+++ test/smoke_test/ssas_test.sh
@@ -1,6 +1,7 @@
+# TODO: Change order, creating ACO before group
 GROUP_ID=$(docker-compose run --rm api sh -c 'tmp/bcda create-group --id="smoke-test-group" --name="Smoke Test Group"' | tail -n1 | tr -d '\r')
 echo "- Group ID: $GROUP_ID"
-ACO_ID=$(docker exec -it bcda-app_api_1 sh -c 'tmp/bcda create-aco --name="Smoke Test ACO" --cms-id=A9996 --group-id="smoke-test-group" --key-file=/go/src/github.com/CMSgov/bcda-app/shared_files/ATO_public.pem' | tail -n1)
+ACO_ID=$(docker exec -it bcda-app_api_1 sh -c 'tmp/bcda create-aco --name="Smoke Test ACO" --cms-id=A9996 --group-id="smoke-test-group"' | tail -n1)
 echo "- ACO ID: $ACO_ID"
 CREDS=($(docker exec -it bcda-app_api_1 sh -c 'tmp/bcda generate-client-credentials --cms-id=A9996' | tail -n2 | tr -d '\r'))
 CLIENT_ID=${CREDS[0]}
