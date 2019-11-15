0b677f34401c673c28ea9e82b861aadc2b4ffeb5
Matt Snook <matt.snook@adhocteam.us>
2019-11-15 15:53:22 -0500

    BCDA-2373: Remove inefficient query.

--- bcda/models/models.go
+++ bcda/models/models.go
@@ -484,13 +484,6 @@ func (cclfBeneficiary *CCLFBeneficiary) GetBlueButtonID(bb client.APIClient) (bl
 	db := database.GetGORMDbConnection()
 	defer db.Close()
 
-	// find another record with this HICN.  If it has a value use it.
-	// Find the first record with a matching HICN and an non empty BlueButtonID
-	db.Where("hicn = ? and blue_button_id <> ''", cclfBeneficiary.HICN).First(&oldRecord)
-	if oldRecord.BlueButtonID != "" {
-		return oldRecord.BlueButtonID, nil
-	}
-
 	// didn't find a local value, need to ask BlueButton
 	hashedHICN := client.HashHICN(cclfBeneficiary.HICN)
 	jsonData, err := bb.GetPatientByHICNHash(hashedHICN)
