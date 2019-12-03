978089650ac12d557c7f879fc56ae0fba7389ada
tbellj <tbell@fearless.tech>
2019-12-03 11:16:57 -0500

    added all test permutations for Patient all endpoint

--- test/smoke_test/bulk_data_requests.sh
+++ test/smoke_test/bulk_data_requests.sh
@@ -11,3 +11,11 @@ echo "Running Coverage"
 go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Coverage
 echo "Running Patient All"
 go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET
+echo "Running Patient, Coverage, EOB (explicitly)"
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Patient,Coverage,ExplanationOfBenefit
+echo "Running Patient, Coverage"
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Patient,Coverage
+echo "Running Patient, EOB"
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Patient,ExplanationOfBenefit
+echo "Running Coverage, EOB"
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Coverage,ExplanationOfBenefit
