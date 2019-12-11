7b823970d06fe9125ee9c5932f1e849fa64f6c91
David Greene <david@adhocteam.us>
2019-12-11 15:14:46 -0500

    Delete migration script

--- db/migrations/20191211-index-cclf-imports.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX idx_cclf_beneficiaries_file_id ON public.cclf_beneficiaries USING btree (file_id);
-CREATE INDEX idx_cclf_beneficiaries_hicn ON public.cclf_beneficiaries USING btree (hicn);
-CREATE INDEX idx_cclf_beneficiaries_mbi ON public.cclf_beneficiaries USING btree (mbi);
