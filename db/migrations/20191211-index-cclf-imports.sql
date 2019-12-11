05925c6474d15954571bf82d6b2bf13813319395
David Greene <david@adhocteam.us>
2019-12-11 10:45:33 -0500

    Add indexes to cclf_beneficiaries

---
+++ db/migrations/20191211-index-cclf-imports.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_cclf_beneficiaries_file_id ON public.cclf_beneficiaries USING btree (file_id);
+CREATE INDEX idx_cclf_beneficiaries_hicn ON public.cclf_beneficiaries USING btree (hicn);
