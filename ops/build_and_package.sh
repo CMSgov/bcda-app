67afde7598d49009336109e539d5b1d33a24e0ea
David Greene <david@adhocteam.us>
2019-12-23 19:51:08 -0500

    Remove static site artifacts

--- ops/build_and_package.sh
+++ ops/build_and_package.sh
@@ -29,12 +29,6 @@ then
   exit 1
 fi
 
-if [ ! -f ../bcda/_site/feed.xml ]
-then
-  echo "Static Site generation must be completed prior to creating package."
-  exit 1
-fi
-
 cd ../bcda
 go clean
 echo "Building bcda binary..." 
