dfd7d0ac672a145742b706a59442ae71165eff5b
tbellj <tbell@fearless.tech>
2020-02-09 15:44:46 -0500

    added more testing around mbi usage

--- bcdaworker/main_test.go
+++ bcdaworker/main_test.go
@@ -258,7 +258,7 @@ func TestWriteEOBDataToFile_BlueButtonIDNotFound(t *testing.T) {
 	defer db.Delete(&cclfFile)
 
 	bbc := testUtils.BlueButtonClient{}
-	bbc.On("GetPatientByIdentifierHash", mock.AnythingOfType("string")).Return("", errors.New("No beneficiary found for HICN"))
+	bbc.On("GetPatientByIdentifierHash", mock.AnythingOfType("string"),mock.AnythingOfType("string")).Return("", errors.New("No beneficiary found for HICN"))
 
 	// clean out the data dir before beginning this test
 	os.RemoveAll(stagingDir)
