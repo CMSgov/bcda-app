3f86c5474e711fb6d8a05746cc8da1ddc89e02f3
Martin Trang <martin.trang@adhocteam.us>
2020-07-24 07:29:15 -0600

    Init work on refactor

---
+++ bcda/models/repository.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+// CCLFFileRepository contains methods need to interact with CCLF files
+type CCLFFileRepository interface {
+	// GetLatestCCLFFile returns the latest CCLF File (most recent timestamp)
+	// that matches the search criteria.
+	// The returned CCLF file will fall between the provided time window.
+	// If any of the time values equals time.Time (default value), then the time value IS NOT used in the filtering.
+	GetLatestCCLFFile(cmsID string, cclfNum int, importStatus string, lowerBound, upperBound time.Time) (*CCLFFile, error)
+}
+
+// CCLFBeneficiaryRepository contains methods need to interact with CCLF Beneficiary data.
+type CCLFBeneficiaryRepository interface {
+}
