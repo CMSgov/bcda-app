b83a9ef4b9740afe24e49c75fee04c15b2c05227
mtrang1263 <martin.trang@adhocteam.us>
2020-09-16 09:23:42 -0600

    BCDA-3551 - Update performance tests commands to use clientID/clientSecret (#547)

--- test/performance_test/performance.go
+++ test/performance_test/performance.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	vegeta "github.com/tsenart/vegeta/lib"
@@ -14,13 +18,18 @@ import (
 
 // might add a with metrics bool option
 var (
-	appTestToken, workerTestToken, apiHost, proto, resourceType, reportFilePath, endpoint string
-	freq, duration                                                                        int
+	appClientID, appClientSecret, workerClientID,
+	workerClientSecret, apiHost, proto,
+	resourceType, reportFilePath, endpoint string
+
+	freq, duration int
 )
 
 func init() {
-	flag.StringVar(&appTestToken, "api_test_token", "", "access token used for api performance testing")
-	flag.StringVar(&workerTestToken, "worker_test_token", "", "access token used for worker performance testing")
+	flag.StringVar(&appClientID, "appClientID", "", "client id for retrieving an access token for api performance testing")
+	flag.StringVar(&appClientSecret, "appClientSecret", "", "client secret for retrieving an access token for api performance testing")
+	flag.StringVar(&workerClientID, "workerClientID", "", "client id for retrieving an access token for worker performance testing")
+	flag.StringVar(&workerClientSecret, "workerClientSecret", "", "client secret for retrieving an access token for worker performance testing")
 	flag.StringVar(&apiHost, "host", "localhost:3000", "host to send requests to")
 	flag.IntVar(&duration, "duration", 60, "seconds: the total time to run the test")
 	flag.IntVar(&freq, "freq", 10, "the number of requests per second")
@@ -40,7 +49,8 @@ func init() {
 }
 
 func main() {
-	if appTestToken != "" {
+	if appClientID != "" && appClientSecret != "" {
+		appTestToken := getAccessToken(appClientID, appClientSecret)
 		targeter := makeTarget(appTestToken)
 		apiResults := runAPITest(targeter)
 		var buf bytes.Buffer
@@ -51,7 +61,8 @@ func main() {
 		writeResults(fmt.Sprintf("%s_%s_api_plot", endpoint, resourceType), buf)
 	}
 
-	if workerTestToken != "" {
+	if workerClientID != "" && workerClientSecret != "" {
+		workerTestToken := getAccessToken(workerClientID, workerClientSecret)
 		targeter := makeTarget(workerTestToken)
 		workerResults := runWorkerTest(targeter)
 		var buf bytes.Buffer
@@ -66,7 +77,7 @@ func main() {
 func makeTarget(accessToken string) vegeta.Targeter {
 	if endpoint == "Group" {
 		groupId := "all"
-		endpoint = fmt.Sprintf("%s/%s",endpoint, groupId)
+		endpoint = fmt.Sprintf("%s/%s", endpoint, groupId)
 	}
 
 	var url string
@@ -80,6 +91,7 @@ func makeTarget(accessToken string) vegeta.Targeter {
 		"Prefer":        {"respond-async"},
 		"Accept":        {"application/fhir+json"},
 		"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
+		"User-Agent":    {"bcda-performance-test"},
 	}
 
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
@@ -99,7 +111,15 @@ func runAPITest(target vegeta.Targeter) *plot.Plot {
 	// 10 request every second for 60 seconds = 600 total calls
 	d := time.Second * time.Duration(duration)
 	rate := vegeta.Rate{Freq: freq, Per: time.Second}
-	plotAttack(p, target, rate, d)
+	var metrics vegeta.Metrics
+	defer func() {
+		// Needed to compute all of the summary metrics
+		metrics.Close()
+		if err := validateMetrics(metrics); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+	plotAttack(p, &metrics, target, rate, d)
 
 	return p
 }
@@ -113,30 +133,81 @@ func runWorkerTest(target vegeta.Targeter) *plot.Plot {
 	// 1 request for 300,000 beneficiaries
 	d := time.Minute
 	rate := vegeta.Rate{Freq: 1, Per: time.Minute}
-	plotAttack(p, target, rate, d)
+	var metrics vegeta.Metrics
+	defer func() {
+		// Needed to compute all of the summary metrics
+		metrics.Close()
+		if err := validateMetrics(metrics); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+	plotAttack(p, &metrics, target, rate, d)
 
 	return p
 }
 
 // need to make rate into some sort of pretty string format
-func plotAttack(p *plot.Plot, t vegeta.Targeter, r vegeta.Rate, du time.Duration) {
+func plotAttack(p *plot.Plot, m *vegeta.Metrics, t vegeta.Targeter, r vegeta.Rate, du time.Duration) {
 	attacker := vegeta.NewAttacker()
 	for results := range attacker.Attack(t, r, du, fmt.Sprintf("%dps:", r.Freq)) {
-		err := p.Add(results)
-		if err != nil {
+		if err := p.Add(results); err != nil {
 			panic(err)
 		}
+		m.Add(results)
 	}
 }
 
 func writeResults(filename string, buf bytes.Buffer) {
+	re := regexp.MustCompile(`[^a-zA-Z0-9\\.\\-]`)
+	clean := re.ReplaceAllString(filename, "-")
 	data := buf.Bytes()
 	if len(data) > 0 {
-		fn := fmt.Sprintf("%s/%s.html", reportFilePath, filename)
+		fn := fmt.Sprintf("%s/%s.html", reportFilePath, clean)
 		fmt.Printf("Writing results: %s\n", fn)
 		err := ioutil.WriteFile(fn, data, 0600)
 		if err != nil {
 			panic(err)
 		}
 	}
+}
+
+func getAccessToken(clientID, clientSecret string) string {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s://%s/auth/token", proto, apiHost), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.SetBasicAuth(clientID, clientSecret)
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	var t map[string]interface{}
+	if err = json.NewDecoder(resp.Body).Decode(&t); err != nil {
+		panic(fmt.Sprintf("unexpected token response format: %s", err.Error()))
+	}
+
+	return t["access_token"].(string)
+}
+
+func validateMetrics(metrics vegeta.Metrics) error {
+	if metrics.Requests == 0 {
+		return nil
+	}
+
+	if len(metrics.Errors) > 0 {
+		return fmt.Errorf("Encountered %v errors", metrics.Errors)
+	}
+
+	if metrics.Success < 1.0 {
+		return fmt.Errorf("Expected success rate of 1.0, received %f",
+			metrics.Success)
+	}
+
+	return nil
 }
