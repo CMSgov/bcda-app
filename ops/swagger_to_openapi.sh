11ae4581f9b5dbf085fee707b9f31913e22cdfb5
Matt Snook <matt.snook@adhocteam.us>
2020-10-02 10:30:05 -0400

    BCDA-3739: Ensure that RPM packaging fails if openapi.json is invalid

--- ops/swagger_to_openapi.sh
+++ ops/swagger_to_openapi.sh
@@ -9,4 +9,7 @@ java -jar -DswaggerUrl=swagger.yaml /swagger-converter/jetty-runner.jar /swagger
 sleep 30
 curl -X POST -d "@swaggerui/swagger.json" localhost:8080/api/convert -H "Content-Type: application/json" -o temp.json
 cat temp.json | jq > swaggerui/openapi.json
-grep "Beneficiary Claims Data API" swaggerui/openapi.json
+
+# Remove openapi.json file if it is invalid
+# Note that build_and_package.sh script validates that openapi.json exists; so RPM packaging will fail if there is an invalid openapi.json
+grep "Beneficiary Claims Data API" swaggerui/openapi.json || rm swaggerui/openapi.json
