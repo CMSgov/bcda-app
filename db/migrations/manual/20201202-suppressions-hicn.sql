e952e23eecec52d39934741a488a6e7f46a15468
mtrang1263 <martin.trang@adhocteam.us>
2020-12-08 09:22:31 -0700

    BCDA-3176 - Adding SQL scripts to cleanup CCLF/suppression data (#596)

---
+++ db/migrations/manual/20201202-suppressions-hicn.sql
@@ -0,0 +1,11 @@
+-- Script will remove suppression entries that contained a HICN value
+BEGIN;
+
+-- Since we only can detect the suppression files to delete by looking at the specific suppresion entry.
+-- We need to store what files we need to remove before we clean the suppresion entries
+CREATE TEMPORARY TABLE suppression_files_to_remove (file_id integer);
+INSERT INTO suppression_files_to_remove SELECT DISTINCT(file_id) from suppressions WHERE LENGTH(hicn) > 0;
+DELETE FROM suppressions WHERE LENGTH(hicn) > 0;
+DELETE FROM suppression_files WHERE id IN (SELECT file_id FROM suppression_files_to_remove);
+
+COMMIT;
