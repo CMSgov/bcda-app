b880d70573e318018399ef829d3ce034e5b9c305
Silvio Luthi <silvio@adhocteam.us>
2020-12-11 11:51:17 -0800

    remove db close err check as its not supported in gorm v2

--- bcda/auth/alpha_test.go
+++ bcda/auth/alpha_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/CMSgov/bcda-app/bcda/models"
 	"github.com/CMSgov/bcda-app/bcda/testUtils"
 	"github.com/dgrijalva/jwt-go"
-	"github.com/jinzhu/gorm"
 	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
 )
 
 var connections = make(map[string]*gorm.DB)
@@ -48,12 +48,9 @@ func (s *AlphaAuthPluginTestSuite) BeforeTest(suiteName, testName string) {
 }
 
 func (s *AlphaAuthPluginTestSuite) AfterTest(suiteName, testName string) {
-	c, ok := connections[testName]
+	_, ok := connections[testName]
 	if !ok {
 		s.FailNow("WTF? no db connection for %s", testName)
-	}
-	if err := c.Close(); err != nil {
-		s.FailNow("error closing db connection for %s because %s", testName, err)
 	}
 }
 
