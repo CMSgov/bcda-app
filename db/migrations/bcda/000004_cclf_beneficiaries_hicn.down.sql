9ec2ccd07c2f5b3f70c68e542c5d8abbd51ee8d2
Katie Campbell Downie <katie.campbell-downie@adhocteam.us>
2021-01-05 12:04:40 -0600

    add sql migration to remove HICN column from suppressions table

--- db/migrations/bcda/000004_cclf_beneficiaries_hicn.down.sql
+++ db/migrations/bcda/000004_cclf_beneficiaries_hicn.down.sql
@@ -2,3 +2,9 @@
 BEGIN;
 ALTER TABLE public.cclf_beneficiaries ADD COLUMN hicn varchar(11) NOT NULL;
 COMMIT;
+
+-- Add HICN column to suppressions
+BEGIN;
+ALTER TABLE public.suppressions ADD COLUMN hicn varchar(11) NOT NULL;
+COMMIT;
+
--- db/migrations/bcda/000004_cclf_beneficiaries_hicn.up.sql
+++ db/migrations/bcda/000004_cclf_beneficiaries_hicn.up.sql
@@ -2,3 +2,9 @@
 BEGIN;
 ALTER TABLE public.cclf_beneficiaries DROP COLUMN IF EXISTS hicn;
 COMMIT;
+
+-- Remove HICN column from suppressions
+BEGIN;
+ALTER TABLE public.suppressions DROP COLUMN IF EXISTS hicn;
+COMMIT;
+
