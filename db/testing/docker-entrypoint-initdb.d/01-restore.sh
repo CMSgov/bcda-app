f70f2eaeb3b5127d5d19f231ab1dc8c3e002e19b
mtrang1263 <martin.trang@adhocteam.us>
2021-01-08 13:18:02 -0700

    BCDA-4119 - Fix GetCCLFBeneficiaries query (#614)

--- db/testing/docker-entrypoint-initdb.d/01-restore.sh
+++ db/testing/docker-entrypoint-initdb.d/01-restore.sh
@@ -2,3 +2,6 @@
 
 # Script used to seed test data within the postgres docker container
 PGPASSWORD=${POSTGRES_PASSWORD}  pg_restore -C -d postgres -v -p 5432 -U postgres --clean --exit-on-error --if-exists "/docker-entrypoint-initdb.d/dump.pgdata"
+# Update the suppression date to guarantee suppression entries satisfy the date requirements.
+# See postgres#GetSuppressedMBIs for more information
+PGPASSWORD=$POSTGRES_PASSWORD psql $POSTGRES_DB postgres -c 'UPDATE suppressions SET effective_date = now() WHERE effective_date = (SELECT max(effective_date) FROM suppressions);'
