0446cc717d1d74e8d6f7f8097b18b6d043bca5ea
Martin Trang <martin.trang@adhocteam.us>
2021-01-22 10:41:50 -0700

    Emit error to stdout

--- bcda/main.go
+++ bcda/main.go
@@ -96,5 +96,8 @@ func main() {
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
+		// Since the logs may be routed to a file, ensure that the error makes it at least once
+		// to stdout
+		fmt.Printf("Error occurred while executing command %s\n", err)
 	}
 }
