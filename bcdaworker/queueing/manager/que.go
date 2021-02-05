b73bcddd0e0e9a49349847c846bda04efe30b671
mtrang1263 <martin.trang@adhocteam.us>
2021-02-05 08:01:45 -0700

    Refactor structure of the bcdaworker (#627)

---
+++ bcdaworker/queueing/manager/que.go
@@ -0,0 +1,170 @@
+package manager
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	goerrors "errors"
+	"fmt"
+	"time"
+
+	"github.com/CMSgov/bcda-app/bcda/database"
+	"github.com/CMSgov/bcda-app/bcda/metrics"
+	"github.com/CMSgov/bcda-app/bcda/models"
+	"github.com/CMSgov/bcda-app/bcda/utils"
+	"github.com/CMSgov/bcda-app/bcdaworker/queueing"
+	"github.com/CMSgov/bcda-app/bcdaworker/repository"
+	"github.com/CMSgov/bcda-app/bcdaworker/worker"
+	"github.com/CMSgov/bcda-app/conf"
+	"github.com/bgentry/que-go"
+	"github.com/jackc/pgx"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+// queue is responsible for retrieving jobs using the que client and
+// transforming and delegating that work to the underlying worker
+type queue struct {
+	// Resources associated with the underlying que client
+	quePool           *que.WorkerPool
+	pool              *pgx.ConnPool
+	healthCheckCancel context.CancelFunc
+
+	worker worker.Worker
+	log    *logrus.Logger
+	queDB  *sql.DB
+
+	cloudWatchEnv string
+}
+
+// StartQue creates a que-go client and begins listening for items
+// It returns immediately since all of the associated workers are started
+// in separate goroutines.
+func StartQue(log *logrus.Logger, queueDatabaseURL string, numWorkers int) *queue {
+	db, err := sql.Open("pgx", queueDatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Allocate the queue in advance to supply the correct
+	// in the workmap
+	q := &queue{
+		worker:        worker.NewWorker(database.GetDbConnection()),
+		log:           log,
+		queDB:         db,
+		cloudWatchEnv: conf.GetEnv("DEPLOYMENT_TARGET"),
+	}
+
+	cfg, err := pgx.ParseURI(queueDatabaseURL)
+	if err != nil {
+		q.log.Fatal(err)
+	}
+
+	q.pool, err = pgx.NewConnPool(pgx.ConnPoolConfig{
+		ConnConfig:   cfg,
+		AfterConnect: que.PrepareStatements,
+	})
+	if err != nil {
+		q.log.Fatal(err)
+	}
+
+	// Ensure that the connections are valid. Needed until we move to pgx v4
+	ctx, cancel := context.WithCancel(context.Background())
+	q.healthCheckCancel = cancel
+	database.StartHealthCheck(ctx, q.pool, 10*time.Second)
+
+	qc := que.NewClient(q.pool)
+	wm := que.WorkMap{
+		queueing.QUE_PROCESS_JOB: q.processJob,
+	}
+
+	q.quePool = que.NewWorkerPool(qc, wm, numWorkers)
+
+	q.quePool.Start()
+
+	return q
+}
+
+// StopQue cleans up any resources created
+func (q *queue) StopQue() {
+	q.healthCheckCancel()
+	if err := q.queDB.Close(); err != nil {
+		q.log.Warnf("Failed to close connection to queue database %s", err)
+	}
+	q.pool.Close()
+	q.quePool.Shutdown()
+}
+
+func (q *queue) processJob(job *que.Job) error {
+	ctx := context.Background()
+	defer q.updateJobQueueCountCloudwatchMetric()
+
+	var jobArgs models.JobEnqueueArgs
+	err := json.Unmarshal(job.Args, &jobArgs)
+	if err != nil {
+		// ACK the job because retrying it won't help us be able to deserialize the data
+		q.log.Warnf("Failed to deserialize job.Args '%s' %s. Removing queuejob from que.", job.Args, err)
+		return nil
+	}
+
+	exportJob, err := q.worker.ValidateJob(ctx, jobArgs)
+	if goerrors.Is(err, worker.ErrParentJobCancelled) {
+		// ACK the job because we do not need to work on queue jobs associated with a cancelled parent job
+		q.log.Warnf("queJob %d associated with a cancelled parent Job %d. Removing queuejob from que.", job.ID, jobArgs.ID)
+		return nil
+	} else if goerrors.Is(err, worker.ErrNoBasePathSet) {
+		// Data is corrupted, we cannot work on this job.
+		q.log.Warnf("Job %d does not contain valid base path. Removing queuejob from que.", jobArgs.ID)
+		return nil
+	} else if goerrors.Is(err, worker.ErrParentJobNotFound) {
+		// Based on the current backoff delay (j.ErrorCount^4 + 3 seconds), this should've given
+		// us plenty of headroom to ensure that the parent job will never be found.
+		maxNotFoundRetries := int32(utils.GetEnvInt("BCDA_WORKER_MAX_JOB_NOT_FOUND_RETRIES", 3))
+		if job.ErrorCount >= maxNotFoundRetries {
+			q.log.Errorf("No job found for ID: %d acoID: %s. Retries exhausted. Removing job from queue.", jobArgs.ID,
+				jobArgs.ACOID)
+			// By returning a nil error response, we're singaling to que-go to remove this job from the jobqueue.
+			return nil
+		}
+
+		q.log.Warnf("No job found for ID: %d acoID: %s. Will retry.", jobArgs.ID, jobArgs.ACOID)
+		return errors.Wrap(repository.ErrJobNotFound, "could not retrieve job from database")
+	} else if err != nil {
+		return errors.Wrap(err, "failed to validate job")
+	}
+
+	if err := q.worker.ProcessJob(ctx, *exportJob, jobArgs); err != nil {
+		return errors.Wrap(err, "failed to process job")
+	}
+
+	return nil
+}
+
+func (q *queue) updateJobQueueCountCloudwatchMetric() {
+
+	// Update the Cloudwatch Metric for job queue count
+	if q.cloudWatchEnv != "" {
+		sampler, err := metrics.NewSampler("BCDA", "Count")
+		if err != nil {
+			fmt.Println("Warning: failed to create new metric sampler...")
+		} else {
+			err := sampler.PutSample("JobQueueCount", q.getQueueJobCount(), []metrics.Dimension{
+				{Name: "Environment", Value: q.cloudWatchEnv},
+			})
+			if err != nil {
+				q.log.Error(err)
+			}
+		}
+	}
+}
+
+func (q *queue) getQueueJobCount() float64 {
+	row := q.queDB.QueryRow(`select count(*) from que_jobs;`)
+
+	var count int
+	if err := row.Scan(&count); err != nil {
+		q.log.Error(err)
+	}
+
+	return float64(count)
+}
---
+++ bcdaworker/queueing/manager/que_test.go
@@ -0,0 +1,155 @@
+package manager
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/CMSgov/bcda-app/bcda/database"
+	"github.com/CMSgov/bcda-app/bcda/models"
+	"github.com/CMSgov/bcda-app/bcda/models/postgres/postgrestest"
+	"github.com/CMSgov/bcda-app/bcda/testUtils"
+	"github.com/CMSgov/bcda-app/bcdaworker/queueing"
+	"github.com/CMSgov/bcda-app/bcdaworker/repository"
+	"github.com/CMSgov/bcda-app/bcdaworker/worker"
+	"github.com/CMSgov/bcda-app/conf"
+	"github.com/bgentry/que-go"
+	"github.com/pborman/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+)
+
+// logHook allows us to retrieve the messages emitted by the logging instance
+var log = logrus.New()
+var logHook = test.NewLocal(log)
+
+// TestProcessJob acts as an end-to-end verification of the process.
+// It uses the postgres/que-go backed implementations
+func TestProcessJob(t *testing.T) {
+	// Reset our environment once we've finished with the test
+	defer func(payload, staging string) {
+		conf.SetEnv(t, "FHIR_PAYLOAD_DIR", payload)
+		conf.SetEnv(t, "FHIR_STAGING_DIR", staging)
+	}(conf.GetEnv("FHIR_PAYLOAD_DIR"), conf.GetEnv("FHIR_STAGING_DIR"))
+
+	defer func(cert, key, ca string) {
+		conf.SetEnv(t, "BB_CLIENT_CERT_FILE", cert)
+		conf.SetEnv(t, "BB_CLIENT_KEY_FILE", key)
+		conf.SetEnv(t, "BB_CLIENT_CA_FILE", ca)
+	}(conf.GetEnv("BB_CLIENT_CERT_FILE"), conf.GetEnv("BB_CLIENT_KEY_FILE"), conf.GetEnv("BB_CLIENT_CA_FILE"))
+
+	conf.SetEnv(t, "BB_CLIENT_CERT_FILE", "../../../shared_files/decrypted/bfd-dev-test-cert.pem")
+	conf.SetEnv(t, "BB_CLIENT_KEY_FILE", "../../../shared_files/decrypted/bfd-dev-test-key.pem")
+	conf.SetEnv(t, "BB_CLIENT_CA_FILE", "../../../shared_files/localhost.crt")
+
+	// Ensure we do not clutter our working directory with any data
+	tempDir, err := ioutil.TempDir("", "*")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	conf.SetEnv(t, "FHIR_PAYLOAD_DIR", tempDir)
+	conf.SetEnv(t, "FHIR_STAGING_DIR", tempDir)
+
+	db := database.GetDbConnection()
+	defer db.Close()
+
+	cmsID := testUtils.RandomHexID()[0:4]
+	aco := models.ACO{UUID: uuid.NewRandom(), CMSID: &cmsID}
+	postgrestest.CreateACO(t, db, aco)
+	job := models.Job{ACOID: aco.UUID, Status: models.JobStatusPending}
+	postgrestest.CreateJobs(t, db, &job)
+
+	defer postgrestest.DeleteACO(t, db, aco.UUID)
+
+	queueURL := conf.GetEnv("QUEUE_DATABASE_URL")
+	q := StartQue(log, queueURL, 1)
+	q.cloudWatchEnv = "dev"
+	defer q.StopQue()
+	// Since the jobArgs does not have any beneIDs, the job should complete almost immediately
+	jobArgs := models.JobEnqueueArgs{ID: int(job.ID), ACOID: cmsID, BBBasePath: uuid.New()}
+
+	enqueuer := queueing.NewEnqueuer(queueURL)
+	assert.NoError(t, enqueuer.AddJob(jobArgs, 1))
+
+	timeout := time.After(10 * time.Second)
+	for {
+		select {
+		case <-timeout:
+			t.Fatal("Job not completed in alloted time.")
+			return
+		default:
+			if postgrestest.GetJobByID(t, db, job.ID).Status == models.JobStatusCompleted {
+				return
+			}
+			log.Infof("Waiting on job to be completed. Current status %s.", job.Status)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+}
+
+func TestProcessJobInvalidArgs(t *testing.T) {
+	job := &que.Job{Args: []byte("{invalid_json")}
+	queue := &queue{log: log}
+	assert.NoError(t, queue.processJob(job),
+		"No error since invalid job data should not be retried")
+	entry := logHook.LastEntry()
+	assert.NotNil(t, entry)
+	assert.Contains(t, entry.Message,
+		fmt.Sprintf("Failed to deserialize job.Args '%s'", job.Args))
+}
+
+func TestProcessJobFailedValidation(t *testing.T) {
+	tests := []struct {
+		name        string
+		validateErr error
+		expectedErr error
+		expLogMsg   string
+	}{
+		{"ParentJobCancelled", worker.ErrParentJobCancelled, nil, `^queJob \d+ associated with a cancelled parent Job`},
+		{"NoBasePath", worker.ErrNoBasePathSet, nil, `^Job \d+ does not contain valid base path`},
+		{"NoParentJob", worker.ErrParentJobNotFound, repository.ErrJobNotFound, `^No job found for ID: \d+ acoID.*Will retry`},
+		{"NoParentJobRetriesExceeded", worker.ErrParentJobNotFound, nil, `No job found for ID: \d+ acoID.*Retries exhausted`},
+		{"OtherError", fmt.Errorf("some other error"), fmt.Errorf("some other error"), ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			worker := &worker.MockWorker{}
+			defer worker.AssertExpectations(t)
+
+			queue := &queue{worker: worker, log: log}
+
+			job := models.Job{ID: uint(rand.Int31())}
+			jobArgs := models.JobEnqueueArgs{ID: int(job.ID), ACOID: uuid.New()}
+
+			var queJob que.Job
+			queJob.Args, err = json.Marshal(jobArgs)
+			assert.NoError(t, err)
+
+			// Set the error count to max to ensure that we've exceeded the retries
+			if tt.name == "NoParentJobRetriesExceeded" {
+				queJob.ErrorCount = rand.Int31()
+			}
+
+			worker.On("ValidateJob", testUtils.CtxMatcher, jobArgs).Return(nil, tt.validateErr)
+
+			err = queue.processJob(&queJob)
+			if tt.expectedErr == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.Contains(t, err.Error(), tt.expectedErr.Error())
+			}
+
+			if tt.expLogMsg != "" {
+				assert.Regexp(t, regexp.MustCompile(tt.expLogMsg), logHook.LastEntry().Message)
+			}
+		})
+	}
+
+}
