4a7934b047a4e69e057965f19d6b00eadd830429
Martin Trang <martin.trang@adhocteam.us>
2021-02-05 08:24:14 -0700

    Fix post-merge issues

--- bcdaworker/worker/worker.go
+++ bcdaworker/worker/worker.go
@@ -269,13 +269,12 @@ func appendErrorToFile(ctx context.Context, fileUUID string,
 	}
 
 	defer utils.CloseFileAndLogError(f)
-
-	ooBytes, err := json.Marshal(oo)
-	if err != nil {
+	if _, err := responseutils.WriteOperationOutcome(f, oo); err != nil {
 		log.Error(err)
 	}
 
-	if _, err = f.WriteString(string(ooBytes) + "\n"); err != nil {
+	// Separate any subsequent error entries
+	if _, err := f.WriteString("\n"); err != nil {
 		log.Error(err)
 	}
 }
