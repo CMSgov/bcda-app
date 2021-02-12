9067b9b03cda739f45ac7df1fef59e2184f0241e
Silvio Luthi <silvio@adhocteam.us>
2021-02-12 09:21:07 -0800

    Put all of migration inside a transaction block for both up and down

--- db/migrations/bcda/000008_remove_cclf_bene_xref_table.down.sql
+++ db/migrations/bcda/000008_remove_cclf_bene_xref_table.down.sql
@@ -1,5 +1,6 @@
--- Name: cclf_beneficiary_xrefs; Type: TABLE; Schema: public; 
 BEGIN;
+
+-- Name: cclf_beneficiary_xrefs; Type: TABLE; Schema: public; 
 CREATE TABLE IF NOT EXISTS public.cclf_beneficiary_xrefs (
     id integer NOT NULL,
     created_at timestamp with time zone,
@@ -12,34 +13,25 @@ CREATE TABLE IF NOT EXISTS public.cclf_beneficiary_xrefs (
     prevs_efct_dt text,
     prevs_obslt_dt text
 );
-COMMIT;
 
 -- Name: cclf_beneficiary_xrefs_id_seq; Type: SEQUENCE; Schema: public; 
-BEGIN;
 CREATE SEQUENCE IF NOT EXISTS public.cclf_beneficiary_xrefs_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-COMMIT;
 
 -- Name: cclf_beneficiary_xrefs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; 
-BEGIN;
 ALTER SEQUENCE public.cclf_beneficiary_xrefs_id_seq OWNED BY public.cclf_beneficiary_xrefs.id;
-COMMIT;
 
 -- Name: cclf_beneficiary_xrefs id; Type: DEFAULT; Schema: public; 
-BEGIN;
 ALTER TABLE ONLY public.cclf_beneficiary_xrefs ALTER COLUMN id SET DEFAULT nextval('public.cclf_beneficiary_xrefs_id_seq'::regclass);
-COMMIT;
 
 -- Name: cclf_beneficiary_xrefs cclf_beneficiary_xrefs_pkey; Type: CONSTRAINT; Schema: public; 
-BEGIN;
 ALTER TABLE ONLY public.cclf_beneficiary_xrefs ADD CONSTRAINT cclf_beneficiary_xrefs_pkey PRIMARY KEY (id);
-COMMIT;
 
 -- Name: idx_cclf_beneficiary_xrefs_deleted_at; Type: INDEX; Schema: public; 
-BEGIN;
 CREATE INDEX IF NOT EXISTS idx_cclf_beneficiary_xrefs_deleted_at ON public.cclf_beneficiary_xrefs USING btree (deleted_at);
+
 COMMIT;
--- db/migrations/bcda/000008_remove_cclf_bene_xref_table.up.sql
+++ db/migrations/bcda/000008_remove_cclf_bene_xref_table.up.sql
@@ -1,7 +1,4 @@
 BEGIN;
 DROP TABLE public.cclf_beneficiary_xrefs CASCADE;
-COMMIT;
-
-BEGIN;
 DROP SEQUENCE IF EXISTS public.cclf_beneficiary_xrefs_id_seq;
 COMMIT;
