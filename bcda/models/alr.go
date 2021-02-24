c4a76eff31066597fdab17b47638bf247044fb7f
Daniel Choi <daniel.choi@adhocteam.us>
2021-02-24 14:40:18 -0500

    Added ALR into models (working progress)

---
+++ bcda/models/alr.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"time"
+)
+
+type Alr struct {
+	ID            uint
+	ACOID         uint // Link to AlrMetaData
+	BeneMBI       string
+	BeneHIC       string
+	BeneFirstName string
+	BeneLastName  string
+	BeneSex       string
+	BeneDOB       time.Time
+	BeneDOD       time.Time
+	Timestamp     time.Time
+	KeyValue      string // This stores all the other "violate" fields
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type AlrMetaData struct {
+	MetaDataID uint // Foreign key
+	ACO        string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+// Thought in progress...
+/* type CSVContents struct {
+	Dataframe dataframe.DataFrame
+} */
