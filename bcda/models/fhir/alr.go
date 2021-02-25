a086d02ded042ec3e90172e9295ee7b2187c9836
Martin Trang <martin.trang@adhocteam.us>
2021-02-25 16:50:56 -0700

    WIP

--- bcda/models/fhir/alr.go
+++ bcda/models/fhir/alr.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/CMSgov/bcda-app/bcda/models"
+	fhircodes "github.com/google/fhir/go/proto/google/fhir/proto/stu3/codes_go_proto"
 	fhirdatatypes "github.com/google/fhir/go/proto/google/fhir/proto/stu3/datatypes_go_proto"
 	fhirmodels "github.com/google/fhir/go/proto/google/fhir/proto/stu3/resources_go_proto"
 )
@@ -12,11 +13,4 @@ func FromALR(alr models.Alr) []*fhirmodels.ContainedResource {
 	return nil
 }
 
-func getPatient(alr models.Alr) *fhirmodels.Patient {
-	p := &fhirmodels.Patient{}
-	p.Meta = &fhirdatatypes.Meta{LastUpdated: &fhirdatatypes.Instant{
-		ValueUs:   alr.Timestamp.UnixNano() / int64(time.Millisecond),
-		Timezone:  time.UTC.String(),
-		Precision: fhirdatatypes.Instant_MILLISECOND}}
-	return nil
-}
+
