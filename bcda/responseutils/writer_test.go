fc892f3f95679cf21afb510d3680fb0f37887784
Martin Trang <martin.trang@adhocteam.us>
2021-02-25 14:41:57 -0700

    Revert basic

--- bcda/responseutils/writer_test.go
+++ bcda/responseutils/writer_test.go
@@ -1,16 +1,13 @@
 package responseutils
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
-	"github.com/CMSgov/bcda-app/bcda/testUtils"
 	"github.com/google/fhir/go/jsonformat"
 	fhircodes "github.com/google/fhir/go/proto/google/fhir/proto/stu3/codes_go_proto"
-	fhirdatatypes "github.com/google/fhir/go/proto/google/fhir/proto/stu3/datatypes_go_proto"
 	fhirmodels "github.com/google/fhir/go/proto/google/fhir/proto/stu3/resources_go_proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -45,6 +42,7 @@ func (s *ResponseUtilsWriterTestSuite) TestCreateOpOutcome() {
 func (s *ResponseUtilsWriterTestSuite) TestWriteError() {
 	oo := CreateOpOutcome(fhircodes.IssueSeverityCode_ERROR, fhircodes.IssueTypeCode_EXCEPTION, RequestErr, "TestCreateOpOutcome")
 	WriteError(oo, s.rr, http.StatusAccepted)
+
 	res, err := s.unmarshaller.Unmarshal(s.rr.Body.Bytes())
 	assert.NoError(s.T(), err)
 	cr := res.(*fhirmodels.ContainedResource)
@@ -95,93 +93,4 @@ func (s *ResponseUtilsWriterTestSuite) TestWriteCapabilityStatement() {
 	assert.Equal(s.T(), cs.Implementation.Url, respCS.Implementation.Url)
 	assert.Equal(s.T(), "3.0.1", respCS.FhirVersion.Value)
 	assert.Equal(s.T(), cs.FhirVersion, respCS.FhirVersion)
-}
-
-func TestBasic(t *testing.T) {
-	// lastUpdatedAt would reflect the timestamp associated with the ALR data.
-	lastUpdatedAt := time.Now().Round(time.Second).UTC()
-	b := &fhirmodels.Basic{}
-	// Code describes the resource that is being described.
-	b.Code = &fhirdatatypes.CodeableConcept{
-		Text: &fhirdatatypes.String{Value: "Assignment List Report"},
-	}
-	b.Meta = &fhirdatatypes.Meta{LastUpdated: &fhirdatatypes.Instant{
-		ValueUs:   lastUpdatedAt.UnixNano() / int64(time.Millisecond),
-		Timezone:  time.UTC.String(),
-		Precision: fhirdatatypes.Instant_MILLISECOND}}
-	b.Extension = []*fhirdatatypes.Extension{
-		// Each field in the ALR data will have a separate extension field.
-		// The URL will be the key, with every entry as a ValueString
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_MBI_ID"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: testUtils.RandomMBI(t)},
-				},
-			},
-		},
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_HIC_NUM"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: "12345678"},
-				},
-			},
-		},
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_1ST_NAME"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: "Jane"},
-				},
-			},
-		},
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_LAST_NAME"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: "Doe"},
-				},
-			},
-		},
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_SEX_CD"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: "Female"},
-				},
-			},
-		},
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_BRTH_DT"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: "1977-05-25"},
-				},
-			},
-		},
-		// An empty field in the ALR data will be represented by an empty string
-		{
-			Url: &fhirdatatypes.Uri{Value: "BENE_DEATH_DT"},
-			Value: &fhirdatatypes.Extension_ValueX{
-				Choice: &fhirdatatypes.Extension_ValueX_StringValue{
-					StringValue: &fhirdatatypes.String{Value: ""},
-				},
-			},
-		},
-	}
-
-	marshaller, err := jsonformat.NewPrettyMarshaller(jsonformat.STU3)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	resource := &fhirmodels.ContainedResource{
-		OneofResource: &fhirmodels.ContainedResource_Basic{Basic: b},
-	}
-
-	result, err := marshaller.MarshalResourceToString(resource)
-
-	assert.NoError(t, err)
-	fmt.Println(result)
 }
