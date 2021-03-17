d1831859cbb61db99e1489bd06a5cee0af2f1e8b
Daniel Choi <daniel.choi@adhocteam.us>
2021-03-17 10:43:04 -0400

    Removing redundant loocation to hcc crosswalk.

--- bcda/models/fhir/alr/hcc.go
+++ bcda/models/fhir/alr/hcc.go
@@ -33,7 +33,6 @@ func init() {
 	// See: ./ops/build_and_package.sh#49
 	paths := []string{"./hcc_crosswalk.tsv",
 		"/etc/sv/api/hcc_crosswalk.tsv",
-		"/go/src/github.com/CMSgov/bcda-app/bcda/models/fhir/alr/hcc_crosswalk.tsv",
 		os.Getenv("GOPATH") + "/src/github.com/CMSgov/bcda-app/bcda/models/fhir/alr/hcc_crosswalk.tsv"}
 
 	var df dataframe.DataFrame
