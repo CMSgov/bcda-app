60a48c5df7d36b24254b94ac2fc08dee233bcc96
Martin Trang <martin.trang@adhocteam.us>
2021-03-17 14:40:00 -0600

    Adding crosswalk file to worker and nfs mounts

--- ops/build_and_package.sh
+++ ops/build_and_package.sh
@@ -46,7 +46,7 @@ go clean
 echo "Building bcda binary..." 
 go build -ldflags "-X github.com/CMSgov/bcda-app/bcda/constants.Version=$VERSION"
 echo "Packaging bcda binary into RPM..."
-fpm -v $VERSION -s dir -t rpm -n bcda bcda=/usr/local/bin/bcda swaggerui=/etc/sv/api models/fhir/alr/hcc_crosswalk.tsv=/etc/sv/api/hcc_crosswalk.tsv
+fpm -v $VERSION -s dir -t rpm -n bcda bcda=/usr/local/bin/bcda swaggerui=/etc/sv/api models/fhir/alr/hcc_crosswalk.tsv=/etc/sv/api/hcc_crosswalk.tsv models/fhir/alr/hcc_crosswalk.tsv=/etc/sv/worker/hcc_crosswalk.tsv models/fhir/alr/hcc_crosswalk.tsv=/etc/sv/nfs/hcc_crosswalk.tsv
 cd ../bcdaworker
 go clean
 echo "Building bcdaworker..."
