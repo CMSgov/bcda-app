3d974934d7342c464cf218463909e7376d1b4fd7
Adam Ayd <aayd@fearless.tech>
2021-03-23 09:13:27 -0400

    Updated db connection in migrations_test.go

--- db/migrations/migrations_test.go
+++ db/migrations/migrations_test.go
@@ -45,7 +45,7 @@ func (s *MigrationTestSuite) SetupSuite() {
 	// postgres://<USER_NAME>:<PASSWORD>@<HOST>: <PORT>/<DB_NAME>
 	re := regexp.MustCompile(`(postgresql\:\/\/\S+\:\S+\@\S+\:\d+\/)(.*)(\?.*)`)
 
-	s.db = database.Connection
+	db := database.Connection
 
 	databaseURL := conf.GetEnv("DATABASE_URL")
 	bcdaDB := fmt.Sprintf("migrate_test_bcda_%d", time.Now().Nanosecond())
