72d6f5cb0754737d2d7f5f7e24e3eca9796021ff
mtrang1263 <martin.trang@adhocteam.us>
2021-04-05 11:40:05 -0600

    Refactor request validation logic to middleware components (#673)

--- bcda/api/requests_test.go
+++ bcda/api/requests_test.go
@@ -3,23 +3,23 @@ package api
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 	"time"
 
+	"github.com/go-testfixtures/testfixtures/v3"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/CMSgov/bcda-app/bcda/auth"
-	"github.com/CMSgov/bcda-app/bcda/database"
+	"github.com/CMSgov/bcda-app/bcda/database/databasetest"
 	"github.com/CMSgov/bcda-app/bcda/models"
 	"github.com/CMSgov/bcda-app/bcda/models/postgres/postgrestest"
 	"github.com/CMSgov/bcda-app/bcda/service"
+	"github.com/CMSgov/bcda-app/bcda/web/middleware"
+	"github.com/CMSgov/bcda-app/bcdaworker/queueing"
 	"github.com/CMSgov/bcda-app/conf"
 
 	"github.com/go-chi/chi"
@@ -43,21 +43,25 @@ func TestRequestsTestSuite(t *testing.T) {
 }
 
 func (s *RequestsTestSuite) SetupSuite() {
-	s.db = database.Connection
+	// See testdata/acos.yml
+	s.acoID = uuid.Parse("ba21d24d-cd96-4d7d-a691-b0e8c88e67a5")
+	s.db, _ = databasetest.CreateDatabase(s.T(), "../../db/migrations/bcda/", true)
+	tf, err := testfixtures.New(
+		testfixtures.Database(s.db),
+		testfixtures.Dialect("postgres"),
+		testfixtures.Directory("testdata/"),
+	)
 
-	// Create an ACO for us to test with
-	s.acoID = uuid.NewUUID()
-	cmsID := "ZYXWV"
-
-	postgrestest.CreateACO(s.T(), s.db, models.ACO{UUID: s.acoID, CMSID: &cmsID})
+	if err != nil {
+		assert.FailNowf(s.T(), "Failed to setup test fixtures", err.Error())
+	}
+	if err := tf.Load(); err != nil {
+		assert.FailNowf(s.T(), "Failed to load test fixtures", err.Error())
+	}
 }
 
 func (s *RequestsTestSuite) SetupTest() {
 	s.runoutEnabledEnvVar = conf.GetEnv("BCDA_ENABLE_RUNOUT")
-}
-
-func (s *RequestsTestSuite) TearDownSuite() {
-	postgrestest.DeleteACO(s.T(), s.db, s.acoID)
 }
 
 func (s *RequestsTestSuite) TearDownTest() {
@@ -87,10 +91,10 @@ func (s *RequestsTestSuite) TestRunoutEnabled() {
 			}
 
 			mockSvc.On("GetQueJobs", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(jobs, tt.errToReturn)
-			h := NewHandler([]string{"ExplanationOfBenefit", "Coverage", "Patient"}, "/v1/fhir")
+			h := newHandler([]string{"ExplanationOfBenefit", "Coverage", "Patient"}, "/v1/fhir", s.db)
 			h.Svc = mockSvc
 
-			req := s.genGroupRequest("runout")
+			req := s.genGroupRequest("runout", middleware.RequestParameters{})
 			w := httptest.NewRecorder()
 			h.BulkGroupRequest(w, req)
 
@@ -110,7 +114,7 @@ func (s *RequestsTestSuite) TestRunoutEnabled() {
 
 func (s *RequestsTestSuite) TestRunoutDisabled() {
 	conf.SetEnv(s.T(), "BCDA_ENABLE_RUNOUT", "false")
-	req := s.genGroupRequest("runout")
+	req := s.genGroupRequest("runout", middleware.RequestParameters{})
 	w := httptest.NewRecorder()
 	h := &Handler{}
 	h.BulkGroupRequest(w, req)
@@ -123,149 +127,56 @@ func (s *RequestsTestSuite) TestRunoutDisabled() {
 	s.Contains(string(body), "Invalid group ID")
 }
 
-func (s *RequestsTestSuite) TestInvalidRequests() {
-	supportedTypes := []string{"ExplanationOfBenefit", "Coverage", "Patient"}
-	h := NewHandler(supportedTypes, "/v1/fhir")
+// TestRequests verifies that we can initiate an export job for all resource types using all the different handlers
+func (s *RequestsTestSuite) TestRequests() {
 
-	type reqParams struct {
-		types        []string
-		since        string
-		outputFormat string
-	}
-	tests := []struct {
-		name             string
-		reqParams        reqParams
-		extraQueryParams map[string]string
-		errMsg           string
-	}{
-		{"_elements query parameter", reqParams{}, map[string]string{"_elements": "blah,blah,blah"}, "Invalid parameter: this server does not support the _elements parameter."},
+	h := newHandler([]string{"ExplanationOfBenefit", "Coverage", "Patient"}, "/v1/fhir", s.db)
 
-		{"Unsupported type", reqParams{types: []string{"Practitioner"}}, nil, "Invalid resource type"},
-		{"Duplicate types", reqParams{types: []string{"Patient", "Patient"}}, nil, "Repeated resource type"},
+	// Use a mock to ensure that this test does not generate artifacts in the queue for other tests
+	enqueuer := &queueing.MockEnqueuer{}
+	enqueuer.On("AddJob", mock.Anything, mock.Anything).Return(nil)
+	h.Enq = enqueuer
 
-		{"Invalid since", reqParams{since: "01-01-2020"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (non-date)", reqParams{since: "invalidDate"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (in the future)", reqParams{since: "2100-01-01T00:00:00Z"}, nil, "Invalid date format supplied in _since parameter. Date must be a date that has already passed"},
-		{"Invalid since (escape character format)", reqParams{since: "2020-03-01T00%3A%2000%3A00.000-00%3A00"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (missing timezone)", reqParams{since: "2020-02-13T08:00:00.000"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (invalid time)", reqParams{since: "2020-02-13T33:00:00.000-05:00"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (invalid date)", reqParams{since: "2020-20-13T08:00:00.000-05:00"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (no time)", reqParams{since: "2020-03-01"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (invalid date no time)", reqParams{since: "2020-04-0"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (junk after time)", reqParams{since: "2020-02-13T08:00:00.000-05:00dfghj"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
-		{"Invalid since (junk before date)", reqParams{since: "erty2020-02-13T08:00:00.000-05:00"}, nil, "Invalid date format supplied in _since parameter.  Date must be in FHIR Instant format."},
+	// Test Group and Patient
+	// Patient, Coverage, and ExplanationOfBenefit
+	// with And without Since parameter
+	resources := []string{"Patient", "ExplanationOfBenefit", "Coverage"}
+	sinces := []time.Time{{}, time.Now().Round(time.Millisecond).Add(-24 * time.Hour)}
+	groupIDs := []string{"all", "runout"}
 
-		{"Invalid output format (text/html)", reqParams{outputFormat: "text/html"}, nil, "_outputFormat parameter must be application/fhir+ndjson, application/ndjson, or ndjson"},
-		{"Invalid output format (application/xml)", reqParams{outputFormat: "application/xml"}, nil, "_outputFormat parameter must be application/fhir+ndjson, application/ndjson, or ndjson"},
-		{"Invalid output format (x-custom)", reqParams{outputFormat: "x-custom"}, nil, "_outputFormat parameter must be application/fhir+ndjson, application/ndjson, or ndjson"},
-		{"Invalid query parameter (extra ?)", reqParams{}, map[string]string{"?_since": "2020-09-13T08:00:00.000-05:00"}, "Invalid parameter: query parameters cannot start with ?"},
-	}
-
-	for _, tt := range tests {
-		u, err := url.Parse("/api/v1")
-		if err != nil {
-			s.Failf("Failed to parse URL %s", err.Error())
-		}
-
-		q := u.Query()
-		if len(tt.reqParams.types) > 0 {
-			q.Set("_type", strings.Join(tt.reqParams.types, ","))
-		}
-		if len(tt.reqParams.since) > 0 {
-			q.Set("_since", tt.reqParams.since)
-		}
-		if len(tt.reqParams.outputFormat) > 0 {
-			q.Set("_outputFormat", tt.reqParams.outputFormat)
-		}
-		for key, value := range tt.extraQueryParams {
-			q.Set(key, value)
-		}
-
-		u.RawQuery = q.Encode()
-		req := httptest.NewRequest("GET", u.String(), nil)
-		rctx := chi.NewRouteContext()
-		rctx.URLParams.Add("groupId", "all")
-		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
-
-		s.T().Run(fmt.Sprintf("%s-group", tt.name), func(t *testing.T) {
-			rr := httptest.NewRecorder()
-			h.BulkGroupRequest(rr, req)
-			assert.Equal(t, http.StatusBadRequest, rr.Code)
-			assert.Contains(t, rr.Body.String(), tt.errMsg)
-		})
-
-		s.T().Run(fmt.Sprintf("%s-patient", tt.name), func(t *testing.T) {
-			rr := httptest.NewRecorder()
-			h.BulkGroupRequest(rr, req)
-			assert.Equal(t, http.StatusBadRequest, rr.Code)
-			assert.Contains(t, rr.Body.String(), tt.errMsg)
-		})
-	}
-}
-
-func (s *RequestsTestSuite) TestCheck429() {
-	validJob := models.Job{RequestURL: "/api/v1/Group/$export", Status: models.JobStatusInProgress, CreatedAt: time.Now()}
-	expiredJob := models.Job{RequestURL: "/api/v1/Group/$export", Status: models.JobStatusInProgress, CreatedAt: time.Now().Add(-2 * GetJobTimeout())}
-	duplicateType := models.Job{RequestURL: "/api/v1/Group/$export?_type=Patient", Status: models.JobStatusInProgress, CreatedAt: time.Now()}
-	tests := []struct {
-		name        string
-		job         models.Job
-		version     string
-		passesCheck bool
-	}{
-		{"Same version", validJob, "v1", false},
-		{"Different version", validJob, "v2", true},
-		{"Invalid job (bad URL)", models.Job{RequestURL: string([]byte{0x7f})}, "", false},
-		{"Invalid job (no version)", models.Job{RequestURL: "/api/Group/$export"}, "", false},
-		{"Same version - expired", expiredJob, "v1", true},
-		{"Duplicate type", duplicateType, "v1", false},
-	}
-
-	for _, tt := range tests {
-		s.T().Run(tt.name, func(t *testing.T) {
-			res, err := check429([]*models.Job{&tt.job}, []string{"Patient"}, tt.version)
-			if tt.passesCheck {
-				assert.NotNil(t, res)
-				assert.NoError(t, err)
-			} else {
-				assert.Nil(t, res)
-				assert.Error(t, err)
+	// Validate group requests
+	for _, resource := range resources {
+		for _, since := range sinces {
+			for _, groupID := range groupIDs {
+				rp := middleware.RequestParameters{
+					Version:       "v1",
+					ResourceTypes: []string{resource},
+					Since:         since,
+				}
+				rr := httptest.NewRecorder()
+				req := s.genGroupRequest(groupID, rp)
+				h.BulkGroupRequest(rr, req)
+				assert.Equal(s.T(), http.StatusAccepted, rr.Code)
 			}
-		})
+		}
+	}
+
+	// Validate patient requests
+	for _, resource := range resources {
+		for _, since := range sinces {
+			rp := middleware.RequestParameters{
+				Version:       "v1",
+				ResourceTypes: []string{resource},
+				Since:         since,
+			}
+			rr := httptest.NewRecorder()
+			h.BulkPatientRequest(rr, s.genPatientRequest(rp))
+			assert.Equal(s.T(), http.StatusAccepted, rr.Code)
+		}
 	}
 }
 
-// TestBulkRequestWithOldJobPaths verifies that bulk requests for a caller with older jobs
-// whose paths are no longer valid will still be accepted.
-func (s *RequestsTestSuite) TestBulkRequestWithOldJobPaths() {
-	// Switch over to validate the 429 behavior
-	dt := "DEPLOYMENT_TARGET"
-	target := conf.GetEnv(dt)
-	defer conf.SetEnv(s.T(), dt, target)
-	conf.SetEnv(s.T(), dt, "prod")
-
-	aco := postgrestest.GetACOByUUID(s.T(), s.db, s.acoID)
-	postgrestest.CreateJobs(s.T(), s.db,
-		&models.Job{ACOID: aco.UUID, RequestURL: "https://api.bcda.cms.gov/api/v1/Coverage/$export", Status: models.JobStatusFailed},
-		&models.Job{ACOID: aco.UUID, RequestURL: "https://api.bcda.cms.gov/api/v1/ExplanationOfBenefit/$export", Status: models.JobStatusExpired},
-		&models.Job{ACOID: aco.UUID, RequestURL: "https://api.bcda.cms.gov/api/v1/SomeCoolUnsupportedResource/$export", Status: models.JobStatusCompleted},
-	)
-	// Create jobs that githave an unsupported path
-
-	resources := []string{"ExplanationOfBenefit", "Coverage", "Patient"}
-	mockSvc := &service.MockService{}
-	mockSvc.On("GetQueJobs", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, nil)
-	h := NewHandler(resources, "/v1/fhir")
-	h.Svc = mockSvc
-
-	req := s.genGroupRequest("all")
-	w := httptest.NewRecorder()
-	h.bulkRequest(resources, w, req, service.RetrieveNewBeneHistData)
-
-	s.Equal(http.StatusAccepted, w.Result().StatusCode)
-}
-
-func (s *RequestsTestSuite) genGroupRequest(groupID string) *http.Request {
+func (s *RequestsTestSuite) genGroupRequest(groupID string, rp middleware.RequestParameters) *http.Request {
 	req := httptest.NewRequest("GET", "http://bcda.cms.gov/api/v1/Group/$export", nil)
 
 	rctx := chi.NewRouteContext()
@@ -274,8 +185,22 @@ func (s *RequestsTestSuite) genGroupRequest(groupID string) *http.Request {
 	aco := postgrestest.GetACOByUUID(s.T(), s.db, s.acoID)
 	ad := auth.AuthData{ACOID: s.acoID.String(), CMSID: *aco.CMSID, TokenID: uuid.NewRandom().String()}
 
-	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
-	req = req.WithContext(context.WithValue(req.Context(), auth.AuthDataContextKey, ad))
+	ctx := context.WithValue(req.Context(), chi.RouteCtxKey, rctx)
+	ctx = context.WithValue(ctx, auth.AuthDataContextKey, ad)
+	ctx = middleware.NewRequestParametersContext(ctx, rp)
+
+	req = req.WithContext(ctx)
 
 	return req
+}
+
+func (s *RequestsTestSuite) genPatientRequest(rp middleware.RequestParameters) *http.Request {
+	req := httptest.NewRequest("GET", "http://bcda.cms.gov/api/v1/Patient/$export", nil)
+	aco := postgrestest.GetACOByUUID(s.T(), s.db, s.acoID)
+	ad := auth.AuthData{ACOID: s.acoID.String(), CMSID: *aco.CMSID, TokenID: uuid.NewRandom().String()}
+
+	ctx := context.WithValue(req.Context(), auth.AuthDataContextKey, ad)
+	ctx = middleware.NewRequestParametersContext(ctx, rp)
+
+	return req.WithContext(ctx)
 }
--- bcda/web/router.go
+++ bcda/web/router.go
@@ -10,6 +10,7 @@ import (
 	"github.com/CMSgov/bcda-app/bcda/logging"
 	"github.com/CMSgov/bcda-app/bcda/monitoring"
 	"github.com/CMSgov/bcda-app/bcda/utils"
+	"github.com/CMSgov/bcda-app/bcda/web/middleware"
 	"github.com/CMSgov/bcda-app/conf"
 
 	"github.com/go-chi/chi"
@@ -23,18 +24,26 @@ var commonAuth = []func(http.Handler) http.Handler{
 func NewAPIRouter() http.Handler {
 	r := chi.NewRouter()
 	m := monitoring.GetMonitor()
-	r.Use(auth.ParseToken, logging.NewStructuredLogger(), SecurityHeader, ConnectionClose)
+	r.Use(auth.ParseToken, logging.NewStructuredLogger(), middleware.SecurityHeader, middleware.ConnectionClose)
 
 	// Serve up the swagger ui folder
 	FileServer(r, "/api/v1/swagger", http.Dir("./swaggerui/v1"))
 
+	var requestValidators = []func(http.Handler) http.Handler{
+		middleware.ValidateRequestURL, middleware.ValidateRequestHeaders,
+	}
+
 	if conf.GetEnv("DEPLOYMENT_TARGET") != "prod" {
 		r.Get("/", userGuideRedirect)
 		r.Get(`/{:(user_guide|encryption|decryption_walkthrough).html}`, userGuideRedirect)
+	} else {
+		// Apply rate limiting on prodcution only
+		requestValidators = append(requestValidators, middleware.CheckConcurrentJobs)
 	}
+
 	r.Route("/api/v1", func(r chi.Router) {
-		r.With(append(commonAuth, ValidateBulkRequestHeaders)...).Get(m.WrapHandler("/Patient/$export", v1.BulkPatientRequest))
-		r.With(append(commonAuth, ValidateBulkRequestHeaders)...).Get(m.WrapHandler("/Group/{groupId}/$export", v1.BulkGroupRequest))
+		r.With(append(commonAuth, requestValidators...)...).Get(m.WrapHandler("/Patient/$export", v1.BulkPatientRequest))
+		r.With(append(commonAuth, requestValidators...)...).Get(m.WrapHandler("/Group/{groupId}/$export", v1.BulkGroupRequest))
 		r.With(append(commonAuth, auth.RequireTokenJobMatch)...).Get(m.WrapHandler("/jobs/{jobID}", v1.JobStatus))
 		r.With(append(commonAuth, auth.RequireTokenJobMatch)...).Delete(m.WrapHandler("/jobs/{jobID}", v1.DeleteJob))
 		r.Get(m.WrapHandler("/metadata", v1.Metadata))
@@ -43,8 +52,8 @@ func NewAPIRouter() http.Handler {
 	if utils.GetEnvBool("VERSION_2_ENDPOINT_ACTIVE", true) {
 		FileServer(r, "/api/v2/swagger", http.Dir("./swaggerui/v2"))
 		r.Route("/api/v2", func(r chi.Router) {
-			r.With(append(commonAuth, ValidateBulkRequestHeaders)...).Get(m.WrapHandler("/Patient/$export", v2.BulkPatientRequest))
-			r.With(append(commonAuth, ValidateBulkRequestHeaders)...).Get(m.WrapHandler("/Group/{groupId}/$export", v2.BulkGroupRequest))
+			r.With(append(commonAuth, requestValidators...)...).Get(m.WrapHandler("/Patient/$export", v2.BulkPatientRequest))
+			r.With(append(commonAuth, requestValidators...)...).Get(m.WrapHandler("/Group/{groupId}/$export", v2.BulkGroupRequest))
 			r.Get(m.WrapHandler("/metadata", v2.Metadata))
 		})
 	}
@@ -56,13 +65,13 @@ func NewAPIRouter() http.Handler {
 }
 
 func NewAuthRouter() http.Handler {
-	return auth.NewAuthRouter(logging.NewStructuredLogger(), SecurityHeader, ConnectionClose)
+	return auth.NewAuthRouter(logging.NewStructuredLogger(), middleware.SecurityHeader, middleware.ConnectionClose)
 }
 
 func NewDataRouter() http.Handler {
 	r := chi.NewRouter()
 	m := monitoring.GetMonitor()
-	r.Use(auth.ParseToken, logging.NewStructuredLogger(), SecurityHeader, ConnectionClose)
+	r.Use(auth.ParseToken, logging.NewStructuredLogger(), middleware.SecurityHeader, middleware.ConnectionClose)
 	r.With(append(commonAuth, auth.RequireTokenJobMatch)...).
 		Get(m.WrapHandler("/data/{jobID}/{fileName}", v1.ServeData))
 	return r
@@ -71,7 +80,7 @@ func NewDataRouter() http.Handler {
 func NewHTTPRouter() http.Handler {
 	r := chi.NewRouter()
 	m := monitoring.GetMonitor()
-	r.Use(ConnectionClose)
+	r.Use(middleware.ConnectionClose)
 	r.With(logging.NewStructuredLogger()).Get(m.WrapHandler("/*", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		url := "https://" + req.Host + req.URL.String()
 		http.Redirect(w, req, url, http.StatusMovedPermanently)
