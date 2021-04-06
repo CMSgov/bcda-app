ca250617c5c2b3f906e0adbdcca3bfdea1161043
Silvio Luthi <silvio@adhocteam.us>
2021-04-05 17:31:05 -0700

    fix watch script by separating the command out

--- watch.sh
+++ watch.sh
@@ -9,12 +9,13 @@ export ENTR_INOTIFY_WORKAROUND=true
 
 # Watch all go extension files with entr and execute go run so it will recompile and 
 # start the docker service up again if changes are detected.
+entr_cmd="go install -v && ${@:2}"
 if [ "$1" == "api" ]; then
     echo "Starting bcda api entr watcher..."
-    find . ../bcdaworker -name '*.go' | entr -nr bash -c "go install -v && ${@:2}"
+    find . ../bcdaworker -name '*.go' | entr -nrs "$entr_cmd"
 fi
 
 if [ "$1" == "worker" ]; then
     echo "Starting bcda worker entr watcher..."
-    find . ../bcda -name '*.go' | entr -nr bash -c "go install -v && ${@:2}"
+    find . ../bcda -name '*.go' | entr -nrs "$entr_cmd"
 fi
