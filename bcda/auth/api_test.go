664244e5d1b77edef7d0ecc2ffd364151a0c5a84
Martin Trang <martin.trang@adhocteam.us>
2021-04-16 12:07:04 -0600

    Change args

--- bcda/auth/api_test.go
+++ bcda/auth/api_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/dgrijalva/jwt-go"
@@ -61,16 +62,16 @@ func (s *AuthAPITestSuite) SetupTest() {
 }
 
 func (s *AuthAPITestSuite) TestNoMatch() {
-	clientID, clientSecret, accessToken := uuid.New(), uuid.New(), uuid.New()
+	_, _, accessToken := uuid.New(), uuid.New(), uuid.New()
 	mock := &auth.MockProvider{}
 	// MakeAccessToken does not match on the auth.Credentials (ClientID is not set properly)
-	mock.On("MakeAccessToken", auth.Credentials{ClientID: uuid.New(), ClientSecret: clientSecret}).
+	mock.On("MakeAccessToken", auth.Credentials{ClientID: "clientA", ClientSecret: "clientSecret"}).
 		Return(accessToken, nil)
 	auth.SetMockProvider(s.T(), mock)
 	s.rr = httptest.NewRecorder()
 
 	req := httptest.NewRequest("POST", "/auth/token", nil)
-	req.SetBasicAuth(clientID, clientSecret)
+	req.SetBasicAuth("clientB", "clientSecret")
 	req.Header.Add("Accept", "application/json")
 	handler := http.HandlerFunc(auth.GetAuthToken)
 	handler.ServeHTTP(s.rr, req)
@@ -79,15 +80,15 @@ func (s *AuthAPITestSuite) TestNoMatch() {
 
 func (s *AuthAPITestSuite) TestNotInvoked() {
 	clientID, clientSecret, accessToken := uuid.New(), uuid.New(), uuid.New()
-	mock := &auth.MockProvider{}
-	mock.On("MakeAccessToken", auth.Credentials{ClientID: clientID, ClientSecret: clientSecret}).
+	mp := &auth.MockProvider{}
+	mp.On("MakeAccessToken", auth.Credentials{ClientID: clientID, ClientSecret: clientSecret}).
 		Return(accessToken, nil)
 
 	// Method below is not called
-	mock.On("MakeAccessToken", auth.Credentials{ClientID: "not_a_client", ClientSecret: "not_a_secret"}).
+	mp.On("MakeAccessToken", auth.Credentials{ClientID: "not_a_client", ClientSecret: "not_a_secret"}).
 		Return("", errors.New("some auth error"))
 
-	auth.SetMockProvider(s.T(), mock)
+	auth.SetMockProvider(s.T(), mp)
 	s.rr = httptest.NewRecorder()
 
 	req := httptest.NewRequest("POST", "/auth/token", nil)
@@ -97,7 +98,7 @@ func (s *AuthAPITestSuite) TestNotInvoked() {
 	handler.ServeHTTP(s.rr, req)
 	assert.Equal(s.T(), http.StatusOK, s.rr.Code)
 
-	mock.AssertExpectations(s.T())
+	mp.AssertExpectations(s.T())
 }
 
 func (s *AuthAPITestSuite) TestDoesNotMatchFunctionArgs() {
@@ -133,14 +134,15 @@ func (s *AuthAPITestSuite) TestNotMatchReturn() {
 	assert.Equal(s.T(), http.StatusOK, s.rr.Code)
 }
 
-func (s *AuthAPITestSuite) TestArgumentCapture() {
+func (s *AuthAPITestSuite) TestArgumentValidator() {
 	clientID, clientSecret, accessToken := uuid.New(), uuid.New(), uuid.New()
 	mp := &auth.MockProvider{}
 	mp.On("MakeAccessToken",
-		mock.MatchedBy(func(creds auth.Credentials) bool{
+		mock.MatchedBy(func(creds auth.Credentials) bool {
 			fmt.Printf("%+v\n", creds)
 			return true
-		})).
+		},
+		)).
 		Return(accessToken, nil)
 	auth.SetMockProvider(s.T(), mp)
 	s.rr = httptest.NewRecorder()
@@ -153,14 +155,17 @@ func (s *AuthAPITestSuite) TestArgumentCapture() {
 	assert.Equal(s.T(), http.StatusOK, s.rr.Code)
 }
 
-func (s *AuthAPITestSuite) TestArgumentValidator() {
+func (s *AuthAPITestSuite) TestArgumentCapture() {
 	clientID, clientSecret, _ := uuid.New(), uuid.New(), uuid.New()
 	mp := &auth.MockProvider{}
-	call := mp.On("MakeAccessToken", auth.Credentials{ClientID: clientID, ClientSecret: clientSecret})
+	call := mp.On("MakeAccessToken", mock.Anything)
+
+	count := 0
 	call.Run(func(args mock.Arguments) {
+		count++
 		fmt.Printf("%v %v\n", args.Get(0), reflect.TypeOf(args.Get(0)))
-		creds := args.Get(0).(auth.Credentials)
-		call.Return(creds.ClientID, nil)
+		// args.Get(0).(auth.Credentials)
+		call.Return(strconv.Itoa(count), nil)
 	})
 	auth.SetMockProvider(s.T(), mp)
 	s.rr = httptest.NewRecorder()
@@ -171,6 +176,12 @@ func (s *AuthAPITestSuite) TestArgumentValidator() {
 	handler := http.HandlerFunc(auth.GetAuthToken)
 	handler.ServeHTTP(s.rr, req)
 	assert.Equal(s.T(), http.StatusOK, s.rr.Code)
+	fmt.Println(string(s.rr.Body.Bytes()))
+
+	s.rr = httptest.NewRecorder()
+	handler.ServeHTTP(s.rr, req)
+	assert.Equal(s.T(), http.StatusOK, s.rr.Code)
+	fmt.Println(string(s.rr.Body.Bytes()))
 }
 
 func (s *AuthAPITestSuite) TestAuthToken() {
