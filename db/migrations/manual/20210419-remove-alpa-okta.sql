5a73363966e740aafb8d3fd7a27b74eded5f2b20
Daniel Choi <daniel.choi@adhocteam.us>
2021-04-19 17:53:38 -0400

    additional comments.

--- db/migrations/manual/20210419-remove-alpa-okta.sql
+++ db/migrations/manual/20210419-remove-alpa-okta.sql
@@ -1,4 +1,4 @@
--- This one-time migration is to remove alpha from DB
--- This is to be run once post-deployment
+-- This one-time migration is to remove alpha from our DB
+-- This is to be run once post-deployment, and run smoke-test one more time
 
 ALTER TABLE acos DROP COLUMN alpha_secret;
