82978e0912740b9e0170d0134ef56b67861f7776
Silvio Luthi <silvio@adhocteam.us>
2021-05-11 16:56:07 -0700

    Create new middleware that checks whether an ACO is marked as enabled or disabled

--- bcda/web/middleware/middleware.go
+++ bcda/web/middleware/middleware.go
@@ -1,9 +1,15 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/CMSgov/bcda-app/bcda/auth"
+	"github.com/CMSgov/bcda-app/bcda/responseutils"
+	"github.com/CMSgov/bcda-app/bcda/service"
 	"github.com/CMSgov/bcda-app/bcda/servicemux"
+	"github.com/CMSgov/bcda-app/log"
+	fhircodes "github.com/google/fhir/go/proto/google/fhir/proto/stu3/codes_go_proto"
 )
 
 func ConnectionClose(next http.Handler) http.Handler {
@@ -20,6 +26,29 @@ func SecurityHeader(next http.Handler) http.Handler {
 			w.Header().Set("Cache-Control", "no-cache; no-store; must-revalidate; max-age=0")
 			w.Header().Set("Pragma", "no-cache")
 			w.Header().Set("X-Content-Type-Options", "nosniff")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func IsACOEnabled(next http.Handler) http.Handler {
+	cfg, err := service.LoadConfig()
+	if err != nil {
+		panic(fmt.Errorf("could not load service config file: %w", err))
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ad, ok := r.Context().Value(auth.AuthDataContextKey).(auth.AuthData)
+		if !ok {
+			panic("AuthData should be set before calling this handler")
+		}
+
+		if cfg.IsACODisabled(ad.CMSID) {
+			log.API.Error(fmt.Sprintf("failed to complete request, CMSID %s is not enabled", ad.CMSID))
+			oo := responseutils.CreateOpOutcome(fhircodes.IssueSeverityCode_ERROR, fhircodes.IssueTypeCode_EXCEPTION,
+				responseutils.InternalErr, "")
+			responseutils.WriteError(oo, w, http.StatusUnauthorized)
+			return
 		}
 		next.ServeHTTP(w, r)
 	})
