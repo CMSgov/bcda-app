be5056a372d78c259c669f8ea3cabe59fd673f74
Daniel Choi <daniel.choi@adhocteam.us>
2021-05-18 23:24:02 -0400

    Work in Progress

--- bcdaworker/worker/alr.go
+++ bcdaworker/worker/alr.go
@@ -31,8 +31,7 @@ import (
 type AlrWorker struct {
 	*postgres.AlrRepository
 	repository.Repository
-	StagingDir     string `conf:"FHIR_STAGING_DIR"`
-	ndjsonFilename string
+	StagingDir string `conf:"FHIR_STAGING_DIR"`
 }
 
 //data is used to send FHIR data to a go routine to write to disk
@@ -40,6 +39,8 @@ type data struct {
 	patient      *resources_go_proto.Patient
 	observations []*resources_go_proto.Observation
 }
+
+var resources = [...]string{"patient", "observations"}
 
 /******************************************************************************
 	Functions
@@ -52,12 +53,11 @@ func NewAlrWorker(db *sql.DB) AlrWorker {
 
 	// embed data struct that has method GetAlr
 	worker := AlrWorker{
-		AlrRepository:  alrR,
-		Repository:     workerpg.NewRepository(db),
-		ndjsonFilename: "", // Filled in later
+		AlrRepository: alrR,
+		Repository:    workerpg.NewRepository(db),
 	}
 
-	err := conf.Checkout(&worker) // worker is already a reference, no & needed
+	err := conf.Checkout(&worker)
 	if err != nil {
 		logrus.Fatal("Could not get data from conf for ALR.", err)
 	}
@@ -65,8 +65,16 @@ func NewAlrWorker(db *sql.DB) AlrWorker {
 	return worker
 }
 
-func goWriter(c chan data, w *bufio.Writer,
-	marshaller *jsonformat.Marshaller, result chan error) {
+func goWriter(ctx context.Context, a *AlrWorker, c chan data, fileMap map[string]*os.File,
+	marshaller *jsonformat.Marshaller, result chan error, resourceTypes []string, id uint) {
+
+	writerPool := make([]*bufio.Writer, len(fileMap))
+
+	for _, file := range fileMap {
+		w := bufio.NewWriter(file)
+		writerPool = append(writerPool, w)
+		defer utils.CloseFileAndLogError(file)
+	}
 
 	for i := range c {
 		// marshall
@@ -90,15 +98,31 @@ func goWriter(c chan data, w *bufio.Writer,
 
 		observation := strings.Join(observations, "\n")
 
+		alrResources := []string{patients, observation}
+
 		// IO operation
-		_, err = w.WriteString(patients + observation)
-		if err != nil {
-			result <- err
-			return
+		for n, resource := range alrResources {
+
+			_, err = writerPool[n].WriteString(resource)
+			if err != nil {
+				result <- err
+				return
+			}
+			err = writerPool[n].Flush()
+			if err != nil {
+				result <- err
+				return
+			}
+
 		}
-		err = w.Flush()
-		if err != nil {
-			result <- err
+	}
+
+	// update the jobs keys
+	for resource, path := range fileMap {
+		filename := filepath.Base(path.Name())
+		jk := models.JobKey{JobID: id, FileName: filename, ResourceType: resource}
+		if err := a.Repository.CreateJobKey(ctx, jk); err != nil {
+			result <- fmt.Errorf("failed to create job key: %w", err)
 			return
 		}
 	}
@@ -139,15 +163,25 @@ func (a *AlrWorker) ProcessAlrJob(
 		return err
 	}
 
-	a.ndjsonFilename = uuid.New()
-	f, err := os.Create(fmt.Sprintf("%s/%d/%s.ndjson", a.StagingDir,
-		id, a.ndjsonFilename))
-	if err != nil {
-		logrus.Error(err)
-		return err
+	// Get the number FHIR resource types we are using for ALR
+	// This is temporary until the resource types become more permanent
+	fieldNum := len(resources)
+	files := make(map[string]*os.File, fieldNum)
+
+	for i := 0; i < fieldNum; i++ {
+
+		ndjsonFilename := uuid.New()
+		f, err := os.Create(fmt.Sprintf("%s/%d/%s.ndjson", a.StagingDir,
+			id, ndjsonFilename))
+
+		files[resources[i]] = f
+
+		if err != nil {
+			logrus.Error(err)
+			return err
+		}
+
 	}
-	w := bufio.NewWriter(f)
-	defer utils.CloseFileAndLogError(f)
 
 	// Serialize data into JSON
 	marshaller, err := jsonformat.NewMarshaller(false, "", "", jsonformat.STU3)
@@ -164,7 +198,7 @@ func (a *AlrWorker) ProcessAlrJob(
 	// A go routine that will streamed data to write to disk.
 	// Reason for a go routine is to not block when writing, since disk writing is
 	// generally slower than memory access. We are streaming to keep mem lower.
-	go goWriter(c, w, marshaller, result)
+	go goWriter(ctx, a, c, files, marshaller, result, resources[:], id)
 
 	// Marshall into JSON and send it over the channel
 	for i := range alrModels {
@@ -180,16 +214,13 @@ func (a *AlrWorker) ProcessAlrJob(
 		return fmt.Errorf("failed to write data: %w", err)
 	}
 
-	fileName := filepath.Base(f.Name())
 	// If we did not have an ALR data to write, we'll write a specific file name that indicates that the
 	// there is no data associated with this job.
 	if len(alrModels) == 0 {
-		fileName = models.BlankFileName
-	}
-
-	jk := models.JobKey{JobID: id, FileName: fileName, ResourceType: "ALR"}
-	if err := a.Repository.CreateJobKey(ctx, jk); err != nil {
-		return fmt.Errorf("failed to create job key: %w", err)
+		jk := models.JobKey{JobID: id, FileName: models.BlankFileName, ResourceType: "ALR"}
+		if err := a.Repository.CreateJobKey(ctx, jk); err != nil {
+			return fmt.Errorf("failed to create job key: %w", err)
+		}
 	}
 
 	return nil
