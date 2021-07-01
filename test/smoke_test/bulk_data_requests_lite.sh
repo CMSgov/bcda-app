244977f861a064f868a1bb15b7f5318061d80114
Daniel Choi <daniel.choi@adhocteam.us>
2021-07-01 10:16:40 -0400

    More parallelization

--- test/smoke_test/bulk_data_requests_lite.sh
+++ test/smoke_test/bulk_data_requests_lite.sh
@@ -3,9 +3,22 @@
 # This script is intended to be run from within the Docker "smoke_test" container
 #
 set -e
-echo "Running Patient All"
-go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Patient
-echo "Running Group All"
-go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Group/all
-echo "Running Group Runout (EOB resource)"
-go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Group/runout -resourceType=ExplanationOfBenefit
+
+pids=()
+
+echo "Running Patient All" && \
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Patient &
+pids+=($!)
+
+echo "Running Group All" && \
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Group/all &
+pids+=($!)
+
+echo "Running Group Runout (EOB resource)" && \
+go run bcda_client.go -host=api:3000 -clientID=$CLIENT_ID -clientSecret=$CLIENT_SECRET -endpoint=Group/runout -resourceType=ExplanationOfBenefit &
+pids+=($!)
+
+
+for pid in "${pids[@]}"; do
+   wait "$pid"
+done
