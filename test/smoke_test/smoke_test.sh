e7b2c72f3b45a3585510b0e90346f07dc06ee0f9
Daniel Choi <daniel.choi@adhocteam.us>
2021-07-02 01:10:50 -0400

    Fixed issue with sh script error

--- test/smoke_test/smoke_test.sh
+++ test/smoke_test/smoke_test.sh
@@ -27,6 +27,6 @@ do
         if [ ${CMS_ID} = "A9996" ]; then
                 testFile=bulk_data_requests.sh
         fi
-        docker-compose -f docker-compose.test.yml run --rm -e CLIENT_ID=${CLIENT_ID} -e CLIENT_SECRET=${CLIENT_SECRET} -w /go/src/github.com/CMSgov/bcda-app/test/smoke_test tests sh ${testFile}
+        docker-compose -f docker-compose.test.yml run --rm -e CLIENT_ID=${CLIENT_ID} -e CLIENT_SECRET=${CLIENT_SECRET} -w /go/src/github.com/CMSgov/bcda-app/test/smoke_test tests bash ${testFile}
 done
 
