a689275fbed4f1e2c24b8bf1e1b1faf3a3cd50bb
Katie Campbell Downie <katie.campbell-downie@adhocteam.us>
2021-07-06 10:52:55 -0500

    covidEpisode r4 mapping to episodeOfCare

--- bcda/models/fhir/alr/v2/covidepisode.go
+++ bcda/models/fhir/alr/v2/covidepisode.go
@@ -1,7 +1,6 @@
 package v2
 
 import (
-	"fmt"
 	"regexp"
 	"time"
 
@@ -14,10 +13,9 @@ import (
 var (
 	admissionDt = regexp.MustCompile(`^(ADMISSION_DT)$`)
 	dischargeDt = regexp.MustCompile(`^(DISCHARGE_DT)$`)
-	covidDates  = regexp.MustCompile(`^((ADMISSION_DT)|(DISCHARGE_DT))$`)
-
-	covidEpisodeP = regexp.MustCompile(``)
-	covidMonth    = regexp.MustCompile(`^(COVID19_MONTH(0[1-9]|1[0-2])$`)
+	diagnosis   = regexp.MustCompile(`^(U071)|(B9729)$`)
+	episode     = regexp.MustCompile(`^(COVID19_EPISODE)$`)
+	month       = regexp.MustCompile(`^(COVID19_MONTH(0[1-9]|1[0-2])$`)
 )
 
 func covidEpisode(mbi string, keyValue []utils.KvPair, lastUpdated time.Time) *r4Models.EpisodeOfCare {
@@ -46,23 +44,34 @@ func covidEpisode(mbi string, keyValue []utils.KvPair, lastUpdated time.Time) *r
 		}
 
 		switch {
-		// admission and discharge dates map to period start and end
-		case covidDates.MatchString(kv.Key):
-			timestamp, err := time.Parse("2006-01-02T15:04:05.000-07:00", kv.Value)
+		// admission date maps to period start
+		case admissionDt.MatchString(kv.Key):
+			fhirTimestamp, err := stringToFhirDate(kv.Value)
 			if err != nil {
 				continue
 			}
-			fhirTimestamp := &r4Datatypes.DateTime{
-				ValueUs:   timestamp.UnixNano() / int64(time.Microsecond),
-				Precision: r4Datatypes.DateTime_DAY,
+			covidEpisode.Period.Start = fhirTimestamp
+
+		// discharge date maps to period end
+		case dischargeDt.MatchString(kv.Key):
+			fhirTimestamp, err := stringToFhirDate(kv.Value)
+			if err != nil {
+				continue
+			}
+			covidEpisode.Period.End = fhirTimestamp
+
+		// one of 12 flags corresponding to a calendar month
+		case month.MatchString(kv.Key):
+			ext := &r4Datatypes.Extension{}
+			ext.Url = &r4Datatypes.Uri{Value: kv.Key}
+			ext.Value = &r4Datatypes.Extension_ValueX{
+				Choice: &r4Datatypes.Extension_ValueX_StringValue{
+					StringValue: &r4Datatypes.String{Value: kv.Value}, // does this need to be an int?
+				},
 			}
 
-			if admissionDt.MatchString(kv.Key) {
-				covidEpisode.Period.Start = fhirTimestamp
-			} else if dischargeDt.MatchString(kv.Key) {
-				covidEpisode.Period.End = fhirTimestamp
-			}
-		case covidMonth.MatchString(kv.Key):
+		// covid episode extension
+		case episode.MatchString(kv.Key):
 			ext := &r4Datatypes.Extension{}
 			ext.Url = &r4Datatypes.Uri{Value: kv.Key}
 			ext.Value = &r4Datatypes.Extension_ValueX{
@@ -70,39 +79,37 @@ func covidEpisode(mbi string, keyValue []utils.KvPair, lastUpdated time.Time) *r
 					StringValue: &r4Datatypes.String{Value: kv.Value},
 				},
 			}
-		case covidEpisodeP.MatchString(kv.Key):
-			ext := &r4Datatypes.Extension{}
-			ext.Url = &r4Datatypes.Uri{Value: kv.Key}
-			ext.Value = &r4Datatypes.Extension_ValueX{
-				Choice: &r4Datatypes.Extension_ValueX_StringValue{
-					StringValue: &r4Datatypes.String{Value: kv.Value},
+		// one of two diagnosis codes (B9729 or U071)
+		case diagnosis.MatchString(kv.Key):
+			episodeDiagnosis := []*r4Models.EpisodeOfCare_Diagnosis{}
+			diagnosis := &r4Models.EpisodeOfCare_Diagnosis{
+				Condition: &r4Datatypes.Reference{
+					Identifier: &r4Datatypes.Identifier{
+						System: &r4Datatypes.Uri{
+							Value: "http://hl7.org/fhir/sid/icd-10",
+						},
+						Value: &r4Datatypes.String{
+							Value: kv.Value,
+						},
+					},
 				},
 			}
-			case covidDiagnosis.MatchString(kv.Key)
+			episodeDiagnosis = append(episodeDiagnosis, diagnosis)
+			covidEpisode.Diagnosis = episodeDiagnosis
 		}
-
-		// if any of the headers are ones that only do binary values
-
-		// if the header is a date
-
-		// loop thru headers;
-		// assign the following:
-		// diagnosis codes,
-		// admission and discharge dts,
-		// episode (ext),
-		// months (ext),
-
-		// an extension can be:
-		// a covid flag
-		// a covid episode (months)
-
-		fmt.Println("kv.Key")
-		fmt.Println(kv.Key)
-		fmt.Println("kv.Value")
-		fmt.Println(kv.Value)
-
-		covidEpisode.Extension = append()
 	}
 	return covidEpisode
 
+}
+
+func stringToFhirDate(timeString string) (*r4Datatypes.DateTime, error) {
+	timestamp, err := time.Parse("2006-01-02T15:04:05.000-07:00", timeString)
+	if err != nil {
+		return nil, err
+	}
+	fhirTimestamp := &r4Datatypes.DateTime{
+		ValueUs:   timestamp.UnixNano() / int64(time.Microsecond),
+		Precision: r4Datatypes.DateTime_DAY,
+	}
+	return fhirTimestamp, nil
 }
