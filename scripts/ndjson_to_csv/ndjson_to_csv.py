5cf227c19592f725fe40673ef725ec3ab3d40c4c
JP Hechter <jhechter@fearless.tech>
2021-07-16 11:00:42 -0400

    refactored code to use Pandas and removed templates

--- scripts/ndjson_to_csv/ndjson_to_csv.py
+++ scripts/ndjson_to_csv/ndjson_to_csv.py
@@ -1,9 +1,18 @@
 # /usr/bin/python3
 
-import csv
 import datetime
 import json
 
+from functools import reduce
+
+import pandas as pd
+
+"""
+Create the environment:
+    python3 -m venv env
+    source ./env/bin/activate
+    pip install pandas
+"""
 
 def flatten(data, parent_key='', sep='.'):
     items = []
@@ -23,63 +32,30 @@ def flatten(data, parent_key='', sep='.'):
             items.append((new_key, v))
     return dict(items)
 
-def convert_to_csv(template:dict, data:list):
-    output = []
-    
-    # Build columns
-    columns_dict = flatten(template)
-    columns = [k for k in columns_dict]
-    
-    output.append(columns)
-
-    # Flatten data
-    data_dict = [flatten(obj) for obj in data]
-
-    # Populate CSV
-    for obj in data_dict:
-        row = []
-        for column in columns:
-            try:
-                row.append(obj[column])
-            except:
-                row.append('')
-        output.append(row)
-
-    return output
-
 
 if __name__=='__main__':
     print("This tool can convert FHIR data stored in an NDJSON file to a CSV format.")
     while True:
-        # Ingest the report results and format the data
+        # Ingest the report results
         filename = input("Enter the name of the NDJSON file you wish to convert: ")
         f = open(filename, 'r')
-        string_json = '[' + f.read()[0:-1] + ']'  # Drop the linebreaks
+        
+        # Format data
+        string_json = '[' + f.read()[0:-1] + ']'
         string_json = string_json.replace('\n', ',')
         data = json.loads(string_json)
-        template = None
-        while template is None:
-            resource_type = input("What kind of resource is this? (enter 1, 2, or 3)\
-                \n1. Patient\n2. Coverage\n3. Benefits\n")
-            if resource_type == '1':
-                template = json.loads(open("templates/patient.json", "r").read())
-            elif resource_type == '2':
-                template = json.loads(open("templates/coverage.json", "r").read())
-            elif resource_type == '3':
-                template = json.loads(open("templates/benefits.json", "r").read())
-        
-        # Create the file and write results
-        report_results = convert_to_csv(template, data)
-        report_file = open('{}_{}.csv'.format(
-            filename,
-            datetime.datetime.today().date()
-            ), 'w')
-        csvwriter = csv.writer(report_file)
-        for item in report_results:
-            csvwriter.writerow(item)
-        report_file.close()
 
-        print("File converted successfully. Output file '{}'".format(report_file.name))
+        list_data_dict = [flatten(obj) for obj in data]  # Flatten data
+        list_dataframes = [  # Create DataFrames
+            pd.DataFrame(obj, index=[list_data_dict.index(obj)])
+            for obj in list_data_dict
+        ]
+        output = reduce(lambda x, y: x.append(y), list_dataframes)  # Merge DataFrames
+        
+        output_filename = '{}_{}.csv'.format(filename, datetime.datetime.today().date())
+        output.to_csv(output_filename, index=False)
+
+        print("File converted successfully. Output file '{}'".format(output_filename))
         if input("Would you like to convert another file? (y/n) ") == 'n':
             print("Goodbye.")
             exit()
