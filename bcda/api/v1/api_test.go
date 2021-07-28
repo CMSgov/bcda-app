a9fe3c6df0765b2b0552e1cd31d6eac51927786d
Silvio Luthi <silvio@adhocteam.us>
2021-07-28 14:37:53 -0700

    readd timestamp check

--- bcda/api/v1/api_test.go
+++ bcda/api/v1/api_test.go
@@ -468,6 +468,7 @@ func (s *APITestSuite) TestGetAttributionStatus() {
 	assert.Equal(s.T(), "default", resp.CCLFFiles[0].Type)
 	assert.Equal(s.T(), cclfFile.CCLFNum, resp.CCLFFiles[0].CCLFNum)
 	assert.Equal(s.T(), cclfFile.Name, resp.CCLFFiles[0].Name)
+	assert.Equal(s.T(), cclfFile.Timestamp.String(), resp.CCLFFiles[0].Timestamp.String())
 }
 
 func (s *APITestSuite) makeContextValues(acoID uuid.UUID) (data auth.AuthData) {
--- bcda/api/v2/api_test.go
+++ bcda/api/v2/api_test.go
@@ -489,6 +489,7 @@ func (s *APITestSuite) TestGetAttributionStatus() {
 	assert.Equal(s.T(), "default", resp.CCLFFiles[0].Type)
 	assert.Equal(s.T(), cclfFile.CCLFNum, resp.CCLFFiles[0].CCLFNum)
 	assert.Equal(s.T(), cclfFile.Name, resp.CCLFFiles[0].Name)
+	assert.Equal(s.T(), cclfFile.Timestamp.String(), resp.CCLFFiles[0].Timestamp.String())
 }
 
 func (s *APITestSuite) getAuthData() (data auth.AuthData) {
