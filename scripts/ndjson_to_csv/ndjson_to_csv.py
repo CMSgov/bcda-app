04f48794796af114ae748eb961f7d85736ae2ddc
JP Hechter <jhechter@fearless.tech>
2021-08-09 18:16:08 -0400

    refactor to flatten data 2x instead of using a tempfile; use argparse instead of sys to get cli args

--- scripts/ndjson_to_csv/ndjson_to_csv.py
+++ scripts/ndjson_to_csv/ndjson_to_csv.py
@@ -1,23 +1,10 @@
 # /usr/bin/python3
 
-import csv, json, sys, tempfile
+import argparse, csv, datetime, json
 from pathlib import Path
 
 from tqdm import tqdm
 
-
-col_headers = set()
-help_message = 'Convert FHIR data stored in an NDJSON file to a CSV format.\n\
-\n\
-Usage:\n\
-\n\
-    ndjson_to_csv.py <inputfile> <output_filename>\n\
-\n\
-\n\
-<inputfile>        Source file to be converted (.json, .ndjson, .txt)\n\
-<output_filename>  Optional desired output filename\n\
-\n\
--h, --help         view help message\n'
 
 def flatten(data, parent_key='', sep='.'):
     items = []
@@ -42,38 +29,25 @@ def flatten(data, parent_key='', sep='.'):
                         items.extend(flatten(item, new_key, sep).items())
             else:
                 list_strings = [str(item) for item in v]
-                col_headers.add(new_key)
                 items.append((new_key, ' '.join(list_strings)))
         else:
-            col_headers.add(new_key)
             items.append((new_key, v))
     return dict(items)
 
 
 if __name__=='__main__':
-    """
-    Convert FHIR data stored in an NDJSON file to a CSV format.
-
-    Usage: ndjson_to_csv.py <inputfile> <output_filename>
+    parser = argparse.ArgumentParser(description='Convert FHIR data stored in an NDJSON file to a CSV format.')
+    parser.add_argument('input', metavar='<inputfile>', type=str, nargs=1,
+                    help='Source file to be converted (.json, .ndjson, .txt)')
+    parser.add_argument('output', metavar='<output_filename>', type=str, nargs='?',
+                    default=f'flat_fhir_output_{datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}.csv',
+                    help='Optional desired output filename')
     
-    <inputfile>        Source file to be converted (.json, .ndjson, .txt)
-    <output_filename>  Optional desired output filename
-    """
-
-    argv = sys.argv[1:]
-    
-    if '-h' in argv or '--help' in argv:
-        print(help_message)
-        exit()
-    if len(argv) > 2:
-        print('Too many arguments.')
-        print(help_message)
-        exit()
-
-    inputfile, outputfile = argv[0], argv[1]
+    args = parser.parse_args()
+    inputfile, outputfile = args.input[0], args.output
     
     if outputfile[-4:] != '.csv':
-        outputfile = f'{outputfile}.csv'
+        outputfile = f'{outputfile}.csv'    
 
     # Error handling
     if not Path(inputfile).is_file():
@@ -83,37 +57,42 @@ if __name__=='__main__':
         print(f'A file with the name "{outputfile}" already exists in this directory.')
         exit()
 
-    # Create Tempfile
-    tf = tempfile.NamedTemporaryFile(prefix=f'{outputfile}_temp_', mode="w+t", delete=True)
     counter = 0
 
-    # Flatten data structures
-    print(f'Flattening "{inputfile}"...')
+    print(f'Determining file size...')
     with open(inputfile, 'r') as source_file:
-        for line in source_file:
+        for line in source_file:  # Count lines to build progress indicator
+            counter += 1
+        source_file.seek(0)
+        
+        print(f'Flattening "{inputfile}"...')
+        col_headers = set()
+        for x in tqdm(range(counter)):
+            line = source_file.readline()
             if line[-1:] == '\n':
                 line = line[0:-1]
-            data = json.loads(line)
-            tf.write(f'{json.dumps(flatten(data))}\n')
-            counter += 1
+            data = flatten(json.loads(line))
+            col_headers.update(data.keys())
 
     # Merge data referencing headers
     print('Merging data and creating CSV...')
-    list_headers = sorted(list(col_headers))  # Lists are ordered
-    tf.seek(0)
+    list_headers = sorted(list(col_headers))
     with open(outputfile, "x") as output_file:
-        csvwriter = csv.writer(output_file)
-        csvwriter.writerow(list_headers)
-        for x in tqdm(range(counter)):
-            row = []
-            line = tf.readline()[0:-1]  # Every line has a trailing return
-            data = json.loads(line)
-            for col in list_headers:
-                try:
-                    row.append(data[col])
-                except:
-                    row.append('')
-            csvwriter.writerow(row)
+        with open(inputfile, 'r') as source_file:
+            csvwriter = csv.writer(output_file)
+            csvwriter.writerow(list_headers)
+            for x in tqdm(range(counter)):
+                row = []
+                line = source_file.readline()
+                if line[-1:] == '\n':
+                    line = line[0:-1]
+                data = flatten(json.loads(line))
+                for col in list_headers:
+                    try:
+                        row.append(data[col])
+                    except:
+                        row.append('')
+                csvwriter.writerow(row)
 
-    print(f'File flattened successfully. Output file {outputfile}.csv')
+    print(f'File flattened successfully. Output file "{outputfile}"')
     exit()
