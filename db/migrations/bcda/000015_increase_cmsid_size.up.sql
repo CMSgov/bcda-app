fadb7a117d99c9462ece8e30a7cdee1fe24005aa
Eric Buckley <eric.buckley@gmail.com>
2021-09-08 19:30:01 -0700

    changing error constraint
    the ACOS table has 2 constraints; a unique primary key and unique cmsid.
    the initial migration created the former constraint before the latter,
    thus violations with both will report the former error.  However with
    migration 0000015, altering the column deletes, then recreates the
    unique cmsid.  thus placing it second, so this test case that violates
    both constraints is now going to report an issue with the primary key

--- db/migrations/bcda/000015_increase_cmsid_size.up.sql
+++ db/migrations/bcda/000015_increase_cmsid_size.up.sql
@@ -2,7 +2,6 @@
 
 BEGIN;
 
-
 ALTER table public.acos ALTER COLUMN cms_id type character varying(8);
 ALTER table public.cclf_files ALTER COLUMN aco_cms_id type character varying(8);
 ALTER table public.suppressions ALTER COLUMN aco_cms_id type character varying(8);
