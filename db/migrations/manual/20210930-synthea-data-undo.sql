68e0a341aa4e5075f0957c7e2de417bfa78ed06f
Daniel Choi <daniel.choi@adhocteam.us>
2021-10-27 13:17:04 -0400

    BCDA-4867 synthetic ingestion opensbx (#754)
    * SQL script to add "synthea" synthetic data and a script to undo it

---
+++ db/migrations/manual/20210930-synthea-data-undo.sql
@@ -0,0 +1,22 @@
+-- This SQL script is for bookkeeping and auditing purposes
+-- This script can be used to remove the improved synthea data from opensbx
+
+BEGIN;
+DO $$
+
+DECLARE Dev INTEGER[];
+DECLARE Small INTEGER[];
+DECLARE Large INTEGER[];
+
+BEGIN
+
+SELECT array_agg(id) FROM cclf_files WHERE aco_cms_id = 'A9989' INTO Dev;
+SELECT array_agg(id) FROM cclf_files WHERE aco_cms_id = 'A9998' INTO Small;
+SELECT array_agg(id) FROM cclf_files WHERE aco_cms_id = 'A9999' INTO Large;
+
+DELETE FROM cclf_beneficiaries WHERE file_id = ANY(Dev || Small || Large);
+DELETE FROM cclf_files WHERE id = ANY(Dev || Small || Large);
+
+END $$;
+
+COMMIT;
