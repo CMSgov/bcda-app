51bec8f8df4ff0a9936e63f969271a0a4c101120
David Gage <whytheplatypus@gmail.com>
2022-05-12 10:03:57 -0400

    Correct test coverage for all packages
    Before this test coverage would not be counted for called functions from
    dependencies (sub directories)

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -22,8 +22,13 @@ PACKAGES_TO_COVER=$(go list ./... | egrep -v 'test|mock|db/migrations' | tr "\n"
 gotestsum \
     --junitfile test_results/${timestamp}/junit.xml -- \
     -covermode atomic \
+    -cover \
+    -coverpkg ${PACKAGES_TO_COVER} \
     -race ./... \
     -coverprofile test_results/${timestamp}/testcoverage.out 2>&1 | tee test_results/${timestamp}/testresults.out
+
+#go test ./... -cover -coverpkg ${PACKAGES_TO_COVER} -coverprofile test_results/${timestamp}/testcoverage.out 2>&1 | tee test_results/${timestamp}/testresults.out
+
 go tool cover -func test_results/${timestamp}/testcoverage.out > test_results/${timestamp}/testcov_byfunc.out
 echo TOTAL COVERAGE:  $(tail -1 test_results/${timestamp}/testcov_byfunc.out | head -1)
 go tool cover -html=test_results/${timestamp}/testcoverage.out -o test_results/${timestamp}/testcoverage.html
