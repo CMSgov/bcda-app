0d894be1bc9bee7b3e254b6e1619f1b8249eaf17
Kevin Yeh <kevinyeah@utexas.edu>
2023-07-17 14:40:44 -0400

    fix import

--- bcda/models/postgres/repository.go
+++ bcda/models/postgres/repository.go
@@ -15,6 +15,7 @@ import (
 	"github.com/CMSgov/bcda-app/bcda/database"
 	"github.com/CMSgov/bcda-app/bcda/models"
 	"github.com/CMSgov/bcda-app/log"
+	"github.com/CMSgov/bcda-app/optout"
 )
 
 const (
@@ -275,7 +276,7 @@ func (r *Repository) GetCCLFBeneficiaries(ctx context.Context, cclfFileID uint, 
 	return beneficiaries, nil
 }
 
-func (r *Repository) CreateSuppression(ctx context.Context, suppression models.Suppression) error {
+func (r *Repository) CreateSuppression(ctx context.Context, suppression optout.Suppression) error {
 	ib := sqlFlavor.NewInsertBuilder().InsertInto("suppressions").
 		Cols("file_id", "mbi", "source_code", "effective_date", "preference_indicator",
 			"samhsa_source_code", "samhsa_effective_date", "samhsa_preference_indicator",
@@ -327,7 +328,7 @@ func (r *Repository) GetSuppressedMBIs(ctx context.Context, lookbackDays int, up
 	return suppressedMBIs, nil
 }
 
-func (r *Repository) CreateSuppressionFile(ctx context.Context, suppressionFile models.SuppressionFile) (uint, error) {
+func (r *Repository) CreateSuppressionFile(ctx context.Context, suppressionFile optout.SuppressionFile) (uint, error) {
 	ib := sqlFlavor.NewInsertBuilder().InsertInto("suppression_files")
 	ib.Cols("name", "timestamp", "import_status").
 		Values(suppressionFile.Name, suppressionFile.Timestamp, suppressionFile.ImportStatus)
