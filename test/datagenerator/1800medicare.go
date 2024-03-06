5b7c63e96dbfff4e6367b32b7cb30cdbbd66e21b
Kevin Yeh <kevinyeah@utexas.edu>
2024-03-06 18:34:41 -0500

    manual changes to generate DPC request files

--- test/datagenerator/1800medicare.go
+++ test/datagenerator/1800medicare.go
@@ -2,14 +2,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"golang.org/x/text/language"
 	"math/rand"
 	"os"
 	"path/filepath"
 	"time"
 
+	"golang.org/x/text/language"
+
+	"github.com/Pallinder/go-randomdata"
 	"golang.org/x/text/cases"
 )
 
@@ -19,15 +20,10 @@ type field struct {
 }
 
 type record struct {
-	hicn, blk                                    field
-	firstName, middleName, lastName              field
-	dob                                          field
-	addr1, addr2, addr3, city, state, zip5, zip4 field
-	gender                                       field
-	encDate                                      field
-	effDate, srcCode, mechCode, prefInd          field
-	saEffDate, saSrcCode, saMechCode, saPrefInd  field
-	acoID, acoName                               field
+	hicn                            field
+	firstName, middleName, lastName field
+	dob                             field
+	effDate, prefInd                field
 }
 
 func main() {
@@ -35,60 +31,41 @@ func main() {
 	now := time.Now()
 
 	// 1-800-MEDICARE filename format: (T|P)#EFT.ON.ACO.NGD1800.DPRF.Dyymmdd.Thhmmsst
-	fileName := fmt.Sprintf("T#EFT.ON.ACO.NGD1800.DPRF.%s", now.Format("D060102.T1504050"))
+	fileName := fmt.Sprintf("P#EFT.ON.DPC.NGD.REQ.%s", now.Format("D060102.T1504050"))
 	outf, err := os.Create(filepath.Clean(fileName))
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = outf.WriteString(fmt.Sprintf("HDR_BENEDATASHR%s\n", now.Format("20060102")))
+	_, err = outf.WriteString(fmt.Sprintf("HDR_BENEDATAREQ%s\n", now.Format("20060102")))
 	if err != nil {
 		panic(err)
 	}
 
 	recCount := 0
 
-	for _, acoID := range []string{"A9990", "A9991", "A9992", "A9993", "A9994"} {
-		inf, err := os.Open(fmt.Sprintf("%s-HICNs", acoID))
-		if err != nil {
-			panic(err)
-		}
-
-		s := bufio.NewScanner(inf)
-
-		for s.Scan() {
-			// Randomly select HICNs from ACO for suppression records
-			if rand.Intn(2) == 0 {
-				continue
-			}
-
-			hicn := string(s.Bytes())
-			p := profile(hicn, acoID)
-			recs := records(p)
-			for _, r := range recs {
-				for _, f := range []field{r.hicn, r.blk, r.firstName, r.middleName, r.lastName, r.dob, r.addr1, r.addr2, r.addr3, r.city, r.state, r.zip5, r.zip4, r.gender, r.encDate, r.effDate, r.srcCode, r.mechCode, r.prefInd, r.saEffDate, r.saSrcCode, r.saMechCode, r.saPrefInd, r.acoID, r.acoName} {
-					_, err = outf.WriteString(fmt.Sprintf("%-"+fmt.Sprint(f.l)+"s", f.v))
-					if err != nil {
-						panic(err)
-					}
-				}
-				_, err = outf.WriteString("\n")
+	for i := 0; i < 10; i++ {
+		hicn := randMbi()
+		p := profile(hicn)
+		recs := records(p)
+		for _, r := range recs {
+			for _, f := range []field{r.hicn, r.firstName, r.middleName, r.lastName, r.dob, r.effDate, r.prefInd} {
+				_, err = outf.WriteString(fmt.Sprintf("%-"+fmt.Sprint(f.l)+"s", f.v))
 				if err != nil {
 					panic(err)
 				}
 			}
-			recCount += len(recs)
+			_, err = outf.WriteString("\n")
+			if err != nil {
+				panic(err)
+			}
 		}
+		recCount += len(recs)
+	}
 
-		_, err = outf.WriteString(fmt.Sprintf("TRL_BENEDATASHR%s%-10d", now.Format("20060102"), recCount))
-		if err != nil {
-			panic(err)
-		}
-
-		err = inf.Close()
-		if err != nil {
-			panic(err)
-		}
+	_, err = outf.WriteString(fmt.Sprintf("TRL_BENEDATAREQ%s%-10d", now.Format("20060102"), recCount))
+	if err != nil {
+		panic(err)
 	}
 
 	err = outf.Close()
@@ -97,69 +74,37 @@ func main() {
 	}
 }
 
-func profile(hicn string, acoID string) record {
+func profile(hicn string) record {
 	dobMin, _ := time.Parse("2006-01-02", "1900-01-01")
 	dobMax := time.Now().Add(-65 * 365 * 24 * time.Hour)
 
 	p := record{
-		hicn:       field{l: 11, v: hicn},                     // HICN
-		blk:        field{l: 10},                              // Beneficiary link key
-		firstName:  field{l: 30, v: randWord(1, 30)},          // Beneficiary first name
-		middleName: field{l: 30},                              // Beneficiary middle name
-		lastName:   field{l: 40, v: randWord(1, 30)},          // Beneficiary last name
-		dob:        field{l: 8, v: ccyymmdd(dobMin, dobMax)},  // Beneficiary date of birth
-		addr1:      field{l: 55, v: addr1()},                  // Beneficiary address line 1
-		addr2:      field{l: 55},                              // Beneficiary address line 2
-		addr3:      field{l: 55},                              // Beneficiary address line 3
-		city:       field{l: 40, v: randWord(1, 40)},          // Beneficiary city
-		state:      field{l: 2, v: "ST"},                      // Beneficiary state
-		zip5:       field{l: 5, v: "00000"},                   // Beneficiary first five digits of ZIP code
-		zip4:       field{l: 4},                               // Beneficiary last four digits of ZIP code
-		gender:     field{l: 1, v: oneOfStr("M", "F", "U")},   // Beneficiary gender
-		acoID:      field{l: 5, v: acoID},                     // ACO identifier
-		acoName:    field{l: 70, v: randWord(1, 66) + " ACO"}, // ACO legal name
+		hicn:      field{l: 11, v: hicn},                                          // HICN
+		firstName: field{l: 30, v: randomdata.FirstName(randomdata.RandomGender)}, // Beneficiary first name
+		lastName:  field{l: 40, v: randomdata.LastName()},                         // Beneficiary last name
+		dob:       field{l: 8, v: ccyymmdd(dobMin, dobMax)},                       // Beneficiary date of birth
 	}
 
 	return p
 }
 
 func records(profile record) []record {
-	// Create 0-3 suppression records for this profile
-	ct := rand.Intn(4)
-	encDtMin, encDtMax := time.Now().Add(-7*24*time.Hour), time.Now()
-	effDtMin, effDtMax := time.Now().Add(-7*24*time.Hour), time.Now().Add(3*24*time.Hour)
+	// Create 0-1 suppression records for this profile
+	// ct := rand.Intn(2)
 	records := []record{}
 
-	for i := 0; i < ct; i++ {
+	for i := 0; i < 1; i++ {
 		r := profile
 
-		r.srcCode = field{l: 5, v: oneOfStr("1800", "")}    // Beneficiary data sharing source code
-		r.saSrcCode = field{l: 5, v: oneOfStr("1-800", "")} // Beneficiary substance abuse data sharing source code
-		if r.srcCode.v == "" && r.saSrcCode.v == "" {
-			// One or both types of data sharing should have values
-			continue
-		}
+		is1800 := oneOfStr("1800", "")
+		r.effDate = field{l: 8, v: ""} // Beneficiary data sharing effective date
+		r.prefInd = field{l: 1, v: ""} // Beneficiary data sharing preference indicator
 
-		r.encDate = field{l: 8, v: ccyymmdd(encDtMin, encDtMax)} // Encounter date
-
-		r.effDate = field{l: 8, v: ""}  // Beneficiary data sharing effective date
-		r.mechCode = field{l: 1, v: ""} // Beneficiary option data sharing decision mechanism code
-		r.prefInd = field{l: 1, v: ""}  // Beneficiary data sharing preference indicator
 		// If beneficiary data sharing source code is populated, also populate its associated fields (ICD v9.1 p.11)
-		if r.srcCode.v != "" {
+		if is1800 != "" {
+			effDtMin, effDtMax := time.Now().Add(-7*24*time.Hour), time.Now().Add(3*24*time.Hour)
 			r.effDate.v = ccyymmdd(effDtMin, effDtMax)
-			r.mechCode.v = "T"
 			r.prefInd.v = oneOfStr("Y", "N")
-		}
-
-		r.saEffDate = field{l: 8, v: ""}  // Beneficiary substance abuse data sharing effective date
-		r.saMechCode = field{l: 1, v: ""} // Beneficiary option substance abuse decision mechanism code
-		r.saPrefInd = field{l: 1, v: ""}  // Beneficiary substance abuse data sharing preference indicator
-		// If beneficiary substance abuse data sharing source code is populated, also populate its associated fields (ICD v9.1 p.11)
-		if r.saSrcCode.v != "" {
-			r.saEffDate.v = ccyymmdd(effDtMin, effDtMax)
-			r.saMechCode.v = "T"
-			r.saPrefInd.v = "N"
 		}
 
 		records = append(records, r)
@@ -178,16 +123,12 @@ func oneOfStr(strs ...string) string {
 	return strs[rand.Intn(len(strs))]
 }
 
-func randWord(minLen, maxLen int) string {
+func randMbi() string {
 	const letters = "abcdefghijklmnopqrstuvwxyz"
-	l := rand.Intn(maxLen-minLen) + minLen
-	w := ""
-	for i := 0; i < l; i++ {
-		w = w + string(letters[rand.Intn(len(letters))])
+	const digits = "0123456789"
+	w := string(letters[rand.Intn(len(letters))])
+	for i := 0; i < 10; i++ {
+		w = w + string(digits[rand.Intn(len(digits))])
 	}
 	return cases.Title(language.Und).String(w)
-}
-
-func addr1() string {
-	return fmt.Sprintf("%d %s %s", rand.Intn(50000), randWord(1, 44), oneOfStr("St", "Ave", "Dr", "Blvd", "Way"))
 }
