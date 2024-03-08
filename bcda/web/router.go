807bc4b29515485bd1e8b3fd4ae5db44d49b0b34
Alex Dzeda <alex@e14s.com>
2024-03-08 10:36:39 -0600

    update comment for which environments rate limiting is enabled for.

--- bcda/web/router.go
+++ bcda/web/router.go
@@ -49,7 +49,7 @@ func NewAPIRouter() http.Handler {
 		r.Get(`/{:(user_guide|encryption|decryption_walkthrough).html}`, userGuideRedirect)
 	}
 	if conf.GetEnv("DEPLOYMENT_TARGET") == "prod" || conf.GetEnv("DEPLOYMENT_TARGET") == "test" {
-		// Apply rate limiting on production only
+		// Apply rate limiting on test +  production only
 		requestValidators = append(requestValidators, middleware.CheckConcurrentJobs)
 	}
 
