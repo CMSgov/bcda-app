a1abbdc47d7561c9cf850118cc2d76d3fa640412
Kevin Yeh <kevinyeah@utexas.edu>
2024-06-04 15:52:30 -0400

    Separate env in tests

--- bcda/lambda/cclf/main_test.go
+++ bcda/lambda/cclf/main_test.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/CMSgov/bcda-app/bcda/database"
 	"github.com/CMSgov/bcda-app/bcda/models/postgres/postgrestest"
 	"github.com/CMSgov/bcda-app/bcda/testUtils"
+	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -24,13 +26,17 @@ func (s *CclfImportMainSuite) TestImportCCLFDirectory() {
 	targetACO := "A0002"
 	assert := assert.New(s.T())
 
-	cleanup := testUtils.SetEnvVars(s.T(), []testUtils.EnvVar{{Name: "CCLF_REF_DATE", Value: "181125"}})
-	defer cleanup()
+	env := uuid.NewUUID()
+	cleanupEnv := testUtils.SetEnvVars(s.T(), []testUtils.EnvVar{
+		{Name: "CCLF_REF_DATE", Value: "181125"},
+		{Name: "ENV", Value: env.String()},
+	})
+	defer cleanupEnv()
 
 	cleanupParams := testUtils.SetParameters(s.T(), []testUtils.AwsParameter{
-		{Name: "/cclf-import/bcda/local/bfd-bucket-role-arn", Value: "arn:aws:iam::000000000000:user/fake-arn", Type: "String"},
-		{Name: "/bcda/local/api/DATABASE_URL", Value: "postgresql://postgres:toor@db-unit-test:5432/bcda_test?sslmode=disable", Type: "SecureString"},
-		{Name: "/bcda/local/api/QUEUE_DATABASE_URL", Value: "doesnt-matter", Type: "SecureString"},
+		{Name: fmt.Sprintf("/cclf-import/bcda/%s/bfd-bucket-role-arn", env), Value: "arn:aws:iam::000000000000:user/fake-arn", Type: "String"},
+		{Name: fmt.Sprintf("/bcda/%s/api/DATABASE_URL", env), Value: "postgresql://postgres:toor@db-unit-test:5432/bcda_test?sslmode=disable", Type: "SecureString"},
+		{Name: fmt.Sprintf("/bcda/%s/api/QUEUE_DATABASE_URL", env), Value: "doesnt-matter", Type: "SecureString"},
 	})
 	defer cleanupParams()
 
