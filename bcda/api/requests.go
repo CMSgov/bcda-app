130b32d6bc95561e6cd0ee952896ebcd69026879
carlpartridge <carlpartridge@navapbc.com>
2024-10-16 17:30:44 -0400

    Quick adjustments

--- bcda/api/requests.go
+++ bcda/api/requests.go
@@ -7,7 +7,6 @@ import (
 	goerrors "errors"
 	"fmt"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 
@@ -633,7 +632,7 @@ func (h *Handler) bulkRequest(w http.ResponseWriter, r *http.Request, reqType se
 		sinceParam := (!rp.Since.IsZero() || conditions.ReqType == service.RetrieveNewBeneHistData)
 		jobPriority := h.Svc.GetJobPriority(conditions.CMSID, j.ResourceType, sinceParam) // first argument is the CMS ID, not the ACO uuid
 
-		logger.Infof("Adding jobs using %v", reflect.TypeOf(h.Enq))
+		logger.Infof("Adding jobs using %T", h.Enq)
 		if err = h.Enq.AddJob(*j, int(jobPriority)); err != nil {
 			logger.Error(err)
 			h.RespWriter.Exception(r.Context(), w, http.StatusInternalServerError, responseutils.InternalErr, "")
