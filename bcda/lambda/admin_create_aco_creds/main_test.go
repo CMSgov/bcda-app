9503291a270fe84b7e36b52cbc02818030924438
carlpartridge <carlpartridge@navapbc.com>
2025-01-29 09:58:03 -0500

    BCDA-8630: Copy over blank files for fresh start

---
+++ bcda/lambda/admin_create_aco_creds/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/slack-go/slack"
+)
+
+type mockNotifier struct {
+	Notifier
+}
+
+func (m *mockNotifier) PostMessageContext(ctx context.Context, channelID string, options ...slack.MsgOption) (string, string, error) {
+	return channelID, time.Now().String(), nil
+}
+
+func TestHandleCreateACOCreds(t *testing.T) {
+	// ctx := context.Background()
+
+	// mockConn, err := pgxmock.NewConn()
+	// assert.Nil(t, err)
+	// defer mockConn.Close(ctx)
+
+	// mockConn.ExpectExec("^UPDATE acos SET termination_details = (.+)").
+	// 	WithArgs(mockTermination{}, testACODenies).
+	// 	WillReturnResult(pgxmock.NewResult("UPDATE", 3))
+
+	// err = handleACODenies(ctx, mockConn, payload{testACODenies}, &mockNotifier{})
+	// assert.Nil(t, err)
+}
