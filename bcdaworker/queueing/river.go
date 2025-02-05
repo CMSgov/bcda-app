afb10a8a5978187472da1bf9cd194ae109d7b7c4
Parwinder Bhagat <Parwinder.Bhagat@e14s.com>
2025-02-05 17:55:10 -0600

    Use CLI functions directly, avoid circular dependency

--- bcdaworker/queueing/river.go
+++ bcdaworker/queueing/river.go
@@ -7,13 +7,14 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
-	"github.com/CMSgov/bcda-app/bcda/bcdacli"
 	"github.com/CMSgov/bcda-app/bcda/constants"
 	"github.com/CMSgov/bcda-app/bcda/database"
 	"github.com/CMSgov/bcda-app/bcda/metrics"
 	"github.com/CMSgov/bcda-app/bcda/models"
+	p "github.com/CMSgov/bcda-app/bcda/models/postgres"
 	"github.com/CMSgov/bcda-app/bcda/utils"
 	"github.com/CMSgov/bcda-app/bcdaworker/repository/postgres"
 	"github.com/CMSgov/bcda-app/bcdaworker/worker"
@@ -141,7 +142,7 @@ func (w *CleanupJobWorker) Work(ctx context.Context, rjob *river.Job[CleanupJobA
 	defer cancel()
 
 	ctx = log.NewStructuredLoggerEntry(log.Worker, ctx)
-	ctx, logger := log.SetCtxLogger(ctx, "transaction_id", uuid.New())
+	_, logger := log.SetCtxLogger(ctx, "transaction_id", uuid.New())
 
 	cutoff := getCutOffTime()
 	archiveDir := conf.GetEnv("FHIR_ARCHIVE_DIR")
@@ -149,25 +150,25 @@ func (w *CleanupJobWorker) Work(ctx context.Context, rjob *river.Job[CleanupJobA
 	payloadDir := conf.GetEnv("PAYLOAD_DIR")
 
 	// Cleanup archived jobs: remove job directory and files from archive and update job status to Expired
-	if err := bcdacli.CleanupJob(cutoff, models.JobStatusArchived, models.JobStatusExpired, archiveDir, stagingDir); err != nil {
+	if err := CleanupJob(cutoff, models.JobStatusArchived, models.JobStatusExpired, archiveDir, stagingDir); err != nil {
 		logger.Error(errors.Wrap(err, fmt.Sprintf("failed to process job: %s", constants.CleanupArchArg)))
 		return err
 	}
 
 	// Cleanup failed jobs: remove job directory and files from failed jobs and update job status to FailedExpired
-	if err := bcdacli.CleanupJob(cutoff, models.JobStatusFailed, models.JobStatusFailedExpired, stagingDir, payloadDir); err != nil {
+	if err := CleanupJob(cutoff, models.JobStatusFailed, models.JobStatusFailedExpired, stagingDir, payloadDir); err != nil {
 		logger.Error(errors.Wrap(err, fmt.Sprintf("failed to process job: %s", constants.CleanupFailedArg)))
 		return err
 	}
 
 	// Cleanup cancelled jobs: remove job directory and files from cancelled jobs and update job status to CancelledExpired
-	if err := bcdacli.CleanupJob(cutoff, models.JobStatusCancelled, models.JobStatusCancelledExpired, stagingDir, payloadDir); err != nil {
+	if err := CleanupJob(cutoff, models.JobStatusCancelled, models.JobStatusCancelledExpired, stagingDir, payloadDir); err != nil {
 		logger.Error(errors.Wrap(err, fmt.Sprintf("failed to process job: %s", constants.CleanupCancelledArg)))
 		return err
 	}
 
 	// Archive expiring jobs: update job statuses and move files to an inaccessible location
-	if err := bcdacli.ArchiveExpiring(cutoff); err != nil {
+	if err := ArchiveExpiring(cutoff); err != nil {
 		logger.Error(errors.Wrap(err, fmt.Sprintf("failed to process job: %s", constants.ArchiveJobFiles)))
 		return err
 	}
@@ -258,4 +259,92 @@ func getQueueJobCount(db *sql.DB, log logrus.FieldLogger) float64 {
 	}
 
 	return float64(count)
+}
+
+func ArchiveExpiring(maxDate time.Time) error {
+	log.API.Info("Archiving expiring job files...")
+
+	db := database.Connection
+	r := p.NewRepository(db)
+	jobs, err := r.GetJobsByUpdateTimeAndStatus(context.Background(),
+		time.Time{}, maxDate, models.JobStatusCompleted)
+	if err != nil {
+		log.API.Error(err)
+		return err
+	}
+
+	var lastJobError error
+	for _, j := range jobs {
+		id := j.ID
+		jobPayloadDir := fmt.Sprintf("%s/%d", conf.GetEnv("FHIR_PAYLOAD_DIR"), id)
+		_, err = os.Stat(jobPayloadDir)
+		jobPayloadDirExist := err == nil
+		jobArchiveDir := fmt.Sprintf("%s/%d", conf.GetEnv("FHIR_ARCHIVE_DIR"), id)
+
+		if jobPayloadDirExist {
+			err = os.Rename(jobPayloadDir, jobArchiveDir)
+			if err != nil {
+				log.API.Error(err)
+				lastJobError = err
+				continue
+			}
+		}
+
+		j.Status = models.JobStatusArchived
+		err = r.UpdateJob(context.Background(), *j)
+		if err != nil {
+			log.API.Error(err)
+			lastJobError = err
+		}
+	}
+
+	return lastJobError
+}
+
+func CleanupJob(maxDate time.Time, currentStatus, newStatus models.JobStatus, rootDirsToClean ...string) error {
+	db := database.Connection
+	r := p.NewRepository(db)
+	jobs, err := r.GetJobsByUpdateTimeAndStatus(context.Background(),
+		time.Time{}, maxDate, currentStatus)
+	if err != nil {
+		return err
+	}
+
+	if len(jobs) == 0 {
+		log.API.Infof("No %s job files to clean", currentStatus)
+		return nil
+	}
+
+	for _, job := range jobs {
+		if err := cleanupJobData(job.ID, rootDirsToClean...); err != nil {
+			log.API.Errorf("Unable to cleanup directories %s", err)
+			continue
+		}
+
+		job.Status = newStatus
+		err = r.UpdateJob(context.Background(), *job)
+		if err != nil {
+			log.API.Errorf("Failed to update job status to %s %s", newStatus, err)
+			continue
+		}
+
+		log.API.WithFields(logrus.Fields{
+			"job_began":     job.CreatedAt,
+			"files_removed": time.Now(),
+			"job_id":        job.ID,
+		}).Infof("Files cleaned from %s and job status set to %s", rootDirsToClean, newStatus)
+	}
+
+	return nil
+}
+
+func cleanupJobData(jobID uint, rootDirs ...string) error {
+	for _, rootDirToClean := range rootDirs {
+		dir := filepath.Join(rootDirToClean, strconv.FormatUint(uint64(jobID), 10))
+		if err := os.RemoveAll(dir); err != nil {
+			return fmt.Errorf("unable to remove %s because %s", dir, err)
+		}
+	}
+
+	return nil
 }
