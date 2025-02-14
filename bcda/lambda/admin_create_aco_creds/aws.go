b6bdbdd0ddc983b0eec63a3a37293e721c0d3cb5
carlpartridge <carlpartridge@navapbc.com>
2025-02-14 13:56:09 -0500

    Try different key and bucket

--- bcda/lambda/admin_create_aco_creds/aws.go
+++ bcda/lambda/admin_create_aco_creds/aws.go
@@ -142,8 +142,8 @@ func putObject(service s3iface.S3API, acoID string, creds string, kmsID string) 
 
 	s3Input := &s3.PutObjectInput{
 		Body:                 aws.ReadSeekCloser(strings.NewReader(creds)),
-		Bucket:               aws.String(fmt.Sprintf("%s/%s", destBucket, bucketSuffix)),
-		Key:                  aws.String(fmt.Sprintf("%s-creds", acoID)),
+		Bucket:               aws.String(destBucket),
+		Key:                  aws.String(fmt.Sprintf("%s/%s-creds", bucketSuffix, acoID)),
 		ServerSideEncryption: aws.String("aws:kms"),
 		SSEKMSKeyId:          aws.String(kmsID),
 	}
