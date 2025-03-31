d2699134389ddb15f694a5709e19514925892310
carlpartridge <carlpartridge@navapbc.com>
2025-03-31 16:50:55 -0400

    Move to manual migrations due to systems relationship dependency

---
+++ db/migrations/manual/20250331-create_metric_views.down.sql
@@ -0,0 +1,8 @@
+DROP VIEW IF EXISTS active_acos;
+DROP VIEW IF EXISTS beneficiaries_attributed_to_active_entities;
+DROP VIEW IF EXISTS active_entities_served;
+DROP VIEW IF EXISTS unique_entities_making_v2_requests;
+DROP VIEW IF EXISTS requests_using_since_param;
+DROP VIEW IF EXISTS requests_for_runout_data;
+DROP VIEW IF EXISTS beneficiaries_per_job;
+DROP VIEW IF EXISTS requests_per_entity;
