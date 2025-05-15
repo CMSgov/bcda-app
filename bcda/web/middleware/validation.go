0c9f916f48239cb64a9e66a21a0abb8f12faf49b
carlpartridge <carlpartridge@navapbc.com>
2025-05-15 09:07:26 -0400

    Stubbing out typeFilter logic

--- bcda/web/middleware/validation.go
+++ bcda/web/middleware/validation.go
@@ -10,6 +10,7 @@ import (
 
 	responseutils "github.com/CMSgov/bcda-app/bcda/responseutils"
 	responseutilsv2 "github.com/CMSgov/bcda-app/bcda/responseutils/v2"
+	"github.com/CMSgov/bcda-app/bcda/utils"
 	"github.com/CMSgov/bcda-app/log"
 )
 
@@ -23,7 +24,10 @@ type RequestParameters struct {
 	ResourceTypes []string
 	Version       string // e.g. v1, v2
 	RequestURL    string
+	TypeFilter    string
 }
+
+const BBSystemURL = "https://bluebutton.cms.gov/fhir/CodeSystem/Adjudication-Status"
 
 // requestkey is an unexported context key to avoid collisions
 type requestkey int
@@ -120,6 +124,27 @@ func ValidateRequestURL(next http.Handler) http.Handler {
 				}
 			}
 			rp.ResourceTypes = resourceTypes
+		}
+
+		// TODO: V3 _typeFilter
+		params, ok = r.URL.Query()["_typeFilter"]
+		if ok {
+			allowedQueryParams := []string{
+				"ExplanationOfBenefit?",
+				"ExplanationOfBenefit?tag=PartiallyAdjudicated",
+				"ExplanationOfBenefit?_tag=Adjudicated",
+				"ExplanationOfBenefit?_tag=PartiallyAdjudicated,Adjudicated",
+				"ExplanationOfBenefit?_source=FISS",
+				"ExplanationOfBenefit?_source=MCS",
+			}
+			if utils.ContainsString(allowedQueryParams, params[0]) {
+				rp.TypeFilter = params[0]
+			} else {
+				errMsg := fmt.Sprintf("invalid _typeFilter (or currently unimplemented query) %s", params[0])
+				log.API.Error(errMsg)
+				rw.Exception(r.Context(), w, http.StatusBadRequest, responseutils.RequestErr, errMsg)
+				return
+			}
 		}
 
 		ctx := SetRequestParamsCtx(r.Context(), rp)
