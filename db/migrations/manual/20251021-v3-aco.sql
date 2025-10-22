620e459c18b17ab6eacb392a20c09e4e303b3dc5
Lauren Krugen <laurenkrugen@navapbc.com>
2025-10-22 13:34:24 -0700

    add test aco for v3

---
+++ db/migrations/manual/20251021-v3-aco.sql
@@ -0,0 +1,20 @@
+
+BEGIN;
+DO $$
+DECLARE FID cclf_files.id%TYPE;
+
+
+BEGIN
+INSERT INTO cclf_files (created_at, updated_at, cclf_num, name, aco_cms_id, timestamp, performance_year, import_status, type) VALUES (now(), now(), 8, 'T.BCD.TEST994.ZC8Y25.D251021.T1322310', 'TEST994', CURRENT_DATE, 25, 'Completed', 0) RETURNING id INTO FID;
+
+
+-- Adding rows into cclf_beneficiaries for TEST994 (V3 Test ACO)
+INSERT INTO cclf_beneficiaries (file_id, mbi)
+VALUES
+        (FID, '1S00E00AA17'),
+        (FID, '1S00E00AA34'),
+        (FID, '1S00E00AA05'),
+        (FID, '1S00E00AA45');
+
+END $$;
+COMMIT;
