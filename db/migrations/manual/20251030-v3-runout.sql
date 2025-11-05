95faefe4bcbc6a48293b59d9cc316ae614b3a37b
Lauren Krugen <laurenkrugen@navapbc.com>
2025-11-05 09:05:31 -0700

    revert changes

--- db/migrations/manual/20251030-v3-runout.sql
+++ db/migrations/manual/20251030-v3-runout.sql
@@ -4,7 +4,7 @@ DECLARE FID cclf_files.id%TYPE;
 
 
 BEGIN
-INSERT INTO cclf_files (created_at, updated_at, cclf_num, name, aco_cms_id, timestamp, performance_year, import_status, type) VALUES (now(), now(), 8, 'T.BCD.TEST994.ZC8R25.D251030.T1322310', 'TEST994', CURRENT_DATE, 24, 'Completed', 1) RETURNING id INTO FID;
+INSERT INTO cclf_files (created_at, updated_at, cclf_num, name, aco_cms_id, timestamp, performance_year, import_status, type) VALUES (now(), now(), 8, 'T.BCD.TEST994.ZC8Y25.D251030.T1322310', 'TEST994', CURRENT_DATE, 24, 'Completed', 1) RETURNING id INTO FID;
 
 
 -- Adding rows into cclf_beneficiaries for TEST994 (V3 Test ACO)
