cda4ea963ef93cbbd8cecffb57f3d224195cb690
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-02 15:16:32 -0600

    Added AWSLambdaVPCAccessExecutionRole

--- ops/services/20-eft-nextgen/main.tf
+++ ops/services/20-eft-nextgen/main.tf
@@ -151,6 +151,11 @@ resource "aws_iam_role_policy_attachment" "this" {
   policy_arn = aws_iam_policy.default_function.arn
 }
 
+resource "aws_iam_role_policy_attachment" "vpc_access" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 # ---------------------------------------------------------------------------
 # Managed policies
 # ---------------------------------------------------------------------------
@@ -378,12 +383,12 @@ resource "aws_sns_topic_subscription" "this" {
   topic_arn = aws_sns_topic.eft_nextgen_topic.arn
 }
 
-# resource "aws_lambda_event_source_mapping" "this" {
-#   event_source_arn = aws_sqs_queue.this.arn
-#   function_name    = aws_lambda_function.this.function_name
-#   batch_size       = 1
-#   enabled          = true
-# }
+resource "aws_lambda_event_source_mapping" "this" {
+  event_source_arn = aws_sqs_queue.this.arn
+  function_name    = aws_lambda_function.this.function_name
+  batch_size       = 1
+  enabled          = true
+}
 
 
 
