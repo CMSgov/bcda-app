7e1aef83670346918cf263767231e46e4e7cfd47
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-06 08:48:37 -0600

    Remove unused imports

--- bcda/lambda/attribution-import/main.go
+++ bcda/lambda/attribution-import/main.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
+// 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func main() {
