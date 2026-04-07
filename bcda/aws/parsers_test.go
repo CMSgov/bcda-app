ec68dd1a725fe6d2be72191b5242362a90c69791
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-07 09:04:38 -0600

    Remove extra )

--- bcda/aws/parsers_test.go
+++ bcda/aws/parsers_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
    	"github.com/stretchr/testify/require"
-    )
+
 )
 
 func TestParseSQSEvent(t *testing.T) {
