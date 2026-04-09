8e8d5a4d88b1c00461e168ba82595e9214d27f57
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-09 14:03:29 -0600

    apply cdap terraform

--- ops/services/20-attribution-import/main.tf
+++ ops/services/20-attribution-import/main.tf
@@ -1,14 +1,13 @@
 locals {
-  service      = "attribution-import"
-  default_tags = module.platform.default_tags
-  env          = terraform.workspace
+  service            = "attribution-import"
+  full_name          = "${local.app}-${var.env}-attribution-import"
+  db_sg_name         = "${local.app}-${var.env}-db"
+  extra_kms_key_arns = local.app == "bcda" ? [data.aws_kms_alias.bcda_app_config_kms_key[0].target_key_arn] : []
+}
 
-  account_id            = module.platform.aws_caller_identity.account_id
-  kms_key_arn_primary   = module.platform.kms_alias_primary.target_key_arn
-  kms_key_arn_secondary = module.platform.kms_alias_secondary.target_key_arn
-  name_prefix           = "${local.service_prefix}-${local.service}"
-  private_subnets       = nonsensitive(toset(keys(module.platform.private_subnets)))
-  lambda_filename       = "lambda_function.zip"
+data "aws_kms_alias" "bcda_app_config_kms_key" {
+  count = local.app == "bcda" ? 1 : 0
+  name  = "alias/bcda-${var.env}-app-config-kms"
 }
 
 module "platform" {
@@ -16,369 +15,101 @@ module "platform" {
 
   providers = { aws = aws, aws.secondary = aws.secondary }
 
-  app         = local.app
-  env         = local.env
-  root_module = "https://github.com/CMSgov/bcda-app/tree/main/ops/services/20-attribution-import"
+  app         = "bcda"
+  env         = var.env
+  root_module = "https://github.com/CMSgov/bcda-app/tree/main/ops/services/10-config"
   service     = local.service
   ssm_root_map = {
-    attribution-import = "/bcda/${local.env}/attribution-import/"
+    attribution-import = "/bcda/${var.env}/${local.service}/"
   }
 }
 
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/lambda/bcda-${local.env}-${local.service}"
-  retention_in_days = 180
 
-  tags = {
-    Name = "/aws/lambda/bcda-${local.env}-${local.service}"
+data "aws_iam_policy_document" "assume_bucket_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    resources = [
+      module.platform.ssm.attribution-import.misp-eft-role_arn.value
+    ]
   }
 }
 
-# ---------------------------------------------------------------------------
-# File Bucket
-# ---------------------------------------------------------------------------
+module "attribution_import_function" {
+  source = "github.com/CMSgov/cdap//terraform/modules/function?ref=f4c14d47cc20e7f6de9112d7155af1213c9bca5a"
+
+  app = local.app
+  env = var.env
+
+  name        = local.full_name
+  description = "Ingests the most recent attribution from BFD"
+
+  handler = "bootstrap"
+  runtime = "provided.al2023"
+
+  memory_size = 2048
+
+  function_role_inline_policies = {
+    assume-bucket-role = data.aws_iam_policy_document.assume_bucket_role.json
+  }
+
+  environment_variables = {
+    ENV      = var.env
+    APP_NAME = "${local.app}-${var.env}-attribution-import"
+  }
+  extra_kms_key_arns = local.extra_kms_key_arns
+}
+
+# Set up queue for receiving messages when a file is added to the bucket
+module "attribution_import_queue" {
+  source = "github.com/CMSgov/cdap//terraform/modules/queue?ref=f4c14d47cc20e7f6de9112d7155af1213c9bca5a"
+
+  app = local.app
+  env = var.env
+
+  name = local.full_name
+
+  function_name    = module.attribution_import_function.name
+  policy_documents = [data.aws_iam_policy_document.sns_send_message.json]
+}
+
+data "aws_iam_policy_document" "sns_send_message" {
+  statement {
+    sid     = "SnsSendMessage"
+    actions = ["sqs:SendMessage"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    resources = [module.attribution_import_queue.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.this.arn]
+    }
+  }
+}
 
 module "attribution-import_file_bucket" {
   source = "github.com/CMSgov/cdap//terraform/modules/bucket?ref=787224b"
 
   app           = local.app
-  env           = local.env
-  name          = "${local.app}-${local.env}-${local.service}-file"
-  ssm_parameter = "/${local.app}/${local.env}/${local.service}/nonsensitive/file_bucket_name"
+  env           = var.env
+  name          = "${local.app}-${var.env}-${local.service}-file"
+  ssm_parameter = "/${local.app}/${var.env}/${local.service}/nonsensitive/file_bucket_name"
 }
 
-# ---------------------------------------------------------------------------
-# Lambda Bucket
-# ---------------------------------------------------------------------------
-
-module "attribution-import_lambda_bucket" {
-  source = "github.com/CMSgov/cdap//terraform/modules/bucket?ref=787224b"
-
-  app           = local.app
-  env           = local.env
-  name          = "${local.app}-${local.env}-${local.service}-lambda"
-  ssm_parameter = "/${local.app}/${local.env}/${local.service}/nonsensitive/lambda_bucket_name"
-}
-
-resource "aws_security_group" "this" {
-  description = "Temporary SG for ${local.name_prefix}"
-  egress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description = ""
-      from_port   = 0
-      ipv6_cidr_blocks = [
-        "::/0",
-      ]
-      prefix_list_ids = []
-      protocol        = "-1"
-      security_groups = []
-      self            = false
-      to_port         = 0
-    },
-  ]
-  name   = local.name_prefix
-  tags   = { Name = local.name_prefix }
-  vpc_id = module.platform.vpc_id
-}
-
-# ---------------------------------------------------------------------------
-# IAM role
-# ---------------------------------------------------------------------------
-
-resource "aws_iam_role" "this" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = module.platform.ssm.attribution-import.misp-eft-role_arn.value
-        }
-      },
-      {
-        Action = [
-          "sts:TagSession",
-          "sts:AssumeRoleWithWebIdentity",
-        ]
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:CMSgov/bcda-app:*"
-          }
-        }
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-      },
-      {
-        Action = [
-          "sts:TagSession",
-          "sts:AssumeRole",
-        ]
-        Effect = "Allow"
-        Principal = {
-          AWS = [
-            module.platform.kion_roles["ct-ado-dasg-application-admin"].arn,
-            module.platform.kion_roles["ct-ado-bcda-application-admin"].arn,
-          ]
-        }
-      },
-    ]
-  })
-
-  force_detach_policies = true
-  name                  = "bcda-${local.env}-${local.service}"
-  path                  = module.platform.iam_defaults.path
-  permissions_boundary  = module.platform.iam_defaults.boundary
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.default_function.arn
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_access" {
-  role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
-
-# ---------------------------------------------------------------------------
-# Managed policies
-# ---------------------------------------------------------------------------
-data "aws_iam_policy_document" "default_function" {
+data "aws_iam_policy_document" "topic" {
   statement {
-    sid = "SsmSqsLogsEc2"
-    actions = [
-      "ssm:GetParameters",
-      "ssm:GetParameter",
-      "sqs:ReceiveMessage",
-      "sqs:GetQueueAttributes",
-      "sqs:DeleteMessage",
-      "logs:PutLogEvents",
-      "logs:CreateLogStream",
-      "logs:CreateLogGroup",
-    ]
-    resources = ["*"]
-  }
-  statement {
-
-    sid = "KmsEncryptDecrypt"
-    actions = [
-      "kms:GenerateDataKey",
-      "kms:Encrypt",
-      "kms:Decrypt",
-    ]
-    resources = [
-      local.kms_key_arn_primary,
-      local.kms_key_arn_secondary,
-    ]
-  }
-}
-
-resource "aws_iam_policy" "default_function" {
-  name        = "bcda-${local.env}-${local.service}-default-function"
-  path        = module.platform.iam_defaults.path
-  description = "SSM, SQS, CloudWatch Logs, EC2 networking, and KMS permissions for ${local.service}."
-
-  policy = data.aws_iam_policy_document.default_function.json
-}
-
-resource "aws_iam_role_policy_attachment" "default_function" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.default_function.arn
-}
-
-# -------------------------------------------------------
-# IAM Policy — S3 Read/Write on the attribution-import file bucket
-# -------------------------------------------------------
-data "aws_iam_policy_document" "attribution-import_bucket_rw" {
-
-  statement {
-    sid    = "ListBucket"
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket",
-      "s3:GetBucketLocation",
-    ]
-
-    resources = [
-      module.attribution-import_file_bucket.arn,
-    ]
-  }
-
-  statement {
-    sid    = "ReadWriteObjects"
-    effect = "Allow"
-
-    actions = [
-      # Read
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetObjectTagging",
-      "s3:PutObject",
-      "s3:PutObjectTagging",
-      "s3:DeleteObject",
-      "s3:DeleteObjectVersion",
-      "s3:AbortMultipartUpload",
-      "s3:ListMultipartUploadParts",
-    ]
-
-    resources = [
-      "${module.attribution-import_file_bucket.arn}/*",
-    ]
-  }
-}
-
-resource "aws_iam_policy" "attribution-import_file_bucket" {
-  name        = "${local.app}-${local.env}-${local.service}-attribution-import-bucket-rw"
-  description = "Read/write access to the ${local.app}-${local.env}-${local.service} attribution-import file bucket"
-  policy      = data.aws_iam_policy_document.attribution-import_bucket_rw.json
-}
-
-resource "aws_iam_role_policy_attachment" "attribution-import_file_bucket_rw" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.attribution-import_file_bucket.arn
-}
-
-# -------------------------------------------------------
-# attribution-import Lambda
-# -------------------------------------------------------
-resource "aws_s3_object" "dummy_file_upload" {
-  bucket = module.attribution-import_lambda_bucket.id
-  key    = local.lambda_filename
-  source = "${path.module}/${local.lambda_filename}"
-}
-
-resource "aws_lambda_function" "this" {
-  s3_bucket         = module.attribution-import_lambda_bucket.id
-  s3_key            = aws_s3_object.dummy_file_upload.key
-  s3_object_version = aws_s3_object.dummy_file_upload.version_id
-  package_type      = "Zip"
-  handler           = "bootstrap"
-
-  function_name                  = local.name_prefix
-  description                    = "Ingests the most recent attribution import from BFD"
-  kms_key_arn                    = local.kms_key_arn_primary
-  memory_size                    = 2048
-  reserved_concurrent_executions = 1
-  role                           = aws_iam_role.this.arn
-  runtime                        = "provided.al2023"
-  skip_destroy                   = false
-  timeout                        = 900
-  architectures = [
-    "arm64",
-  ]
-
-  tags = {
-    code = "https://github.com/CMSgov/bcda-app/tree/main/bcda/lambda/attribution-import"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      s3_object_version,
-      s3_key,
-    ]
-  }
-
-  environment {
-    variables = {
-      APP_NAME = local.name_prefix
-      DB_HOST  = "postgres://${data.aws_rds_cluster.this.endpoint}:${data.aws_rds_cluster.this.port}/bcda"
-      ENV      = local.env
-    }
-  }
-
-  ephemeral_storage {
-    size = 512
-  }
-
-  logging_config {
-    log_format = "Text"
-    log_group  = "/aws/lambda/bcda-${local.env}-${local.service}"
-  }
-
-  tracing_config {
-    mode = "Active"
-  }
-
-  vpc_config {
-    ipv6_allowed_for_dual_stack = false
-    security_group_ids          = [aws_security_group.this.id]
-    subnet_ids                  = local.private_subnets
-  }
-}
-
-data "aws_rds_cluster" "this" {
-  cluster_identifier = "${local.app}-${local.env}-aurora"
-}
-
-data "aws_security_group" "db" {
-  name = "bcda-${local.env}-db"
-}
-
-resource "aws_security_group_rule" "db" {
-  type                     = "ingress"
-  from_port                = data.aws_rds_cluster.this.port
-  to_port                  = data.aws_rds_cluster.this.port
-  protocol                 = "tcp"
-  description              = "attribution-import lambda access"
-  security_group_id        = data.aws_security_group.db.id
-  source_security_group_id = aws_security_group.this.id
-}
-
-# -------------------------------------------------------
-# attribution-import SQS and SNS
-# -------------------------------------------------------
-
-resource "aws_sqs_queue" "this" {
-  content_based_deduplication       = false
-  delay_seconds                     = 0
-  fifo_queue                        = false
-  kms_data_key_reuse_period_seconds = 300
-  kms_master_key_id                 = local.kms_key_arn_primary
-  name                              = local.name_prefix
-
-  receive_wait_time_seconds  = 0
-  visibility_timeout_seconds = 900
-}
-
-resource "aws_sns_topic" "attribution-import_nextgen_topic" {
-  display_name      = "bfd-${local.env}-attribution-import-inbound-received-s3-bcda"
-  name              = "bfd-${local.env}-attribution-import-inbound-received-s3-bcda"
-  kms_master_key_id = "alias/${module.platform.app}-${module.platform.env}"
-}
-
-resource "aws_sns_topic_subscription" "this" {
-  endpoint  = aws_sqs_queue.this.arn
-  protocol  = "sqs"
-  topic_arn = aws_sns_topic.attribution-import_nextgen_topic.arn
-}
-
-data "aws_iam_policy_document" "attribution-import_topic" {
-  statement {
-    sid       = "AllowS3Publish"
-    actions   = ["sns:Publish"]
-    resources = [aws_sns_topic.attribution-import_nextgen_topic.arn]
-
     principals {
       type        = "Service"
       identifiers = ["s3.amazonaws.com"]
     }
+
+    actions   = ["SNS:Publish"]
+    resources = [aws_sns_topic.this.arn]
 
     condition {
       test     = "ArnLike"
@@ -388,7 +119,36 @@ data "aws_iam_policy_document" "attribution-import_topic" {
   }
 }
 
-resource "aws_sns_topic_policy" "attribution-import_nextgen_topic" {
-  arn    = aws_sns_topic.attribution-import_nextgen_topic.arn
-  policy = data.aws_iam_policy_document.attribution-import_topic.json
+resource "aws_sns_topic" "this" {
+  name              = "${local.full_name}-topic"
+  kms_master_key_id = "alias/bcda-${var.env}"
+}
+
+resource "aws_sns_topic_policy" "this" {
+  arn = aws_sns_topic.this.arn
+
+  policy = data.aws_iam_policy_document.topic.json
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  endpoint  = module.attribution_import_queue.arn
+  protocol  = "sqs"
+  topic_arn = aws_sns_topic.this.arn
+}
+
+# Add a rule to the database security group to allow access from the function
+
+data "aws_security_group" "db" {
+  name = local.db_sg_name
+}
+
+resource "aws_security_group_rule" "function_access" {
+  type        = "ingress"
+  from_port   = 5432
+  to_port     = 5432
+  protocol    = "tcp"
+  description = "attribution-import function access"
+
+  security_group_id        = data.aws_security_group.db.id
+  source_security_group_id = module.attribution_import_function.security_group_id
 }
