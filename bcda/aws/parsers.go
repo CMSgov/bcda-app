0f77c0c047b5aa50265689566e94a47051f83db0
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-09 11:28:03 -0600

    gofmt

--- bcda/aws/parsers.go
+++ bcda/aws/parsers.go
@@ -25,7 +25,7 @@ func ParseSQSEvent(event events.SQSEvent) (*events.S3Event, error) {
 
 	var s3Event events.S3Event
 	err = json.Unmarshal([]byte(snsEntity.Message), &s3Event)
-    unmarshalTypeErr = new(json.UnmarshalTypeError)
+	unmarshalTypeErr = new(json.UnmarshalTypeError)
 	if errors.As(err, &unmarshalTypeErr) {
 		log.Warn("Skipping event due to unrecognized format for S3")
 		return nil, nil
