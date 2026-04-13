3e88bdf7e036556b5efb2608c6e28875b4f75956
juliareynolds-nava <juliareynolds@navapbc.com>
2026-04-13 10:43:35 -0600

    fix missing underscores

--- bcda/lambda/attribution-import/main_test.go
+++ bcda/lambda/attribution-import/main_test.go
@@ -334,7 +334,7 @@ func TestSQSEventRoutingNoObjectCreated(t *testing.T) {
 
 func TestLoadBCDAParamsSetsEnv(t *testing.T) {
 	os.Setenv("ENV", "test")
-	-, err := loadBCDAParams()
+	_, err := loadBCDAParams()
 	assert.NoError(t, err)
 }
 
