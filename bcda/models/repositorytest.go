77bbf37c3209262aec4c17f8d95f4de305ac9fdf
Parwinder Bhagat <Parwinder.Bhagat@e14s.com>
2026-05-13 16:25:02 -0500

    fix a cyclical dependency in mock generation

--- bcda/models/repositorytest.go
+++ bcda/models/repositorytest.go
@@ -4,7 +4,7 @@ import "testing"
 
 // SetMockRepository sets the current repository to the one that's supplied in this function.
 // It leverages the Cleanup() func to ensure the original repository is restored at the end of the test.
-func SetMockRepository(t *testing.T, other *MockRepository) {
+func SetMockRepository(t *testing.T, other Repository) {
 	// Ensure that we restore the original provider when the test completes
 	originalRepository := repository
 	t.Cleanup(func() {
