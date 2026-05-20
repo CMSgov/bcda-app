fc9c6f7ab20ce847e41001b9ecc32eca8d15dd45
juliareynolds-nava <juliareynolds@navapbc.com>
2026-05-20 08:41:14 -0600

    remove static site

--- terraform/modules/insights_data_sampler_lambda/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_arn" {
-  value = aws_lambda_function.insights_data_sampler.arn
-}
-
-output "lambda_function_name" {
-  value = aws_lambda_function.insights_data_sampler.function_name
-}
--- terraform/static_site/stage/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "bcda-test-tfstate-20250409171646342600000001"
-    key          = "static-site/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
