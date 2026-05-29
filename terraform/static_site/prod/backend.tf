2c4b53cdf2fc86bb0b70d3877b7d9084a0848e14
juliareynolds-nava <juliareynolds@navapbc.com>
2026-05-29 12:20:16 -0600

    remove static site tf

--- terraform/static_site/prod/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "bcda-prod-tfstate-20250411203841436200000001"
-    key          = "static-site/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
