a3c5ae82a5a1258e51ee519a89fdfc69ad5044c9
Michael Valdes <michaelvaldes@navapbc.com>
2026-06-02 16:40:52 -0400

    BCDA-10031: Update create-creds build arch and omit dummy function

--- ops/services/30-create-aco-creds/main.tf
+++ ops/services/30-create-aco-creds/main.tf
@@ -1,6 +1,7 @@
 locals {
+  app               = "bcda"
   service           = "admin-create-aco-creds"
-  full_name         = "${var.app}-${var.env}-${local.service}"
+  full_name         = "${local.app}-${var.env}-${local.service}"
   db_sg_name        = "bcda-${var.env}-db"
   memory_size       = 256
   creds_bucket_name = "bcda-${var.env}-aco-creds-*"
@@ -34,7 +35,7 @@ module "platform" {
 
   providers = { aws = aws, aws.secondary = aws.secondary }
 
-  app         = var.app
+  app         = local.app
   env         = var.env
   root_module = "https://github.com/CMSgov/bcda-app/tree/main/ops/services/10-config"
   service     = local.service
@@ -46,15 +47,16 @@ module "platform" {
 module "admin_create_aco_creds_function" {
   source = "github.com/CMSgov/cdap//terraform/modules/function?ref=2874c72ccd4c4821e5e3f77ccf61cf77ed05169f"
 
-  app          = var.app
+  app          = local.app
   env          = var.env
   architecture = "arm64"
 
   name        = local.full_name
   description = "Finds and Creates ACO Credentials for passed in ACO ID and IP addresses"
 
-  handler = "bootstrap"
-  runtime = "provided.al2"
+  handler             = "bootstrap"
+  runtime             = "provided.al2"
+  create_function_zip = false
 
   memory_size = local.memory_size
 
@@ -62,7 +64,7 @@ module "admin_create_aco_creds_function" {
 
   environment_variables = {
     ENV      = var.env
-    APP_NAME = "${var.app}-${var.env}-admin-create-aco-creds"
+    APP_NAME = "${local.app}-${var.env}-admin-create-aco-creds"
   }
 
   extra_kms_key_arns = [data.aws_kms_alias.bcda_app_config_kms_key.target_key_arn]
--- ops/services/30-create-aco-creds/variables.tf
+++ ops/services/30-create-aco-creds/variables.tf
@@ -1,12 +1,3 @@
-variable "app" {
-  description = "The application name (bcda)"
-  type        = string
-  validation {
-    condition     = contains(["bcda"], var.app)
-    error_message = "Valid value for app is bcda."
-  }
-}
-
 variable "env" {
   description = "The application environment (dev, test, sbx, sandbox, prod)"
   type        = string
