e0b57746f34804332250f7988ad80e5b2f5bca11
juliareynolds-nava <juliareynolds@navapbc.com>
2026-06-02 12:40:15 -0600

    update directories

---
+++ ops/services/40-datadog-monitors/main.tf
@@ -0,0 +1,55 @@
+locals {
+
+  ## Evaluates config/defaults.yml and overwrites values with those from config/${var.env}.yml for each
+  ## variable/key type. Creates a hierarchy of defaults, so the modules/datadog_monitors defaults are
+  ## the least prioritized, followed by config/defaults.yml, followed by the environment specific settings.
+
+  defaults   = yamldecode(file("config/defaults.yml"))
+  env_config = yamldecode(file("config/${var.env}.yml"))
+
+  shadow_mode = lookup(local.env_config, "shadow_mode", local.defaults.shadow_mode)
+
+  # map-typed keys
+  monitor_config = merge(
+    { for key in keys(local.defaults) : key => merge(
+      lookup(local.defaults, key, {}),
+      lookup(local.env_config, key, {})
+      ) if can(keys(local.defaults[key])) # only process map-typed keys
+    },
+    { shadow_mode = local.shadow_mode }
+  )
+
+  # handles a case where the notifications are null
+  _env_channels = try(local.env_config.notifications.channels, null)
+
+  # always use the notification channels set up in the defaults, and adds those from the environment
+  notify = join(" ", concat(
+    local.defaults.notifications.channels,
+    local._env_channels != null ? local._env_channels : []
+  ))
+}
+
+# Use platform module to derive datadog keys via ssm_root_map
+# Can be replaced with direct data lookups 
+module "platform" {
+  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=941672f97adfd8a19ce6533313302c4c74bac7a8"
+  providers = { aws = aws, aws.secondary = aws.secondary }
+
+  app          = "bcda"
+  env          = var.env
+  root_module  = "https://github.com/CMSgov/bcda/tree/main/terraform/services/${basename(abspath(path.module))}/"
+  service      = replace(basename(abspath(path.module)), "/^[0-9]+-/", "")
+  ssm_root_map = { datadog = "/cdap/${var.env}/datadog/cicd/" }
+}
+
+###################
+# Common Monitors #
+###################
+
+module "common_datadog_monitors" {
+  source = "github.com/CMSgov/cdap//terraform/modules/datadog_monitors?ref=PLT-1655/notify"
+
+  app            = "bcda"
+  env            = var.env
+  monitor_config = local.monitor_config
+}
