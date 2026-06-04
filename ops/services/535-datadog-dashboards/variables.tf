19eb88f0f46caf5f6a46fa3deef48caf102c7e20
juliareynolds-nava <juliareynolds@navapbc.com>
2026-06-04 13:35:51 -0600

    remove old mod

--- ops/services/535-datadog-dashboards/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "env" {
-  description = "The application environment (test, prod)"
-  type        = string
-  validation {
-    condition     = contains(["test", "prod"], var.env)
-    error_message = "Valid values for env are test, prod."
-  }
-}
