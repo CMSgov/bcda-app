b5940abaad41569aa8f281a8df96010ffaaf5667
Michael Valdes <michaelvaldes@navapbc.com>
2026-06-05 16:55:16 -0400

    Update function module for admin-create-aco

--- ops/services/30-admin-create-aco/main.tf
+++ ops/services/30-admin-create-aco/main.tf
@@ -23,18 +23,16 @@ module "platform" {
 }
 
 module "admin_create_aco_function" {
-  source = "github.com/CMSgov/cdap//terraform/modules/function?ref=2874c72ccd4c4821e5e3f77ccf61cf77ed05169f"
+  source = "github.com/CMSgov/cdap//terraform/modules/function?ref=945fbd644cc8d239bdf3f3a3a7241fb6066a0f55"
 
-  app          = local.app
-  env          = local.env
+  platform     = module.platform
   architecture = "arm64"
 
-  name        = local.full_name
+  name        = local.service
   description = "Creates an ACO for BCDA."
 
-  handler             = "bootstrap"
-  runtime             = "provided.al2023"
-  create_function_zip = false
+  handler = "bootstrap"
+  runtime = "provided.al2023"
 
   memory_size = local.memory_size
 
@@ -43,7 +41,14 @@ module "admin_create_aco_function" {
     APP_NAME = "${local.app}-${local.env}-admin-create-aco"
   }
 
+  ssm_parameter_paths = [
+    "/bcda/${local.env}/sensitive/DATABASE_URL",
+    "/slack/token/workflow-alerts"
+  ]
+
   extra_kms_key_arns = [data.aws_kms_alias.bcda_app_config_kms_key.target_key_arn]
+
+  github_actions_repos = ["CMSgov/bcda-app"]
 }
 
 # Add a rule to the database security group to allow access from the function
@@ -60,4 +65,9 @@ resource "aws_security_group_rule" "function_access" {
 
   security_group_id        = data.aws_security_group.db.id
   source_security_group_id = module.admin_create_aco_function.security_group_id
+}
+
+import {
+  to = module.admin_create_aco_function.aws_cloudwatch_log_group.function
+  id = "/aws/lambda/${local.full_name}"
 }
