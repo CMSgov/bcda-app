a531f3950ac45489f51799ceaa53b101b8425740
juliareynolds-nava <juliareynolds@navapbc.com>
2026-06-11 12:54:31 -0600

    added ingress for 5432

--- ops/services/30-attribution-import/main.tf
+++ ops/services/30-attribution-import/main.tf
@@ -251,6 +251,17 @@ resource "aws_security_group_rule" "function_access" {
   source_security_group_id = module.attribution_import_function.security_group_id
 }
 
+resource "aws_vpc_security_group_ingress_rule" "function_db_access" {
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+  description = "attribution-import function access"
+
+  security_group_id            = data.aws_security_group.db.id
+  referenced_security_group_id = module.attribution_import_function.security_group_id
+  depends_on                   = [aws_security_group_rule.function_access]
+}
+
 resource "aws_s3_bucket_notification" "this" {
   bucket = module.attribution-import_file_bucket.id
 
