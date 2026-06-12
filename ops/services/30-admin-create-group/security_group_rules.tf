2e0d7ba640f4c17d11c0081dd67f3a832680c93e
Michael Valdes <michaelvaldes@navapbc.com>
2026-06-12 16:10:16 -0400

    Add db egress rule to lambdas

--- ops/services/30-admin-create-group/security_group_rules.tf
+++ ops/services/30-admin-create-group/security_group_rules.tf
@@ -12,3 +12,13 @@ resource "aws_vpc_security_group_ingress_rule" "function_db_access" {
   referenced_security_group_id = module.admin_create_group_function.security_group_id
   depends_on                   = [aws_security_group_rule.function_access] # TODO: Delete depends_on after deploying BCDA-10031
 }
+
+resource "aws_vpc_security_group_egress_rule" "db_tcp" {
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+  description = "egress to db"
+
+  security_group_id            = module.admin_create_group_function.security_group_id
+  referenced_security_group_id = data.aws_security_group.db.id
+}
