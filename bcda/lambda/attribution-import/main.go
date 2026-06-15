22c7c275182f42331e98ec0c7349918aaa05e9a7
Carl Partridge <carlpartridge@navapbc.com>
2026-06-15 12:11:21 -0400

    BCDA-10120: Add datadog APM monitoring (#1399)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/BCDA-10120
    
    ## 🛠 Changes
    
    Add Datadog orchestrion which adds compile time automated APM.
    
    ## ℹ️ Context
    
    Part of moving monitoring to datadog.
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    Local linting and testing, will deploy to dev from this branch, make
    sure everything builds/deploys, and also verify in datadog that metrics
    are happening.
    
    ---------
    
    Co-authored-by: Parwinder Bhagat <Parwinder.Bhagat@e14s.com>

--- bcda/lambda/attribution-import/main.go
+++ bcda/lambda/attribution-import/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/DataDog/dd-trace-go/v2/profiler"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -15,7 +16,9 @@ import (
 
 	bcdaaws "github.com/CMSgov/bcda-app/bcda/aws"
 	"github.com/CMSgov/bcda-app/bcda/cclf"
+	"github.com/CMSgov/bcda-app/bcda/constants"
 	"github.com/CMSgov/bcda-app/bcda/database"
+	"github.com/CMSgov/bcda-app/log"
 	"github.com/CMSgov/bcda-app/optout"
 
 	"github.com/CMSgov/bcda-app/conf"
@@ -26,6 +29,16 @@ import (
 )
 
 func main() {
+	err := profiler.Start(
+		profiler.WithService("BCDA Attribution Import"),
+		profiler.WithEnv(conf.GetEnv("ENV")),
+		profiler.WithVersion(constants.Version),
+	)
+	if err != nil {
+		log.API.Warn(err)
+	}
+	defer profiler.Stop()
+
 	lambda.Start(attributionImportHandler)
 }
 
