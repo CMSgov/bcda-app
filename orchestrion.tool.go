dabd853cad41f285c0de78d562369f1115dc1856
Michael Valdes <michaelvaldes@navapbc.com>
2026-07-10 18:10:00 -0400

    BCDA-10186: reinstall orchestrion with limited deps

--- orchestrion.tool.go
+++ orchestrion.tool.go
@@ -19,5 +19,23 @@ import (
 	// Do not remove.
 	_ "github.com/DataDog/orchestrion" // integration
 
-	_ "github.com/DataDog/dd-trace-go/orchestrion/all/v2" // integration
+	_ "github.com/DataDog/dd-trace-go/contrib/aws/aws-sdk-go-v2/v2/aws" // integration
+	_ "github.com/DataDog/dd-trace-go/contrib/aws/aws-sdk-go/v2/aws"    // integration
+
+	_ "github.com/DataDog/dd-trace-go/contrib/database/sql/v2" // integration
+
+	_ "github.com/DataDog/dd-trace-go/contrib/go-chi/chi.v5/v2" // integration
+	_ "github.com/DataDog/dd-trace-go/contrib/go-chi/chi/v2"    // integration
+
+	_ "github.com/DataDog/dd-trace-go/contrib/jackc/pgx.v5/v2" // integration
+
+	_ "github.com/DataDog/dd-trace-go/contrib/log/slog/v2" // integration
+
+	_ "github.com/DataDog/dd-trace-go/contrib/net/http/v2" // integration
+
+	_ "github.com/DataDog/dd-trace-go/v2/contrib/os" // integration
+
+	_ "github.com/DataDog/dd-trace-go/v2/ddtrace/tracer" // integration
+	_ "github.com/DataDog/dd-trace-go/v2/orchestrion"    // integration
+	_ "github.com/DataDog/dd-trace-go/v2/profiler"       // integration
 )
