bf1fe2477dcff6ee85d6f46741e3d75ca5421e32
Michael Valdes <michaelvaldes@navapbc.com>
2026-08-05 13:24:45 -0400

    Refactor bene-prefs to use lambda module

--- ops/services/30-bene-prefs/main.tf
+++ ops/services/30-bene-prefs/main.tf
@@ -5,12 +5,10 @@ locals {
   env            = terraform.workspace
   service_prefix = "${local.app}-${local.env}"
 
-  account_id            = module.platform.aws_caller_identity.account_id
-  kms_key_arn_primary   = module.platform.kms_alias_primary.target_key_arn
-  kms_key_arn_secondary = module.platform.kms_alias_secondary.target_key_arn
-  name_prefix           = "${local.service_prefix}-${local.service}"
-  private_subnets       = nonsensitive(toset(keys(module.platform.private_subnets)))
-  lambda_filename       = "lambda_function.zip"
+  account_id          = module.platform.aws_caller_identity.account_id
+  kms_key_arn_primary = module.platform.kms_alias_primary.target_key_arn
+  name_prefix         = "${local.service_prefix}-${local.service}"
+  db_sg_name          = "bcda-${local.env}-db"
 }
 
 module "platform" {
@@ -31,277 +29,41 @@ data "aws_rds_cluster" "this" {
   cluster_identifier = "${local.app}-${local.env}-aurora"
 }
 
-data "aws_security_groups" "db" {
-  tags = {
-    Name = "bcda-${local.env}-db"
+module "bene_prefs_function" {
+  source = "github.com/CMSgov/cdap//terraform/modules/function?ref=8a6527c0689bb46ae0e74bd47e4087ab59cff1b0"
+
+  architecture = "arm64"
+
+  name        = local.service
+  description = "Ingests the most recent beneficiary opt-out list from BFD"
+
+  handler = "bootstrap"
+  runtime = "provided.al2023"
+
+  memory_size = 128
+
+  platform = module.platform
+
+  liveness_check_enabled = false
+
+  github_actions_repos = ["bcda-app:*"]
+
+  environment_variables = {
+    APP_NAME = local.name_prefix
+    DB_HOST  = "postgres://${data.aws_rds_cluster.this.endpoint}:${data.aws_rds_cluster.this.port}/bcda"
+    ENV      = local.env
   }
-}
 
-resource "aws_security_group_rule" "db" {
-  type                     = "ingress"
-  from_port                = data.aws_rds_cluster.this.port
-  to_port                  = data.aws_rds_cluster.this.port
-  protocol                 = "tcp"
-  security_group_id        = one([data.aws_security_groups.db.ids])[0]
-  source_security_group_id = aws_security_group.this.id
-}
-
-# ---------------------------------------------------------------------------
-# Managed policies
-# ---------------------------------------------------------------------------
-
-data "aws_iam_policy_document" "assume_bucket_role" {
-  statement {
-    sid       = "AssumeBucketRole"
-    actions   = ["sts:AssumeRole"]
-    resources = [module.platform.ssm.bene-prefs.iam_bucket_role_arn.value]
+  function_role_inline_policies = {
+    sqs-bene-prefs-bucket-events = data.aws_iam_policy_document.bucket_sqs.json,
+    assume-bucket                = data.aws_iam_policy_document.assume_bucket_role.json,
+    admin-subscribe-bfd-topic    = data.aws_iam_policy_document.subscribe_bfd_topic.json
   }
-}
 
-resource "aws_iam_policy" "assume_bucket_role" {
-  name        = "bcda-${local.env}-${local.service}-assume-bucket-role"
-  path        = module.platform.iam_defaults.path
-  description = "Allows ${local.service} to assume the S3 bucket role from SSM."
-
-  policy = data.aws_iam_policy_document.assume_bucket_role.json
-}
-
-resource "aws_iam_policy" "admin_subscribe_bfd_topic" {
-  name        = "admin-subscribe-bfd-topic2"
-  description = "Allows subscribing to the BFD bene-prefs-received SNS topic in account 830858426211"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid      = "AllowBFDSNSSubscribe"
-        Effect   = "Allow"
-        Action   = "sns:Subscribe"
-        Resource = "arn:aws:sns:us-east-1:830858426211:bfd-test-bene-prefs-received-s3-bcda"
-      }
-    ]
-  })
-}
-
-data "aws_iam_policy_document" "default_function" {
-  statement {
-    sid = "SsmSqsLogsEc2"
-    actions = [
-      "ssm:GetParameters",
-      "ssm:GetParameter",
-      "sqs:ReceiveMessage",
-      "sqs:GetQueueAttributes",
-      "sqs:DeleteMessage",
-      "logs:PutLogEvents",
-      "logs:CreateLogStream",
-      "logs:CreateLogGroup",
-    ]
-    resources = ["*"] #TODO: Consider splitting into discrete statements/policy allowances
-  }
-  statement {
-
-    sid = "KmsEncryptDecrypt"
-    actions = [
-      "kms:GenerateDataKey",
-      "kms:Encrypt",
-      "kms:Decrypt",
-    ]
-    resources = [
-      local.kms_key_arn_primary,
-      local.kms_key_arn_secondary,
-      module.platform.ssm.bene-prefs.iam_bucket_role_arn.value
-    ]
-  }
-}
-
-resource "aws_iam_policy" "default_function" {
-  name        = "bcda-${local.env}-${local.service}-default-function"
-  path        = module.platform.iam_defaults.path
-  description = "SSM, SQS, CloudWatch Logs, EC2 networking, and KMS permissions for ${local.service}."
-
-  policy = data.aws_iam_policy_document.default_function.json
-}
-
-# ---------------------------------------------------------------------------
-# IAM role
-# ---------------------------------------------------------------------------
-
-resource "aws_iam_role" "this" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-      {
-        Action = [
-          "sts:TagSession",
-          "sts:AssumeRoleWithWebIdentity",
-        ]
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:CMSgov/bcda-app:*"
-          }
-        }
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-      },
-      {
-        Action = [
-          "sts:TagSession",
-          "sts:AssumeRole",
-        ]
-        Effect = "Allow"
-        Principal = {
-          AWS = [
-            module.platform.kion_roles["ct-ado-dasg-application-admin"].arn,
-            module.platform.kion_roles["ct-ado-bcda-application-admin"].arn,
-          ]
-        }
-      },
-    ]
-  })
-
-  force_detach_policies = true
-  name                  = "bcda-${local.env}-${local.service}"
-  path                  = module.platform.iam_defaults.path
-  permissions_boundary  = module.platform.iam_defaults.boundary
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_access" {
-  role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.default_function.arn
-}
-
-resource "aws_iam_role_policy_attachment" "assume_bucket_role" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.assume_bucket_role.arn
-}
-
-resource "aws_iam_role_policy_attachment" "admin_subscribe_bfd_topic" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.admin_subscribe_bfd_topic.arn
-}
-
-module "bucket" {
-  source = "github.com/CMSgov/cdap//terraform/modules/bucket?ref=787224b"
-
-  app           = local.app
-  env           = local.env
-  name          = "${local.app}-${local.env}-${local.service}"
-  ssm_parameter = "/${local.app}/${local.env}/${local.service}/nonsensitive/bucket_name"
-}
-
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/lambda/bcda-${local.env}-${local.service}"
-  retention_in_days = 180
-  skip_destroy      = true
-
-  tags = {
-    Name = "/aws/lambda/bcda-${local.env}-${local.service}"
-  }
-}
-
-resource "aws_s3_object" "dummy_file_upload" {
-  bucket = module.bucket.id
-  key    = local.lambda_filename
-  source = "${path.module}/${local.lambda_filename}"
-}
-
-resource "aws_lambda_function" "this" {
-  s3_bucket         = aws_s3_object.dummy_file_upload.bucket
-  s3_key            = aws_s3_object.dummy_file_upload.key
-  s3_object_version = aws_s3_object.dummy_file_upload.version_id
-  package_type      = "Zip"
-  handler           = "bootstrap"
-
-  function_name                  = local.name_prefix
-  description                    = "Ingests the most recent beneficiary opt-out list from BFD"
-  kms_key_arn                    = local.kms_key_arn_primary
-  memory_size                    = 128
-  reserved_concurrent_executions = 1
-  role                           = aws_iam_role.this.arn
-  runtime                        = "provided.al2023"
-  skip_destroy                   = false
-  timeout                        = 900
-  architectures = [
-    "arm64",
+  ssm_parameter_paths = [
+    "/bcda/${local.env}/sensitive/api/DATABASE_URL",
+    "/bcda/${local.env}/bene-prefs/sensitive/iam_bucket_role_arn"
   ]
-
-  tags = {
-    code = "https://github.com/CMSgov/bcda-app/tree/main/bcda/lambda/bene-prefs"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      filename,
-    ]
-  }
-
-  environment {
-    variables = {
-      APP_NAME = local.name_prefix
-      DB_HOST  = "postgres://${data.aws_rds_cluster.this.endpoint}:${data.aws_rds_cluster.this.port}/bcda"
-      ENV      = local.env
-    }
-  }
-
-  ephemeral_storage {
-    size = 512
-  }
-
-  logging_config {
-    log_format = "Text"
-    log_group  = "/aws/lambda/bcda-${local.env}-${local.service}"
-  }
-
-  tracing_config {
-    mode = "Active"
-  }
-
-  vpc_config {
-    ipv6_allowed_for_dual_stack = false
-    security_group_ids          = [aws_security_group.this.id]
-    subnet_ids                  = local.private_subnets
-  }
-}
-
-resource "aws_security_group" "this" {
-  description = "Temporary SG for ${local.name_prefix}"
-  egress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description = ""
-      from_port   = 0
-      ipv6_cidr_blocks = [
-        "::/0",
-      ]
-      prefix_list_ids = []
-      protocol        = "-1"
-      security_groups = []
-      self            = false
-      to_port         = 0
-    },
-  ]
-  name   = local.name_prefix
-  vpc_id = module.platform.vpc_id
-  tags   = { Name = local.name_prefix }
 }
 
 resource "aws_sqs_queue" "this" {
@@ -356,7 +118,7 @@ resource "aws_sns_topic_subscription" "this" {
 
 resource "aws_lambda_event_source_mapping" "this" {
   event_source_arn = aws_sqs_queue.this.arn
-  function_name    = aws_lambda_function.this.function_name
+  function_name    = module.bene_prefs_function.name
   batch_size       = 1
   enabled          = true
 }
