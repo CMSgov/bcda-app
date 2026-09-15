4d8866e3bbb2a7529f9d1d16397a28dfd4133094
Grant Freeman <grantfreeman@navapbc.com>
2026-09-15 13:34:04 -0700

    add ssas parameter lookup

--- ops/services/22-ssas/data.tf
+++ ops/services/22-ssas/data.tf
@@ -2,6 +2,10 @@ data "aws_vpc" "main" {
   id = module.platform.vpc_id
 }
 
+data "aws_ecr_repository" "ecr_ssas" {
+  name = "bcda-ssas"
+}
+
 data "aws_security_group" "app_sg" {
   vpc_id = module.platform.vpc_id
   filter {
@@ -12,4 +16,10 @@ data "aws_security_group" "app_sg" {
 
 data "aws_acm_certificate" "ssas" {
   domain = local.ssas_domain
+}
+
+data "aws_ssm_parameter" "params_ssas" {
+  for_each        = toset(local.config.parameter_names)
+  name            = "/bcda/${module.platform.parent_env}/sensitive/ssas/${each.value}"
+  with_decryption = true
 }
