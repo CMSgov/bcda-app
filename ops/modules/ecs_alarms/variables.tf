9e0be183aeff1eefa025d7943da82d4c8fe40a84
Grant Freeman <grantfreeman@navapbc.com>
2026-09-17 13:16:26 -0700

    format ecs_alarms module

--- ops/modules/ecs_alarms/variables.tf
+++ ops/modules/ecs_alarms/variables.tf
@@ -8,19 +8,19 @@ variable "service_name" {
 
 variable "ok_notification_arn" {
   type = string
-} 
+}
 
 variable "alarm_notification_arn" {
   type = string
-} 
+}
 
 variable "alarms" {
   type = list(object(
-    { 
-     alarm_name : string,
+    {
+      alarm_name : string,
       alarm_description : string,
       metric_name : string,
-      period : number, 
-      eval_periods : number, 
-      threshold : number }))
+      period : number,
+      eval_periods : number,
+  threshold : number }))
 }
